67c72d121e4bd035932b1be4fea154ab6ee7da7e
DuPengFei-du <935820240@qq.com>
2019-10-10 13:01:12 +0800

    test

---
+++ c21/c21/c21.c
@@ -0,0 +1,74 @@
+//#include<stdio.h>
+//int main()
+//{
+//	char a = -1;//-1
+//	signed char b = -1;//-1
+//	unsigned char c = -1;//255
+//	printf("%d %d %d", a, b, c);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	char a = -128;
+//	printf("%u\n", a);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	char a = 128;
+//	printf("%u\n", a);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i = -20;
+//	unsigned int j = 10;
+//	printf("%d\n", i + j);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<windows.h>
+//int main()
+//{
+//	unsigned int i;
+//	for (i = 9; i >= 0; i--)
+//	{
+//		Sleep(100);
+//		printf("%u\n", i);
+//	}
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	char a[1000];
+//	int i;
+//	for (i = 0; i < 1000; i++)
+//	{
+//		a[i] = -1 - i;
+//	}
+//	printf("%d", strlen(a));
+//}
+//输出结果为255
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	unsigned char i = 0;
+//	for (i = 0; i <= 255; i++)
+//	{
+//		printf("hello world\n");
+//	}
+//	return 0;
+//}
