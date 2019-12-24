9363796f706653b3a2ff6847cb3dc4a00bc606cb
DuPengFei-du <935820240@qq.com>
2019-12-24 09:27:03 +0800

    test

---
+++ Pro28/Pro28/Pro28.cpp
@@ -0,0 +1,25 @@
+//编写一个程序，可以一直接收键盘字符，
+//如果是小写字符就输出对应的大写字符，
+//如果接收的是大写字符，就输出对应的小写字符，
+//如果是数字不输出。
+//void Exchange()
+//{
+//	char a;
+//	while (1)
+//	{
+//		fflush(stdin);
+//		scanf("%c", &a);
+//		if (a>= 65 && a<= 90)
+//			printf("%c\n", a + 32);
+//		else if (a >= 97 && a <= 122)
+//			printf("%c\n", a - 32);
+//		else
+//			continue;
+//	}
+//}
+//
+//int main()
+//{
+//	Exchange();
+//	return 0;
+//}
