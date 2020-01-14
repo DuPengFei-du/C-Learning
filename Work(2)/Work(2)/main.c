6c2133601f4398471cc0884825ad0365f2a5b33e
DuPengFei-du <935820240@qq.com>
2020-01-14 10:57:01 +0800

    test

---
+++ Work(2)/Work(2)/main.c
@@ -0,0 +1,219 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//计算n的阶乘
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int i = 1;
+	int sum = 1;
+	(void)scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		sum = sum * i;
+	}
+	printf("%d", sum);
+	return 0;
+}
+
+
+//求阶乘的和
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int i = 1;
+	int ret = 1;
+	int sum = 0;
+	(void)scanf("%d", &n);
+	for (n = 1; n <= 10; n++)
+	{
+		//ret = 1;
+		//i = 1;
+		while (i <= n)
+		{
+			ret = ret * i;
+			i++;
+		}
+		sum = sum + ret;
+	}
+	printf("%d", sum);
+	return 0;
+}
+
+
+//二分查找
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int left = 0;
+	int key = 7;
+	int right = sz - 1;
+	while (left <= right)
+	{
+		int mid = left + (right - left) / 2;
+		if (arr[mid] < key)
+		{
+			left = mid + 1;
+		}
+		else if (arr[mid] > key)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			printf("找到了,下标为:%d", mid);
+			break;
+		}
+	}
+	if (left > right)
+		printf("找不到\n");
+	return 0;
+}
+
+
+//将二分查找封装成一个函数
+#include<stdio.h>
+int BinarySearch(int arr[], int sz)
+{
+	int key = 7;
+	int left = 0;
+	int right = sz - 1;
+	while (left <= right)
+	{
+		int mid = left + (right - left) / 2;
+		if (arr[mid] < key)
+		{
+			left = mid + 1;
+		}
+		else if (arr[mid] > key)
+		{
+			right = mid - 1;
+		}
+		else
+			return mid;
+	}
+	return -1;
+}
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int ret = BinarySearch(arr, sz);
+	if (ret == -1)
+	{
+		printf("找不到\n");
+	}
+	else
+		printf("找到了，下标为:%d", ret);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<windows.h>
+int main()
+{
+	char arr1[] = "###########";
+	char arr2[] = "Hello World";
+	int sz = sizeof(arr1) / sizeof(arr1[0]);
+	int left = 0;
+	int right = sz - 2;
+	while (left <= right)
+	{
+		arr1[left] = arr2[left];
+		arr1[right] = arr2[right];
+		printf("%s\n", arr1);
+		Sleep(1000);
+		left++;
+		right--;
+	}
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int i = 0;
+	char password[20] = { 0 };
+	for (i = 0; i < 3; i++)
+	{
+		printf("请输入密码:>");
+		scanf("%s", password);
+		if (0 == strcmp(password, "123456"))
+		{
+			printf("登陆成功\n");
+			break;
+		}
+		else
+		{
+			printf("密码错误，请重新输入！\n");
+		}
+	}
+	if (i == 3)
+		printf("三次密码均错误，退出程序！\n");
+	return 0;
+}
+
+
+//猜数字游戏
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+void menu()
+{
+	printf("***** 欢迎来到猜数字游戏！*****\n");
+	printf("*****        1. play      *****\n");
+	printf("*****        0. exit      *****\n");
+	printf("***** 欢迎来到猜数字游戏！*****\n");
+}
+void game()
+{
+	int ret = 0;
+	int guess = 0;
+	ret = rand() % 100 + 1;
+	while (1)
+	{
+		printf("请输入数字:>");
+		(void)scanf("%d", &guess);
+		if (guess > ret)
+			printf("猜大了\n");
+		else if (guess < ret)
+			printf("猜小了\n");
+		else
+		{
+			printf("猜对了\n");
+			break;
+		}
+	}
+}
+int main()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do
+	{
+		menu();
+		printf("请选择:>\n");
+		(void)scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏！\n");
+			break;
+		default:
+			printf("输入错误，请重新输入!\n");
+			break;
+		}
+	} while (input);
+	return 0;
+}
