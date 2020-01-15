184c78924c58e8dca66d435332a448521570428b
DuPengFei-du <935820240@qq.com>
2020-01-15 15:22:42 +0800

    test

---
+++ Array/Array/main.c
@@ -0,0 +1,158 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//一维数组的创建
+#include<stdio.h>
+int main()
+{
+	int arr1[10];
+	char arr2[5];
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5 };
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = { 'a','b','c' };
+	char arr2[] = "abc";
+	printf("%s", arr1);
+	printf("%s", arr2);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int i = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (i = 0; i < sz; i++)
+	{
+		printf("&arr[%d]=%p\n", i,&arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int* p = arr;  //得到了首元素的地址
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%p===%p\n", &arr[i], p + i);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int* p = arr;
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", *(p + i));
+	}
+	return 0;
+}
+
+
+//二维数组
+#include<stdio.h>
+int main()
+{
+	int arr1[3][4]; //创建了一个三行四列的数组
+	char arr2[4][5];
+	double arr3[3][5];
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[3][4] = { 1,2,3,4,5,6 };//后面没有初始化的元素全部当0对待
+	int arr2[3][4] = { {1,2},{3,4},{5,6} };//第一行为1 2 0 0 第二行为3 4 0 0
+}
+
+
+//二维数组的打印
+#include<stdio.h>
+int main()
+{
+	int arr[3][4] = { 0 };
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+
+//二维数组在内存中的存储
+#include<stdio.h>
+int main()
+{
+	int arr[3][4] = { 0 };
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			printf("&arr[%d][%d]=%p\n", i, j, &arr[i][j]);
+		}
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[3][4] = { 1,2,3,4,5,6,7,8,9,10,11,12 };
+	int i = 0;
+	int j = 0;
+	int* p = &arr[0][0];
+	for (i = 0; i < 12; i++)
+	{
+		printf("%d ", *(p + i));
+	}
+	return 0;
+}
