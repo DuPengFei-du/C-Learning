364ca318ebb66135334920deb4bcb46a436a1695
DuPengFei-du <935820240@qq.com>
2020-01-15 17:03:23 +0800

    test

---
+++ Array(2)/Array(2)/main.c
@@ -0,0 +1,116 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//冒泡排序
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,3,5,7,9,2,4,6,8,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < sz - 1; i++)
+	{
+		for (j = 0; j < sz - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+void BubbleSort(int arr[], int sz)
+{
+	int i = 0;
+	int j = 0;
+	for(i=0;i<sz-1;i++)
+	{
+		for (j = 0; j < sz - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
+int main()
+{
+	int i = 0;
+	int arr[10] = { 1,3,5,7,9,2,4,6,8,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	BubbleSort(arr, sz);
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+void BubbleSort(int arr[], int sz)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < sz - 1; i++)
+	{
+		int flag = 1;//假设已经有序
+		for (j = 0; j < sz - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+				flag = 0;
+			}
+		}
+		if (flag == 1)
+			break;
+	}
+}
+int main()
+{
+	int i = 0;
+	int arr[10] = { 1,3,5,7,9,2,4,6,8,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	BubbleSort(arr, sz);
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	printf("%d", sizeof(arr));
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	printf("%p\n", &arr);
+	printf("%p\n", &arr + 1);
+	return 0;
+}
