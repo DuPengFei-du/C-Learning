1eabf14472811c0a48bbfbd4fb9850302cdad7c5
DuPengFei-du <935820240@qq.com>
2020-01-17 15:52:40 +0800

    test

---
+++ Operator(2)/Operator(2)/main.c
@@ -0,0 +1,258 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	printf("%d\n", sizeof(a));  //4
+	printf("%d\n", sizeof(int));   //4
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	printf("%d\n", sizeof(arr)); //40  计算的是数组的大小
+	return 0;
+}
+
+
+#include<stdio.h>
+void test(int arr[])   //int *arr
+{
+	printf("%d\n", sizeof(arr));   //数组传参传过来的是首元素的地址，所以结果是4
+	//要用指针接收
+}
+int main()
+{
+	int arr[10] = { 0 };
+	test(arr);
+}
+
+
+#include<stdio.h>
+int main()
+{
+	short a = 10;
+	int b = 3;
+	printf("%d\n", sizeof(a = b + 5));
+	printf("%d\n", a);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = ~a;
+	printf("%d\n", b);
+	return 0;
+	//结果是-11
+    //00000000000000000000000000001010  --补码
+	//11111111111111111111111111110101  --按位取反后的补码
+	//11111111111111111111111111110100  --反码
+	//10000000000000000000000000001011  --原码
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = a++;
+	printf("%d\n", b);  //10
+	printf("%d\n", a);  //11
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = ++a;
+	printf("%d\n", b);  //11
+	printf("%d\n", a);  //11
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int* p = a;
+	printf("%d\n", a);
+	return 0;
+	//*p其实就是a
+}
+
+
+#include <stdio.h>
+void test1(int arr[])
+{
+	printf("%d\n", sizeof(arr));//(2)   //用指针接收的，大小为4
+}
+void test2(char ch[])
+{
+	printf("%d\n", sizeof(ch));//(4)  //这里是一个易错的点，他是用指针接收的
+	//所以大小还是4，与char是没有任何关系的
+}
+int main()
+{
+	int arr[10] = { 0 };
+	char ch[10] = { 0 };
+	printf("%d\n", sizeof(arr));//(1)
+	printf("%d\n", sizeof(ch));//(3)
+	test1(arr);
+	test2(ch);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr1[] = "abcdef";
+	if (arr1 == "abcdef")
+	{
+		printf("hehe\n");
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int len = strlen("abc");   //传过去的是a的地址
+	char* p = "abcdef";       //是把a的地址放在p中的
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 3;
+	int b = 5;
+	int c = a && b;
+	printf("%d\n", c);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 3;
+	int b = 5;
+	int c = a || b;
+	printf("%d\n", c);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 5;
+	int c = a && b;
+	printf("%d\n", c);
+	return 0;
+}
+
+
+#include <stdio.h>
+int main()
+{
+	int i = 0, a = 0, b = 2, c = 3, d = 4;
+	i = a++ && ++b && d++;
+	printf("a = %d\n b = %d\n c = %d\nd = %d\n", a, b, c, d);
+	return 0;
+}
+
+
+#include <stdio.h>
+int main()
+{
+	int i = 0, a = 0, b = 2, c = 3, d = 4;
+	i = a++||++b||d++;
+	printf("a = %d\n b = %d\n c = %d\nd = %d\n", a, b, c, d);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	int b = 2;
+	int c = (a > b, a = b + 10, a, b = a + 1);
+	printf("%d\n", c);
+	return 0;
+	//结果是13，逗号表达式
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	printf("%d\n", arr[5]);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int* p = arr;
+	printf("%d\n", arr[5]);
+	printf("%d\n", p[5]);
+	printf("%d\n", *(p+5));
+	printf("%d\n", *(arr + 5));
+	return 0;
+//p[5]==*(p+5)
+//arr[5]==*(arr+5)==*(5+arr)==5[arr]
+}
+
+
+#include<stdio.h>
+struct Stu
+{
+	char name[20];
+	int age;
+	char sex[5];
+};
+int main()
+{
+	struct Stu s = { "张三", 30, "男" };
+	printf("%s\n", s.name);
+	printf("%d\n", s.age);
+	printf("%s\n", s.sex);
+	return 0;
+}
+
+
+#include<stdio.h>
+struct Stu
+{
+	char name[20];
+	int age;
+	char sex[5];
+};
+int main()
+{
+	struct Stu s = { "张三", 30, "男" };
+	struct Stu* ps = &s;
+	printf("%s\n", ps->name);
+	printf("%d\n", ps->age);
+	printf("%s\n", ps->sex);
+	return 0;
+}
