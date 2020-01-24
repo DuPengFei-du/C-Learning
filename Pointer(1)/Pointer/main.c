91831ad2bbe128185457252126c414d72f63ce47
DuPengFei-du <935820240@qq.com>
2020-01-24 09:37:27 +0800

    test

---
+++ Pointer(1)/Pointer/main.c
@@ -0,0 +1,143 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+//指针
+#include<stdio.h>
+int main()
+{
+	int a = 10;  //在内存中开辟一个空间
+	int* p = &a; //这里我们对变量a取出他的地址，可以使用&操作符
+	//将a的地址存在变量p中，p就是一个指针变量。
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int* p = arr;  //四个字节四个字节去改变
+	for (int i = 0; i < 10; i++)
+	{
+		*(p + i) = 0;
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	char* p = arr;  //一个字节一个字节去改变
+	for (int i = 0; i < 10; i++)
+	{
+		*(p + i) = 0;
+	}
+	return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int* pa = &a;
+	char* pc = &a;
+	printf("%p\n", pa);
+	printf("%p\n", pc);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	printf("%p\n", arr);
+	printf("%p\n", &arr[0]);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	double d[10] = { 0 };
+	double* pd = d;
+	printf("%p\n", pd);
+	printf("%p\n", pd+4);
+	//加4实际上是加了32，因为4*8=32
+	return 0;
+}
+
+
+#include<stdio.h>
+#define a 5
+int main()
+{
+	float values[a];
+	float* vp;
+	for (vp = &values[0]; vp < &values[a];)
+	{
+		*vp++ = 0;
+	}
+	return 0;
+}
+//功能是将前五个元素的值都变成0
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	printf("%d\n", &arr[9] - &arr[0]);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	printf("%d\n", &arr[0] - &arr[9]);
+	return 0;
+}
+
+
+#include<stdio.h>
+int my_strlen(char* str)
+{
+	int count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+int main()
+{
+	char arr[] = "Hello World";
+	int len = my_strlen(arr);
+	printf("%d", len);
+	return 0;
+}
+
+
+//递归写法
+#include<stdio.h>
+int my_strlen(char* str)
+{
+	if (*str == '\0')
+		return 0;
+	else
+		return 1 + my_strlen(str + 1);
+}
+int main()
+{
+	char arr[] = "Hello World";
+	int len = my_strlen(arr);
+	printf("%d", len);
+	return 0;
+}
