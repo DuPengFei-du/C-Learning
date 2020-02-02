03c16637a88178ff4f9c1a6a867ba12bb67a3f4f
DuPengFei-du <935820240@qq.com>
2020-02-02 11:15:47 +0800

    test

---
+++ Pointer Promote(2)/Pointer Promote(2)/main.c
@@ -0,0 +1,132 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//一级指针传参
+#include<stdio.h>
+void Print(int* p, int sz)
+{
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d ", *(p + i));
+	}
+}
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int* p = arr;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	Print(p, sz);
+	return 0;
+}
+
+
+//二级指针传参
+#include<stdio.h>
+void test(int** ptr)
+{
+
+}
+int main()
+{
+	int n = 10;
+	int* p = &n;
+	int** pp = &p;
+	test(pp);
+	test(&p);
+	return 0;
+}
+
+
+//函数指针
+//把函数的地址取出来放在指针里
+#include<stdio.h>
+int Add(int x, int y)
+{
+	return x + y;
+}
+int main()
+{
+	printf("%p\n", &Add);   //证实了函数也是有地址的
+	int (*p)(int x, int y) = &Add;   //p是函数指针
+	int ret =(*p)(2, 3);   //函数的调用
+	//(*p)是先找到这个函数，然后传参进行调用
+	printf("%d", ret);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	void (*pfun1)();   //函数指针 ，函数无参，返回值类型为void类型
+	void* pfun2();    //函数声明，函数无参，函数的返回值类型为void*类型
+}
+
+
+//两个有趣的代码
+#include<stdio.h>
+int main()
+{
+	//代码1
+	(*(void(*)())0)();
+
+	/*
+	首先，void(*)()为函数指针类型，其次void(*)()内放在括号中
+	形成了(void(*)()),我们都知道，一个类型放在括号中，是用来
+	强制类型转换的，(void(*)())又放在0前面，意思就是吧0转换成
+	函数指针类型，0本来的类型是int，我希望把0当成函数指针类型的这样的
+	一个函数的地址，所以我希望0这个地址处放的是一个无参，返回值类型
+	为void的函数，然后，既然是个地址，那么我就解引用一下
+	所以综上，上述是一场函数调用。
+	调用0位置处的函数，被调用的这个函数是无参的，返回值类型为void的类型
+	*/
+
+	//代码2
+	void (*signal(int, void(*)(int)))(int);
+	/*
+	这段代码整体来看是一个函数声明，其中signal为函数名，signal函数的第一个参数
+	为int类型，第二个参数类型为函数指针类型，有了函数名，有了参数
+	剩下的就是函数的返回值类型了，这个函数的返回值类型也是一个函数指针类型
+	*/
+	/*
+	但是又因为这个代码出现了两次函数指针类型，所以我们可以用typedef进行
+	类型重命名，即：
+	typedef void(*pfun_t)(int);
+	所以上述声明也可以变成
+	pfun_t signal(int,pfun_t);
+	*/
+	}
+
+
+//函数指针数组
+#include<stdio.h>
+int my_strlen(const char* str)
+{
+
+}
+int main()
+{
+	int* arr[10];  //整形指针数组
+	char* arr[5];  //字符指针数组
+	int (*ps)(const char*) = &my_strlen;
+	int (*ps2)(const char*) = &my_strlen;
+	int (*ps3)(const char*) = &my_strlen;
+	int (*pArr[3])(const char*) = &my_strlen;   //函数指针数组
+	//存放函数指针的数组
+	return 0;
+}
+
+
+//指向函数指针数组的指针
+int Add(int x, int y)
+{
+	return x + y;
+}
+int Sub(int x, int y)
+{
+	return x - y;
+}
+int (*p)(int, int);   //函数指针
+int (*parray[2])(int, int);  //函数指针数组
+int(*(*ptr)[2])(int, int) = &parray;//指向函数指针数组的指针
+//ptr是指针
