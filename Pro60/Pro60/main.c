8cd4712faf4b04681b8eb22da12959ecb2079e7a
DuPengFei-du <935820240@qq.com>
2020-02-13 14:52:09 +0800

    test

---
+++ Pro60/Pro60/main.c
@@ -0,0 +1,204 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int j = 0;
+	int arr[10] = { 1,3,5,7,9,2,4,6,8,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (i = 0; i < sz - i ; i++)
+	{
+		for (j = 0; j < sz - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int key = 8;
+	int left = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int right = sz - 1;
+	while (left <= right)
+	{
+		int mid = left + (right - left) / 2;
+		if (arr[mid] > key)
+			right = mid - 1;
+		else if (arr[mid] < key)
+			left = mid + 1;
+		else
+		{
+			printf("找到了，下标为:%d", mid);
+			break;
+		}
+	}
+	return 0;
+}
+
+
+//不创建临时变量，交换两个数字的值
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = a << 1;
+	//<<左移操作符
+	//左边丢弃，右边补0
+	printf("%d\n", b);
+	printf("%d\n", a);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = -1;
+	int b = a << 1;
+	//整数在内存中存储的是二进制补码
+	//-1的在内存中存储，就是存储32个1，32个1为-1的补码。
+	//正数的原，反，补码相同
+	//负数：写先原码，最高位为符号位，负数的最高位为1，负数的符号位为1，正数为0
+	//反码：在原码的基础上，符号位不动，其他位按位取反，得到反码
+	//反码+1得到补码
+	printf("%d\n", b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = -1;
+	int b = a >> 1;
+	printf("%d\n", b);
+	return 0;
+	//采用的是算数右移
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	printf("%p\n", &a);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int* p = &a;
+	printf("%d\n", a);
+	return 0;
+	//*p其实就是a
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int temp = 0;
+	(void)scanf("%d %d", &a, &b);
+	temp = a;
+	a = b;
+	b = temp;
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+void Swap(int* x, int* y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	Swap(&a, &b);
+	printf("%d %d", a, b);
+	return 0;
+}
