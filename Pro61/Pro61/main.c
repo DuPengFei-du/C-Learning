c9cf2131bb79bd3fcca5cc2015e4246af678e946
DuPengFei-du <935820240@qq.com>
2020-02-14 15:45:29 +0800

    test

--- Pro61/Pro61/main.c
+++ Pro61/Pro61/main.c
@@ -110,3 +110,79 @@
 //	}
 //	return 0;
 //}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;  //在内存中开辟一个空间
+	int* p = &a; //这里我们对变量a取出他的地址，可以使用&操作符
+	//将a的地址存在变量p中，p就是一个指针变量。
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int* p = arr;  //四个字节四个字节去改变
+	for (int i = 0; i < 10; i++)
+	{
+		*(p + i) = 0;
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int* pa = &a;
+	char* pc = &a;
+	printf("%p\n", pa);
+	printf("%p\n", pc);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	printf("%p\n", arr);
+	printf("%p\n", &arr[0]);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	double d[10] = { 0 };
+	double* pd = d;
+	printf("%p\n", pd);
+	printf("%p\n", pd + 4);
+	//加4实际上是加了32，因为4*8=32
+	return 0;
+}
+
+
+#include<stdio.h>
+int my_strlen(char* str)
+{
+	char* start = str;
+	while (*str != '\0')
+	{
+		str++;
+	}
+	return str - start;
+}
+int main()
+{
+	char arr[] = "Hello World";
+	int len = my_strlen(arr);
+	printf("%d\n", len);
+	return 0;
+}
