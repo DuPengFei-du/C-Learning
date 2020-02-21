6b7b5cdd04226d3c90e29f8f2b9bb8f1b6abe7e8
DuPengFei-du <935820240@qq.com>
2020-02-21 08:53:41 +0800

    test

---
+++ Pro67/Pro67/main.c
@@ -0,0 +1,213 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = { 'a','b','c' };
+	char arr2[] = "abc";
+	printf("%s", arr1);
+	printf("%s", arr2);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	(void)scanf("%d", &n);
+	int count = 0;
+	for (int i = 0; i < 32; i++)
+	{
+		if (((n >> i) & 1) == 1)
+			count++;
+	}
+	printf("%d", count);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int num = 0;
+	int count = 0;
+	(void)scanf("%d", &num);
+	while (num)
+	{
+		if (num % 2 == 1)
+			count++;
+		num = num / 2;
+	}
+	printf("%d", count);
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = a << 1;
+	//<<左移操作符
+	//左边丢弃，右边补0
+	printf("%d\n", b);
+	printf("%d\n", a);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = -1;
+	int b = a << 1;
+	//整数在内存中存储的是二进制补码
+	//-1的在内存中存储，就是存储32个1，32个1为-1的补码。
+	//正数的原，反，补码相同
+	//负数：写先原码，最高位为符号位，负数的最高位为1，负数的符号位为1，正数为0
+	//反码：在原码的基础上，符号位不动，其他位按位取反，得到反码
+	//反码+1得到补码
+	printf("%d\n", b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = -1;
+	int b = a >> 1;
+	printf("%d\n", b);
+	return 0;
+	//采用的是算数右移
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = !a;
+	printf("%d\n", b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	printf("%d\n", sizeof(a));  //4
+	printf("%d\n", sizeof(int));   //4
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	short a = 10;
+	int b = 3;
+	printf("%d\n", sizeof(a = b + 5));  //2
+	printf("%d\n", a);         //10
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr1[] = "abcdef";
+	if (arr1 == "abcdef")
+	{
+		printf("hehe\n");
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int* p = arr;  //四个字节四个字节去改变
+	for (int i = 0; i < 10; i++)
+	{
+		*(p + i) = 0;
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	printf("%p\n", arr);
+	printf("%p\n", &arr[0]);
+	return 0;
+}
+
+
+#include<stdio.h>
+struct Stu
+{
+	//成员变量
+	//成员列表
+	char name[20];
+	short age;
+	char sex[5];
+	float score;
+}s1, s2, s3;  //学生s1,s2,s3   //创建了三个学生出来  //创建的是全局变量
+int main()
+{
+	struct Stu s;
+	return 0;
+}
+
+
+#include<stdio.h>
+struct Stu
+{
+	//成员变量
+	//成员列表
+	char name[20];
+	short age;
+	char sex[5];
+	float score;
+};
+int main()
+{
+	struct Stu s;
+	return 0;
+}
