6cb797628e066e6220ba8476864e0f8ccac747e6
DuPengFei-du <935820240@qq.com>
2020-02-22 10:38:56 +0800

    test

---
+++ Pro69/Pro69/main.c
@@ -0,0 +1,258 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+#include<stdio.h>
+int main()
+{
+	printf("%d\n", sizeof(int));  //4
+	printf("%d\n", sizeof(char)); //1
+	printf("%d\n", sizeof(short)); //2
+	printf("%d\n", sizeof(long));  //4/8
+	printf("%d\n", sizeof(long long));  //8
+	printf("%d\n", sizeof(float));  //4
+	printf("%d\n", sizeof(double));  //8
+	return 0;
+}
+//类型的大小单位都是字节
+//计算机中的单位 
+//bit(位) byte(字节) kbyte(kb) mb gb tb pb  
+//前两个单位之间的换算单位是8，后面的单位之间的是1024
+
+
+#include<stdio.h>
+int main()
+{
+	{
+		int num = 10;
+	}
+	//printf("%d", num);
+	return 0;
+	//此时，这个程序会出错，num作用域中不包括有printf语句。
+}
+
+
+#include<stdio.h>
+int main()
+{
+	printf("%c\n",'\'');
+	printf("%s\n","\"");
+	return 0;
+}
+
+
+#include<stdio.h>
+int Add(int x, int y)
+{
+	return x + y;
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	(void)scanf("%d %d", &a, &b);
+	c = Add(a, b);
+	printf("%d", c);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 5 % 2;
+	printf("%d", a);
+	return 0;
+}
+//针对于取模运算，有一点需要特别注意：
+//取模运算符左右两边的操作数都必须是整数，如果不是整数，那么就会出错。
+
+
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	//int a = 5/2;
+	//int a = 5/2.0;
+	//printf("%d\n", a);
+	//int a = 5%2;
+	//printf("%d\n", a);
+	int a = 15;
+	int b = a << 1;
+	//a = a<<1;
+	printf("%d\n", a);
+	printf("%d\n", b);
+	//<< 移位操作符-移动的是二进制位
+	//原码-反码-补码
+	//00000000000000000000000000001111-原码-反码-补码相同
+	//00000000000000000000000000011110
+	//
+	//-1
+	//10000000000000000000000000000001-原码
+	//11111111111111111111111111111110-反码
+	//11111111111111111111111111111111-补码
+	system("pause");
+	return 0;
+}
+//详细还是看笔记本上相关内容
+
+
+#include<stdio.h>
+int main()
+{
+	int a = (1, 2, 3, 4, 5);
+	printf("%d\n", a);
+	return 0;
+}
+
+
+#include<stdio.h>
+//自定义类型
+struct Stu
+{
+	char name[20];   //一个汉字是两个字符
+	int age;
+	float score;
+};
+int main()
+{
+	struct Stu s = { "张三",20,60.0f };
+	struct Stu* ps = &s;  //结构体指针
+	printf("%s\n%d\n%f\n", (*ps).name, (*ps).age, (*ps).score);
+	return 0;
+}
+
+
+//悬挂else问题
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 2;
+	if (a == 1)
+		if (b == 2)
+			printf("hehe\n");
+		else
+			printf("haha\n");
+	return 0;
+}
+//这段代码什么都不会打印 第二个else会在编译器中主动和第二个if靠近
+//所以代码什么都不会打印，没有符号的条件。
+
+
+//判断奇数还是偶数
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	(void)scanf("%d", &n);
+	if (n % 2 == 0)
+		printf("偶数");
+	else
+		printf("奇数");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 1;
+	int m = 2;
+	switch (n)
+	{
+	case 1:m++;
+	case 2:n++;
+	case 3:
+		switch (n)
+		{//switch允许嵌套使用
+		case 1:	n++;
+		case 2:m++; n++;	break;
+		}
+	case 4:m++; break;
+	default:
+		break;
+	}
+	printf("m = %d\nn = %d\n", m, n);
+	return 0;
+}
+//结果是m=5，n=3；
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		printf("%d ", i);
+		i++;
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		if (i == 5)
+			break;
+		printf("%d ", i);
+		i++;
+	}
+	return 0;
+}
+//打印结果为1，2，3，4
+//break永久的跳出循环，停止后期的所有循环，永久的终止循环
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		if (i == 5)
+			continue;
+		printf("%d ", i);
+		i++;
+	}
+	return 0;
+}
+//打印结果是1，2，3，4，4，4，4，4...死循环
+
+
+//最大公约数
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 1;
+	(void)scanf("%d %d", &a, &b);
+	while (c = a % b)
+	{
+		a = b;
+		b = c;
+	}
+	printf("%d", b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+	printf("%d %d", a, b);
+	return 0;
+}
