20f526fd88190b4e7628512c503797a869e8de3e
DuPengFei-du <935820240@qq.com>
2020-02-24 07:55:23 +0800

    test

---
+++ c41/c41/main.c
@@ -0,0 +1,190 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = a << 1;
+	//<<左移操作符
+	//左边丢弃，右边补0
+	printf("%d\n", b);
+	printf("%d\n", a);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = -1;
+	int b = a << 1;
+	//整数在内存中存储的是二进制补码
+	//-1的在内存中存储，就是存储32个1，32个1为-1的补码。
+	//正数的原，反，补码相同
+	//负数：写先原码，最高位为符号位，负数的最高位为1，负数的符号位为1，正数为0
+	//反码：在原码的基础上，符号位不动，其他位按位取反，得到反码
+	//反码+1得到补码
+	printf("%d\n", b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = -1;
+	int b = a >> 1;
+	printf("%d\n", b);
+	return 0;
+	//采用的是算数右移
+}
+
+
+int num = 10;
+//num >> -1;  //error
+//移动负数位的标准并没有定义
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int count = 0;
+	(void)scanf("%d", &n);
+	for (int i = 0; i < 32; i++)
+	{
+		if (((n >> i) & 1) == 1)
+			count++;
+	}
+	printf("%d", count);
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int count = 0;
+	(void)scanf("%d", &n);
+	while (n)
+	{
+		if (n % 2 == 1)
+			count++;
+		n = n / 2;
+	}
+	printf("%d", count);
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int count = 0;
+	(void)scanf("%d", &n);
+	while (n)
+	{
+		count++;
+		n = n & (n - 1);
+	}
+	printf("%d", count);
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = !a;
+	printf("%d\n", b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 3;
+	int b = -a;
+	printf("%d\n", b);
+	return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	printf("%p\n", arr);      //首元素的地址
+	printf("%p\n", &arr[0]); //首元素的地址
+	printf("%p\n", &arr);    //数组的地址
+	return 0;
+}
+//三个的值一样
+
+
+#include<stdio.h>
+int main()
+{
+	short a = 10;
+	int b = 3;
+	printf("%d\n", sizeof(a = b + 5));  //2
+	printf("%d\n", a);         //10
+	return 0;
+}
+//值得需要注意的是：
+//sizeof是运算符，并不是函数。
+//所以sizeof括号中的内容只能算的上是传参，括号中的表达式是不会被计算的。
+//所以a的值是并没有发生改变的。
+//把int类型的东西非要放在short类型中，那当然是short说了算的，所以sizeof算出来的字节数的结果是2
+
+
+#include <stdio.h>
+void test1(int arr[])
+{
+	printf("%d\n", sizeof(arr));//(2)   //用指针接收的，大小为4
+}
+void test2(char ch[])
+{
+	printf("%d\n", sizeof(ch));//(4)  //这里是一个易错的点，他是用指针接收的
+	//所以大小还是4，与char是没有任何关系的
+}
+int main()
+{
+	int arr[10] = { 0 };
+	char ch[10] = { 0 };
+	printf("%d\n", sizeof(arr));//(1)
+	printf("%d\n", sizeof(ch));//(3)
+	test1(arr);
+	test2(ch);
+	return 0;
+}
