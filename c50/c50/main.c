91d91417d9efceac9ce8ab8aa0d96c28811ae82a
DuPengFei-du <935820240@qq.com>
2020-03-02 22:02:42 +0800

    test

---
+++ c50/c50/main.c
@@ -0,0 +1,202 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+//猜数字游戏的实现
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+void game()
+{
+	int guess = 0;
+	int ret = 0;
+	ret = rand() % 100 + 1;
+	while (1)
+	{
+		printf("请输入:>\n");
+		(void)scanf("%d", &guess);
+		if (guess > ret)
+			printf("猜大了\n");
+		else if (guess < ret)
+			printf("猜小了\n");
+		else
+		{
+			printf("猜对了\n");
+			break;
+		}
+	}
+}
+void menu()
+{
+	printf("*****************************\n");
+	printf("     欢迎来到猜数字游戏！    \n");
+	printf("           1.play            \n");
+	printf("           0.exit            \n");
+	printf("*****************************\n");
+}
+void test()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do
+	{
+		menu();
+		printf("请选择:>\n");
+		(void)scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏!\n");
+			break;
+		default:
+			printf("输入错误，请重新输入!\n");
+			break;
+		}
+	} while (input);
+}
+int main()
+{
+	test();
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	//数组也是一种自定义类型
+	int arr[10] = { 0 };
+	printf("%d\n", sizeof(arr));                //40
+	printf("%d\n", sizeof(int[10]));          //40
+	int a = 10;
+	sizeof(int);  //可以的
+	sizeof(a);    //可以的
+	//那么问题来了，数组的类型是什么呢？
+	//数组的类型，就是数组去掉数组名剩下的部分
+	//比如上述对数组的声明 int arr[10],那么数组的类型就是int [10]
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0x11223344;
+	//0x11223344放在a里面是刚刚好的
+	//因为11是一个字节，22是一个字节，33是一个字节，44是一个字节
+	//通过监视看：转换到16进制，观察到a的值为0x1223344
+	//通过内存看：输入&a，可以观察到，a显示为44332211
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = -10;
+	//10000000000000000000000000001010  --原码
+	//11111111111111111111111111110101  --反码
+	//11111111111111111111111111110110  --补码
+	//fffffff6               ---补码转换成16进制的样子
+	//但是在内存中&a显示的结果是f6ffffff
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	if (*pa == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	return *pa;
+}
+int main()
+{
+	int ret = CheckSystem();
+	if (ret == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char a = 'w';
+	char* pc = &a;   //这个时候，pc称为字符指针，pc的类型为char*
+	char arr[10] = "abc";
+	char* pa = arr;   //arr作为数组名，他代表的是数组首元素的地址，pa的类型为char*
+	char* p = "abcdef";  //此时p只具有4个字节，但是abcdef不算\0有6个字节，显然放不下
+	//那么此时，p中存的就是"abcdef"中首个字母a的地址，地址用指针接收，刚好为4个字节
+	//上述常量表达式的结果就是首字符a的地址。
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char* p1 = "abcdef";
+	char* p2 = "abcdef";
+	if (p1 == p2)
+		printf("p1==p2");
+	else
+		printf("p1!=p2");
+	return 0;
+	//打印结果为p1==p2
+	//虽然把字符串abcdef地址存在了p1和p2当中，但是两个字符串是一样的
+	//都是abcdef，所以首元素a的地址自然也是一样的了
+	//abcdef为常量字符串，所以说他是不能被改变的，其实也只会出现一次abcdef
+	//但是p1的地址和p2的地址是绝对不一样的。
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = "abcdef";
+	//创建了一个数组，数组中存的是abcdef
+	const char* p = "abcdef";
+	//将字符串abcdef中首元素的地址，即a的地址，存在p中
+	arr1[2] = 'w';
+	//修改数组中的内容，是被允许的
+	//*(p + 2) = 'w';        //p[2]
+	//p所指向的是一个常量字符串，常量字符串中的值是不可以被修改的
+	return 0;
+	//这个程序运行起来是会崩溃的，崩溃的主要原因在于，*(p+2)='w'
+	//这个语句是不正确的
+	//字符指针不仅仅用来存放字符的地址，还可能存放一个字符串的起始地址
+}
+
+
+#include<stdio.h>
+void Print(int* p, int sz)
+{
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d ", *(p + i));
+	}
+}
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int* p = arr;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	Print(p, sz);
+	return 0;
+}
