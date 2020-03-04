4d576aa94eb98369bed15eaca8773d237435d43a
DuPengFei-du <935820240@qq.com>
2020-03-04 08:40:44 +0800

    test

---
+++ c52/c52/main.c
@@ -0,0 +1,205 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include<stdio.h>
+int main()
+{
+	//在屏幕上打印打引号和双引号
+	printf("%c\n", '\'');
+	printf("%s\n", "\"");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int c = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	c = a + b;
+	printf("%d ", c);
+	return 0;
+}
+
+
+#include<stdio.h>
+int Add(int x, int y)
+{
+	return x + y;
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	(void)scanf("%d %d", &a, &b);
+	c = Add(a, b);
+	printf("%d", c);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int i = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for(i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int num = 0;
+	(void)scanf("%d", &num);
+	printf("%d", num);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	printf("%d\n", sizeof(int));  //4
+	printf("%d\n", sizeof(char)); //1
+	printf("%d\n", sizeof(short)); //2
+	printf("%d\n", sizeof(long));  //4/8
+	printf("%d\n", sizeof(long long));  //8
+	printf("%d\n", sizeof(float));  //4
+	printf("%d\n", sizeof(double));  //8
+	return 0;
+}
+//全局变量的作用域是整个函数的作用域
+
+
+//enum Sex
+//{
+//	MALE,
+//	FEMALE,
+//	SECRET
+//};
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = "abc";
+	char arr2[] = { 'a','b','c',0 };
+	printf("%s\n", "abc");
+	printf("%s\n", arr1);
+	printf("%s\n", arr2);
+	return 0;
+	//三种打印的结果都是abc
+	//如果arr2后面不加0的话，那么打印结果就会是再abc的后面还会出现烫烫烫的字符
+	//因为如果不加0的话，字符串就会并没有结束，只有加了0或者\0才会停止打印
+	//烫烫烫背后的真实东西其实是0xcccccc
+	//由此可以看来\0对字符串来说是非常重要的
+	//字符串是\0需要的结束标志的符号；
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	while (i < 10)
+	{
+		printf("%d ", arr[i]);
+		i++;
+	}
+	printf("\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a1 = 5 / 2;
+	float a2 = 5 / 2;
+	float a3 = 5.0 / 2;
+	printf("%d\n", a1);
+	printf("%f\n", a2);
+	printf("%f\n", a3);
+	return 0;
+}
+//针对于除法操作，与他存储的是什么类型是没有关系的
+//主要是看两个除法的操作数到底是什么类型的，如果有一个是浮点型的，得到的结果就会是浮点型的结果
+
+
+#include<stdio.h>
+int main()
+{
+	//int a = 5/2;
+	//int a = 5/2.0;
+	//printf("%d\n", a);
+	//int a = 5%2;
+	//printf("%d\n", a);
+	int a = 15;
+	int b = a << 1;
+	//a = a<<1;
+	printf("%d\n", a);
+	printf("%d\n", b);
+	//<< 移位操作符-移动的是二进制位
+	//原码-反码-补码
+	//00000000000000000000000000001111-原码-反码-补码相同
+	//00000000000000000000000000011110
+	//
+	//-1
+	//10000000000000000000000000000001-原码
+	//11111111111111111111111111111110-反码
+	//11111111111111111111111111111111-补码
+	system("pause");
+	return 0;
+}
+//详细还是看笔记本上相关内容
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int max = 0;
+	max = (a > b) ? a : b;
+	printf("%d", max);
+	return 0;
+}
+
+
+#include<stdio.h>
+#define ADD(X,Y) X+Y
+int main()
+{
+	printf("%d\n", 10 * ADD(2, 3));
+	return 0;
+}
+//宏的替换是直接替换的，不经运算，直接替换。
+//结果为23
+
+
+#include<stdio.h>
+//自定义类型
+struct Stu
+{
+	char name[20];   //一个汉字是两个字符
+	int age;
+	float score;
+};
+int main()
+{
+	struct Stu s = { "张三",20,60.0f };
+	printf("name :%s\n", s.name);
+	printf("age:%d\n", s.age);
+	printf("score :%f\n", s.score);
+	return 0;
+}
