37be91817b0aa8a16bba80734896616122ab7cab
DuPengFei-du <935820240@qq.com>
2020-03-16 08:20:54 +0800

    test

---
+++ c63/c63/main.c
@@ -0,0 +1,226 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+#include<stdio.h>
+int main()
+{
+	//数组也是一种自定义类型
+	int arr[10] = { 0 };
+	printf("%d\n", sizeof(arr));                //40
+	printf("%d\n", sizeof(int[10]));          //40
+	int a = 10;
+	sizeof(int);  //可以的
+	sizeof(a);    //可以的
+	//那么问题来了，数组的类型是什么呢？
+	//数组的类型，就是数组去掉数组名剩下的部分
+	//比如上述对数组的声明 int arr[10],那么数组的类型就是int [10]
+}
+//数组的类型是去掉数组名的部分
+
+
+#include<stdio.h>
+void test()
+{
+	printf("hehe");
+}
+int main()
+{
+	test(20);
+	return 0;
+}
+//屏幕上打印的结果是hehe，虽然test函数实际上是没有参数的，但是在主函数调用test函数时候，还是传了参数，但是是没有任何影响的。
+//你传是你的事情，我接不接受是我的事情。
+//但是如果没有参数的话，最好还是在test函数的括号中加一个void，声明这个函数是没有参数的。
+
+
+#include<stdio.h>
+int main()
+{
+	int a = -10;
+	//10000000000000000000000000001010  --原码
+	//11111111111111111111111111110101  --反码
+	//11111111111111111111111111110110  --补码
+	//fffffff6               ---补码转换成16进制的样子
+	//但是在内存中&a显示的结果是f6ffffff
+}
+//在内存中的存储存在有大小端的问题
+
+
+//大小端问题
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	if (*pa == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	return *pa;
+}
+int main()
+{
+	int ret = CheckSystem();
+	if (ret == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	union Un
+	{
+		int i;
+		char c;
+	}u;
+	u.i = 1;
+	return u.c;
+}
+int main()
+{
+	int ret = CheckSystem();
+	if (ret == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char a = 'w';
+	char* pc = &a;   //这个时候，pc称为字符指针，pc的类型为char*
+	char arr[10] = "abc";
+	char* pa = arr;   //arr作为数组名，他代表的是数组首元素的地址，pa的类型为char*
+	char* p = "abcdef";  //此时p只具有4个字节，但是abcdef不算\0有6个字节，显然放不下
+	//那么此时，p中存的就是"abcdef"中首个字母a的地址，地址用指针接收，刚好为4个字节
+	//上述常量表达式的结果就是首字符a的地址。
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = "abcdef";
+	char arr2[] = "abcdef";
+	if (arr1 == arr2)
+		printf("arr1==arr2\n");
+	else
+		printf("arr1!=arr2\n");
+	return 0;
+	//打印结果为arr1!=arr2
+	//原因:因为arr1和arr2会开辟两块空间的大小，虽然都表示的是首字母a的地址
+	//但是两块空间的地址是不一样的，所以两块空间中a的地址自然也就是不一样的了。
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char* p1 = "abcdef";
+	char* p2 = "abcdef";
+	if (p1 == p2)
+		printf("p1==p2");
+	else
+		printf("p1!=p2");
+	return 0;
+	//打印结果为p1==p2
+	//虽然把字符串abcdef地址存在了p1和p2当中，但是两个字符串是一样的
+	//都是abcdef，所以首元素a的地址自然也是一样的了
+	//abcdef为常量字符串，所以说他是不能被改变的，其实也只会出现一次abcdef
+	//但是p1的地址和p2的地址是绝对不一样的。
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = "abcdef";
+	//创建了一个数组，数组中存的是abcdef
+	const char* p = "abcdef";
+	//将字符串abcdef中首元素的地址，即a的地址，存在p中
+	arr1[2] = 'w';
+	//修改数组中的内容，是被允许的
+	//*(p + 2) = 'w';        //p[2]
+	//p所指向的是一个常量字符串，常量字符串中的值是不可以被修改的
+	return 0;
+	//这个程序运行起来是会崩溃的，崩溃的主要原因在于，*(p+2)='w'
+	//这个语句是不正确的
+	//字符指针不仅仅用来存放字符的地址，还可能存放一个字符串的起始地址
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	char c = 'w';
+	int* p1 = &a;
+	char* p2 = &c;
+	int arr[10] = { 0 };
+	char ch[5] = { 0 };
+	char* arr2[6] = { 0 };
+	//数组指针就是指向数组的指针
+	//arr  表示数组首元素的地址
+	//&arr[0]表示数组首元素的地址
+	//&arr 表示数组的地址
+	int(*p)[10] = &arr;  //数组的地址 
+	char(*pc)[5] = &ch;
+	char* (*pa)[6] = &arr2;
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[5];          //整形数组
+	int* parr1[10];      //指针数组
+	int(*parr2)[10];     //数组指针
+	int(*parr3[10])[5];  //存放数组指针的数组，可以存放10个这样的指针
+	//数组有10个对象，数组中存储对象的类型是数组指针
+	//5个整形元素的数组的地址 数组中每个元素的类型是int
+}
+
+
+#include<stdio.h>
+void test(int arr[3][5])
+{
+
+}
+//void test(int arr[][])
+//{
+//	//这种传参是不正确的，可以省略行，不可以省略列
+//}
+void test(int arr[][5])
+{
+
+}
+//总结:二维数组传参，函数形式的设计只能省略第一维的[],不能省略第二维的
+//因为对一个二维数组，可以不知道有多少行，但是必须知道一行有多少个元素
+//这样才会方便运算，同样，一样的道理，如果用数组接收参数，那么就原模原样的接收就可以了
+void test(int(*arr)[5])
+{
+
+}
+int main()
+{
+	int arr[3][5];
+	test(arr);
+}
