99dd33e0cc1ff7110db8ee20861ee0cad1d6e7d8
DuPengFei-du <935820240@qq.com>
2020-03-28 08:58:41 +0800

    test

---
+++ c75/c75/main.c
@@ -0,0 +1,292 @@
+#define _CRT_SECURE_NO_WARNINS 1
+
+
+#include<stdio.h>
+int main()
+{
+	//数组也是一种自定义类型
+	int arr[10] = { 0 };
+	printf("%d\n", sizeof(arr));                //40
+	printf("%d\n", sizeof(int[10]));          //40
+	int a = 10;
+	sizeof(int);  //可以的
+	sizeof(a);    //可以的
+	//那么问题来了，数组的类型是什么呢？
+	//数组的类型，就是数组去掉数组名剩下的部分
+	//比如上述对数组的声明 int arr[10],那么数组的类型就是int [10]
+}
+
+
+#include<stdio.h>
+void test()
+{
+	printf("hehe");
+}
+int main()
+{
+	test(20);
+	return 0;
+}
+//屏幕上打印的结果是hehe，虽然test函数实际上是没有参数的，但是在主函数调用test函数时候，还是传了参数，但是是没有任何影响的。
+//你传是你的事情，我接不接受是我的事情。
+//但是如果没有参数的话，最好还是在test函数的括号中加一个void，声明这个函数是没有参数的。
+
+
+#include<stdio.h>
+int main()
+{
+	//加入说我想算一个1-1的结果是多少
+	//但是遗憾的是，计算机不能算减法
+	//那我只好把减法转换为加法的形式
+	//1 - 1;
+	1 + (-1);
+	//如果按原码来计算的话
+	// 1的原码
+	//00000000000000000000000000000001
+	//-1的原码
+	//10000000000000000000000000000001
+	//两个码相加的结果为
+	//10000000000000000000000000000010
+	//如果按照原码直接相加的话，结果就为-2，这显然是不正确的
+	//所以科学家们推出了补码的概念
+	//用补码来算的时候，可以算出正确的结果
+}
+
+
+//解题思路：
+//我们可以通过把1存在内存中，然后通过看他的存储形式，我们来判断当前机器到底是大端还是小端
+//1的16形式的格式为00000001
+//如果存储形式是01000000那么就是小端
+//如果存储形式是00000001那么就是大段
+//我们可以通过读取第一个字节的内容，来判断到底是大端还是小端，想要读取一个整形的第一个字节，那么存储在char*类型的变量中就可以了。
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	if (*pa == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+//封装成一个函数
+#include<stdio.h>
+int check_system()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	if (*pa == 1)
+		return 1;
+	else
+		return 0;
+}
+int main()
+{
+	int ret = check_system();
+	if (ret == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+struct Stu
+{
+	char name[20];
+	int age;
+};
+union Un
+{
+	int i;
+	char c;
+};    //联合体类型
+int main()
+{
+	union Un u = { 0 };   //u为联合体对象
+	printf("%d\n", sizeof(u));   //结果为4
+	printf("%p\n", &u);
+	printf("%p\n", &(u.i));
+	printf("%p\n", &(u.c));
+	//三个的地址是一模一样的，说明i和c占用同一块空间
+	//所以联合体又叫共用体，共用体的意思就是我的成员要共用同一块空间
+	//那么我给i里面放东西c就会发生变化
+	//我给c里面放东西，i也会发生变化
+	//c是i占的四个字节中的第一个字节，这也解决了大小端算法的另一种解法问题
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char a[1000];
+	int i;
+	for (i = 0; i < 1000; i++)
+	{
+		a[i] = -1 - i;
+	}
+	printf("%d", strlen(a));
+	//什么时候遇到\0，长度就是多少，参考char的范围的哪个圆圈
+	//所以结果为255
+	return 0;
+}
+
+
+#include<stdio.h>
+unsigned char i = 0;
+int main()
+{
+	for (i = 0; i <= 255; i++)
+	{
+		printf("hello world\n");
+	}
+	return 0;
+	//这个程序会死循环
+	//unsigned char 的范围就是0-255，所以会死循环
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char* p1 = "abcdef";
+	char* p2 = "abcdef";
+	if (p1 == p2)
+		printf("p1==p2");
+	else
+		printf("p1!=p2");
+	return 0;
+	//打印结果为p1==p2
+	//虽然把字符串abcdef地址存在了p1和p2当中，但是两个字符串是一样的
+	//都是abcdef，所以首元素a的地址自然也是一样的了
+	//abcdef为常量字符串，所以说他是不能被改变的，其实也只会出现一次abcdef
+	//但是p1的地址和p2的地址是绝对不一样的。
+}
+
+
+#include<stdio.h>
+void menu()
+{
+	printf("**********************\n");
+	printf("*** 欢迎来到计算器 ***\n");
+	printf("***  1.Add   2.Sub ***\n");
+	printf("***  3.Mul   4.Div ***\n");
+	printf("**********************\n");
+}
+int Add(int x, int y)
+{
+	return x + y;
+}
+int Sub(int x, int y)
+{
+	return x - y;
+}
+int Mul(int x, int y)
+{
+	return x * y;
+}
+int Div(int x, int y)
+{
+	return x / y;
+}
+void test()
+{
+	int input = 0;
+	int x = 0;
+	int y = 0;
+	do
+	{
+		menu();
+		printf("请选择:>");
+		(void)scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			printf("请输入:>\n");
+			(void)scanf("%d %d", &x, &y);
+			int ret = Add(x, y);
+			printf("%d\n", ret);
+			break;
+		case 2:
+			printf("请输入:>\n");
+			(void)scanf("%d %d", &x, &y);
+			ret = Sub(x, y);
+			printf("%d\n", ret);
+			break;
+		case 3:
+			printf("请输入:>\n");
+			(void)scanf("%d %d", &x, &y);
+			ret = Mul(x, y);
+			printf("%d\n", ret);
+			break;
+		case 4:
+			printf("请输入:>\n");
+			(void)scanf("%d %d", &x, &y);
+			ret = Div(x, y);
+			printf("%d\n", ret);
+			break;
+		default:
+			printf("输入错误，请重新输入\n");
+		}
+	} while (input);
+}
+int main()
+{
+	test();
+	return 0;
+}
+
+
+//利用函数指针数组优化计算器
+#include<stdio.h>
+void menu()
+{
+	printf("**********************\n");
+	printf("*** 欢迎来到计算器 ***\n");
+	printf("***  1.Add   2.Sub ***\n");
+	printf("***  3.Mul   4.Div ***\n");
+	printf("**********************\n");
+}
+int Add(int x, int y)
+{
+	return x + y;
+}
+int Sub(int x, int y)
+{
+	return x - y;
+}
+int Mul(int x, int y)
+{
+	return x * y;
+}
+int Div(int x, int y)
+{
+	return x / y;
+}
+void test()
+{
+	int input = 0;
+	int x = 0;
+	int y = 0;
+	int ret = 0;
+	int (*parray[5])(int, int) = { 0,Add,Sub,Mul,Div };
+	do
+	{
+		menu();
+		printf("请选择:>");
+		(void)scanf("%d", &input);
+		printf("情输入两个操作数:>\n");
+		(void)scanf("%d %d", &x, &y);
+		ret = parray[input](x, y);
+		printf("%d\n", ret);
+	} while (input);
+}
+int main()
+{
+	test();
+	return 0;
+}
