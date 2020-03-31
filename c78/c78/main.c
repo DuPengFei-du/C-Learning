bd8665d0fdecefd264ec97cb2a85980eacc840cc
DuPengFei-du <935820240@qq.com>
2020-03-31 08:24:19 +0800

    test

---
+++ c78/c78/main.c
@@ -0,0 +1,349 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include<stdio.h>
+int main()
+{
+	printf("%c\n", '\'');
+	printf("%s\n", "\"");
+	return 0;
+}
+
+
+#include<stdio.h>
+int Add(int x, int y)
+{
+	return x + y;
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	(void)scanf("%d %d", &a, &b);
+	c = Add(a, b);
+	printf("%d\n", c);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int i = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	while (i < 10)
+	{
+		printf("%d ", arr[i]);
+		i++;
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	(void)scanf("%d", &n);
+	if (n % 2 == 1)
+		printf("奇数\n");
+	else
+		printf("偶数\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int count = 0;
+	for (; i <= 100; i++)
+	{
+		if (i % 2 == 1)
+		{
+			count++;
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	printf("%d", count);
+	return 0;
+}
+
+
+//n的阶乘
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	int sum = 1;
+	int n = 0;
+	(void)scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		sum *= i;
+	}
+	printf("%d", sum);
+	return 0;
+}
+
+
+#include<stdio.h>
+int fac(int n)
+{
+	if (n == 0 || n == 1)
+		return 1;
+	else
+		return n * fac(n - 1);
+}
+int main()
+{
+	int n = 0;
+	int ret = 0;
+	(void)scanf("%d", &n);
+	ret = fac(n);
+	printf("%d", ret);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int age = 10;
+	if (age == 5)
+		printf("age==5\n");
+	return 0;
+}
+//屏幕上什么都不会打印，因为age！=5；
+
+
+#include<stdio.h>
+int main()
+{
+	int day = 0;
+	scanf("%d", &day);
+	switch (day)
+	{
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+		printf("weekday\n");
+		break;
+	case 6:
+	case 7:
+		printf("weekend\n");
+		break;
+	default:
+		printf("选择错误\n");
+		break;
+	}
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 1;
+	int m = 2;
+	switch (n)
+	{
+	case 1:m++;
+	case 2:n++;
+	case 3:
+		switch (n)
+		{//switch允许嵌套使用
+		case 1:	n++;
+		case 2:m++; n++;	break;
+		}
+	case 4:m++; break;
+	default:
+		break;
+	}
+	printf("m = %d, n = %d\n", m, n);
+	system("pause");
+	return 0;
+}
+//结果是m=5，n=3；
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		printf("%d ", i);
+		i++;
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		if (i == 5)
+			break;
+		printf("%d ", i);
+		i++;
+	}
+	return 0;
+}
+//打印结果为1，2，3，4
+//break永久的跳出循环，停止后期的所有循环，永久的终止循环
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		if (i == 5)
+			continue;
+		printf("%d ", i);
+		i++;
+	}
+	return 0;
+}
+//打印结果是1，2，3，4，4，4，4，4...死循环
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		i++;
+		if (i == 5)
+			continue;
+		printf("%d ", i);
+	}
+	return 0;
+}
+//打印结果为2，3，4，5，7，8，9，10，11
+//continue作用：continue用来终止本次的循环，当次continue之后的语句不再执行。
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int j = 0;
+	int count = 0;
+	for (i = 100; i <= 200; i++)
+	{
+		for (j = 2; j <= i - 1; j++)
+		{
+			if (i % j == 0)
+				break;
+		}
+		if (j == i)
+		{
+			printf("%d ", i);
+			count++;
+		}
+	}
+	printf("\n");
+	printf("%d", count);
+	return 0;
+}
+
+
+//最大公约数
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	(void)scanf("%d %d", &a, &b);
+	while (c = a % b)
+	{
+		a = b;
+		b = c;
+	}
+	printf("%d", b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int j = 0;
+	for (; i < 10; i++)
+	{
+		for (; j < 10; j++)
+		{
+			printf("hehe\n");
+		}
+	}
+	return 0;
+}
+//这段代码得打印结果只有10个hehe
+//并且10个hehe是从i=0-- i=9一口气打印出来的
+//当i=0时，j从一直到9，打印了10个hehe之后，j的条件就不再满足了
+//所以一共只会打印10个hehe
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int i = 1;
+	int ret = 1;
+	int sum = 0;
+	(void)scanf("%d", &n);
+	for (n = 1; n <= 10; n++)
+	{
+		//ret = 1;
+		//i = 1;
+		while (i <= n)
+		{
+			ret = ret * i;
+			i++;
+		}
+		sum = sum + ret;
+	}
+	printf("%d", sum);
+	return 0;
+}
+
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int i = 0;
+	char password[20] = { 0 };
+	for (i = 0; i < 3; i++)
+	{
+		printf("请输入密码:>");
+		scanf("%s", password);
+		if (0 == strcmp(password, "123456"))
+		{
+			printf("登陆成功\n");
+			break;
+		}
+		else
+		{
+			printf("密码错误，请重新输入！\n");
+		}
+	}
+	if (i == 3)
+		printf("三次密码均错误，退出程序！\n");
+	return 0;
+}
