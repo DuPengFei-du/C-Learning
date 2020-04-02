89504dd507801168431881ffe8242edb6bace99b
DuPengFei-du <935820240@qq.com>
2020-04-02 08:27:20 +0800

    test

---
+++ c79/c79/main.c
@@ -0,0 +1,271 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+/*
+原码：直接将二进制按照正负的形式翻译成二进制就可以了
+反码：将原码的符号位不变，其他位按位取反就可以得到了
+补码：反码加1是补码
+*/
+
+
+#include<stdio.h>
+int main()
+{
+	int a = -10;
+	//10000000000000000000000000001010  --原码
+	//11111111111111111111111111110101  --反码
+	//11111111111111111111111111110110  --补码
+	//fffffff6               ---补码转换成16进制的样子
+	//但是在内存中&a显示的结果是f6ffffff
+}
+
+
+#include<stdio.h>
+int main()
+{
+	//加入说我想算一个1-1的结果是多少
+	//但是遗憾的是，计算机不能算减法
+	//那我只好把减法转换为加法的形式
+	//1 - 1;
+	1 + (-1);
+	//如果按原码来计算的话
+	// 1的原码
+	//00000000000000000000000000000001
+	//-1的原码
+	//10000000000000000000000000000001
+	//两个码相加的结果为
+	//10000000000000000000000000000010
+	//如果按照原码直接相加的话，结果就为-2，这显然是不正确的
+	//所以科学家们推出了补码的概念
+	//用补码来算的时候，可以算出正确的结果
+}
+
+
+
+//大小端问题
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	if (*pa == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	return *pa;
+}
+int main()
+{
+	int ret = CheckSystem();
+	if (ret == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	union Un
+	{
+		int i;
+		char c;
+	}u;
+	u.i = 1;
+	return u.c;
+}
+int main()
+{
+	int ret = CheckSystem();
+	if (ret == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 9;
+	//9存在内存中，存储的时他的补码
+	//00000000000000000000000000001001
+	float* pFloat = (float*)& n;
+	//要把一个整形，转换层成一个浮点型，那么现在就来转换一下
+	//0 00000000 00000000000000000001001
+	//又因为上述浮点型的存储中E全为0，所以这个数字是一个无限接近于0的数字 ，所以打印的结果为0.000000
+	printf("n的值为:%d\n", n);
+	//以整形的形式存入，再以整形的形式取出，那么结果当然还是整形了，所以说结果为9;
+	printf("*pFloat的值为:%f\n", *pFloat);
+	//要把一个整形，转换层成一个浮点型，那么现在就来转换一下
+	//0 00000000 00000000000000000001001
+	//又因为上述浮点型的存储中E全为0，所以这个数字是一个无限接近于0的数字 ，所以打印的结果为0.000000
+	*pFloat = 9.0;
+	printf("n的值为:%d\n", n);
+	//以浮点数的形式存进去，以浮点数的形式往出拿
+	//先将9写成二进制的形式
+	//1001.0
+	//1.001*2^3;
+	//0 100000010 00100000000000000000000  --补码
+	//整数，原，反，补相同
+	//所以结果为010000001000100000000000000000000  --结果为1091567616
+	printf("*pFloat的值为:%f\n", *pFloat);
+	//以浮点数形式存进去，再以浮点是结果拿出来，结果是不会变化的
+	//结果为9.000000
+	return 0;
+}
+
+
+#include<stdio.h>
+void Print1(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 1; i <= sz; i++)
+	{
+		printf("%d ", i);
+	}
+	printf("\n");
+}
+void Print2(int(*p)[10], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", (*p)[i]);
+		//首先找到数组，用(*p)找到数组，然后去访问第i个元素
+		//如果不带括号的话，那么p就会先和[]进行结合
+		//其实(*p)就等于是数组名然后[i]，就相当于是arr[i];
+	}
+}
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	Print1(arr, sz);
+	Print2(&arr, sz);
+	return 0;
+}
+
+
+#include<stdio.h>
+void Print1(int arr[3][5], int x, int y)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < x; i++)
+	{
+		for (j = 0; j < y; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+void Print2(int(*p)[5], int x, int y)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < x; i++)
+	{
+		for (j = 0; j < y; j++)
+		{
+			//printf("%d ", (*(p+i))[j]);
+			printf("%d ", *(*(p + i) + j));
+			//*(p+i)相当于找到了第i行首元素的地址
+			//也可以写成p[i][j]
+			//其实p就相当于arr
+		}
+		printf("\n");
+	}
+}
+int main()
+{
+	int arr[3][5] = { 1,2,3,4,5,3,4,5,6,7,4,5,6,7,8 };
+	Print1(arr, 3, 5);
+	Print2(arr, 3, 5);
+	//arr代表首元素的地址，对于二维数组来说，首元素的地址就是第一行的地址
+	//所以列标是很重要的
+	return 0;
+}
+
+
+#include<stdio.h>
+int Add(int x, int y)
+{
+	return x + y;
+}
+int main()
+{
+	printf("%p\n", &Add);   //证实了函数也是有地址的
+	int (*p)(int x, int y) = &Add;
+	int ret = (*p)(2, 3);   //函数的调用
+	//也可以写车： int ret=p(2,3);
+	//哪个*写不写都行，写几个都行
+	//(*p)是先找到这个函数，然后传参进行调用
+	//p是函数指针
+	return 0;
+}
+
+
+//利用函数指针数组优化计算器
+#include<stdio.h>
+void menu()
+{
+	printf("**********************\n");
+	printf("*** 欢迎来到计算器 ***\n");
+	printf("***  1.Add   2.Sub ***\n");
+	printf("***  3.Mul   4.Div ***\n");
+	printf("**********************\n");
+}
+int Add(int x, int y)
+{
+	return x + y;
+}
+int Sub(int x, int y)
+{
+	return x - y;
+}
+int Mul(int x, int y)
+{
+	return x * y;
+}
+int Div(int x, int y)
+{
+	return x / y;
+}
+void test()
+{
+	int input = 0;
+	int x = 0;
+	int y = 0;
+	int ret = 0;
+	int (*parray[5])(int, int) = { 0,Add,Sub,Mul,Div };
+	do
+	{
+		menu();
+		printf("请选择:>");
+		(void)scanf("%d", &input);
+		printf("情输入两个操作数:>\n");
+		(void)scanf("%d %d", &x, &y);
+		ret = parray[input](x, y);
+		printf("%d\n", ret);
+	} while (input);
+}
+int main()
+{
+	test();
+	return 0;
+}
