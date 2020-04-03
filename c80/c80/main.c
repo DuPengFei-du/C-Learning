2d941c5b51cb7bb184cfc34382ba394e0707803b
DuPengFei-du <935820240@qq.com>
2020-04-03 08:24:41 +0800

    test

---
+++ c80/c80/main.c
@@ -0,0 +1,296 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int i = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int Add(int x, int y)
+{
+	return x + y;
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	(void)scanf("%d %d", &a, &b);
+	c = Add(a, b);
+	printf("%d", c);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	(void)scanf("%d", &n);
+	if (n % 2 == 1)
+		printf("奇数");
+	else
+		printf("偶数");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int i = 1;
+	int sum = 1;
+	(void)scanf("%d", &n);
+	for (; i <= n; i++)
+	{
+		sum *= i;
+	}
+	printf("%d", sum);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = "###########";
+	char arr2[] = "hello world";
+	int left = 0;
+	int sz = sizeof(arr1) / sizeof(arr1[0]);
+	int right = sz - 1;
+	while (left <= right)
+	{
+		arr1[left] = arr2[left];
+		arr1[right] = arr2[right];
+		left++;
+		right--;
+	}
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[] = "###########";
+	char arr2[] = "hello world";
+	strcpy(arr1, arr2);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int n = 0;
+	int k = 0;
+	int ret = 0;
+	(void)scanf("%d %d", &n, &k);
+	ret = pow(n, k);
+	printf("%d", ret);
+	return 0;
+}
+
+
+#include<stdio.h>
+int MAX(int x, int y)
+{
+	return x > y ? x : y;
+}
+int main()
+{
+	int x = 0;
+	int y = 0;
+	int c = 0;
+	(void)scanf("%d %d",& x, &y);
+	c = MAX(x, y);
+	printf("%d", c);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int is_prime(int n)
+{
+	for (int i = 2; i < n; i++)
+	{
+		if (n % i== 0)
+			return 0;
+	}
+	return 1;
+}
+int main()
+{
+	int n = 0;
+	(void)scanf("%d", &n);
+	if (is_prime(n) == 1)
+		printf("素数");
+	else
+		printf("不是素数");
+	return 0;
+}
+
+
+//打印所输入数字的每一位
+#include<stdio.h>
+void Print(int n)
+{
+	if (n > 9)
+		Print(n / 10);
+	printf("%d ", n % 10);
+}
+int main()
+{
+	int n = 0;
+	(void)scanf("%d", &n);
+	Print(n);
+	return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = "hello world";
+	memset(arr1, 'x', 5);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+void Swap(int* x, int* y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	Swap(&a, &b);
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int is_leapyear(int i)
+{
+	if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0)
+		return 1;
+	else
+		return 0;
+}
+int main()
+{
+	int n = 0;
+	(void)scanf("%d", &n);
+	if (is_leapyear(n) == 1)
+	{
+		printf("是闰年\n");
+	}
+	else
+	{
+		printf("不是闰年\n");
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+void new_line()
+{
+	printf("hehe\n");
+}
+void three_line()
+{
+	for (int i = 0; i < 3; i++)
+	{
+		new_line();
+	}
+}
+int main()
+{
+	three_line();
+	return 0;
+}
+
+
+#include<stdio.h>
+int my_strlen(const char* str)
+{
+	int count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+int main()
+{
+	int len = my_strlen("abc");
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+int my_strlen(const char* str)
+{
+	if (*str == '\0')
+		return 0;
+	else
+		return 1 + my_strlen(str + 1);
+}
+int main()
+{
+	int len = my_strlen("abcdef");
+	printf("%d\n", len);
+	return 0;
+}
