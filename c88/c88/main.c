f077d7c3a53113c3b9201e45b5daac3fecd795e2
DuPengFei-du <935820240@qq.com>
2020-04-11 21:24:22 +0800

    test

---
+++ c88/c88/main.c
@@ -0,0 +1,277 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+#include<stdio.h>
+//自定义类型
+struct Stu
+{
+	char name[20];   //一个汉字是两个字符
+	int age;
+	float score;
+};
+int main()
+{
+	struct Stu s = { "张三",20,60.0f };
+	struct Stu* ps = &s;  //结构体指针
+	printf("%s %d %f", (*ps).name, (*ps).age, (*ps).score);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	printf("%d\n", sizeof(int));  //4
+	printf("%d\n", sizeof(char)); //1
+	printf("%d\n", sizeof(short)); //2
+	printf("%d\n", sizeof(long));  //4/8
+	printf("%d\n", sizeof(long long));  //8
+	printf("%d\n", sizeof(float));  //4
+	printf("%d\n", sizeof(double));  //8
+	return 0;
+}
+//类型的大小单位都是字节
+//计算机中的单位 
+//bit(位) byte(字节) kbyte(kb) mb gb tb pb  
+//前两个单位之间的换算单位是8，后面的单位之间的是1024
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	(void)scanf("%d %d", &a, &b);
+	c = a + b;
+	printf("%d", c);
+	return 0;
+}
+
+
+#include<stdio.h>
+int Add(int x, int y)
+{
+	return x + y;
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	(void)scanf("%d %d", &a, &b);
+	c =Add(a,b);
+	printf("%d", c);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	printf("%c\n", '\'');
+	printf("%s\n", "\"");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int i = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int num = 0;
+	(void)scanf("%d", &num);
+	printf("%d", num);
+	return 0;
+}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	{
+//		int num = 10;
+//	}
+//	printf("%d", num);
+//	return 0;
+//	//此时，这个程序会出错，num作用域中不包括有printf语句。
+//}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = "abc";
+	char arr2[] = { 'a','b','c',0 };
+	printf("%s\n", "abc");
+	printf("%s\n", arr1);
+	printf("%s\n", arr2);
+	return 0;
+	//三种打印的结果都是abc
+	//如果arr2后面不加0的话，那么打印结果就会是再abc的后面还会出现烫烫烫的字符
+	//因为如果不加0的话，字符串就会并没有结束，只有加了0或者\0才会停止打印
+	//烫烫烫背后的真实东西其实是0xcccccc
+	//由此可以看来\0对字符串来说是非常重要的
+	//字符串是\0需要的结束标志的符号；
+}
+
+
+#include<stdio.h>
+int main()
+{
+	printf("c:\test\test.c\n");
+	return 0;
+}
+//上面代码的输出结果为
+//c:      est     est.c
+//主要是应为编译器将\t也当成了一个转义字符在使用
+//如果想要输出和printf语句中内容一样的东西，那么在\t的前面在加上一个\就行了
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	printf("%d\n", 10);
+	printf("%d\n", strlen("abc"));
+	//printf("%d\n", strlen("c:\test\47\test.c");
+	return 0;
+	//其中，第三个printf语句的输出结果为13，主要的陷阱就在于\47
+	//这其中的本质还是在考察对于转义字符的理解
+	//\ddd也为转义字符，其中ddd表示1-3个八进制的数字，这三个东西统一的看成一个字符，所以说长度为1.
+	//\xddd  ddd表示3个16进制数字
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	while (i < 10)
+	{
+		printf("%d ", arr[i]);
+		i++;
+	}
+	printf("\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a1 = 5 / 2;
+	float a2 = 5 / 2;
+	float a3 = 5.0 / 2;
+	printf("%d\n", a1);
+	printf("%f\n", a2);
+	printf("%f\n", a3);
+	return 0;
+}
+//针对于除法操作，与他存储的是什么类型是没有关系的
+//主要是看两个除法的操作数到底是什么类型的，如果有一个是浮点型的，得到的结果就会是浮点型的结果
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 3;
+	int b = 5;
+	int c = a ^ b;
+	printf("%d", c);
+	return 0;
+	//相同为0，相异为1
+	//结果为6
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = ++a;
+	printf("%d\n", a);   //11
+	printf("%d\n", b);   //11
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = --a;
+	printf("%d\n", a);   //9
+	printf("%d\n", b);   //9
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 3;
+	int b = 5;
+	int c = a && b;
+	printf("%d", c);
+	return 0;
+}
+//结果为1
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = a || b;
+	printf("%d", c);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = (1, 2, 3, 4, 5);
+	printf("%d\n", a);
+	return 0;
+}
+
+
+#include<stdio.h>
+#define ADD(X,Y) X+Y
+int main()
+{
+	printf("%d\n", 10 * ADD(2, 3));
+	return 0;
+}
+//宏的替换是直接替换的，不经运算，直接替换。
+
+
+#include<stdio.h>
+int main()
+{
+	char c = 'w';
+	int a = 10;
+	int* pa = &a;
+	char* pc = &c;
+	printf("%d\n", sizeof(pa));  //4
+	printf("%d\n", sizeof(pc));  //4
+	return 0;
+}
