dcaf8540c7459c8a9f33f5cee8fc4b4f167b51be
DuPengFei-du <935820240@qq.com>
2020-04-14 08:26:00 +0800

    test

---
+++ c91/c91/main.c
@@ -0,0 +1,454 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+#include<stdio.h>
+int main()
+{
+	//数组也是一种自定义类型
+	int arr[10] = { 0 };
+	printf("%d\n", sizeof(arr));                //40
+	printf("%d\n", sizeof(int[10]));          //40
+	int a = 10;
+	sizeof(int);  //可以的
+	sizeof(a);    //可以的
+	//那么问题来了，数组的类型是什么呢？
+	//数组的类型，就是数组去掉数组名剩下的部分
+	//比如上述对数组的声明 int arr[10],那么数组的类型就是int [10]
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0x11223344;
+	//0x11223344放在a里面是刚刚好的
+	//因为11是一个字节，22是一个字节，33是一个字节，44是一个字节
+	//通过监视看：转换到16进制，观察到a的值为0x11223344
+	//通过内存看：输入&a，可以观察到，a显示为44332211
+	return 0;
+}
+//整形在内存中的存储分为大小端的情况
+
+
+#include<stdio.h>
+int main()
+{
+	//加入说我想算一个1-1的结果是多少
+	//但是遗憾的是，计算机不能算减法
+	//那我只好把减法转换为加法的形式
+	//1 - 1;
+	1 + (-1);
+	//如果按原码来计算的话
+	// 1的原码
+	//00000000000000000000000000000001
+	//-1的原码
+	//10000000000000000000000000000001
+	//两个码相加的结果为
+	//10000000000000000000000000000010
+	//如果按照原码直接相加的话，结果就为-2，这显然是不正确的
+	//所以科学家们推出了补码的概念
+	//用补码来算的时候，可以算出正确的结果
+}
+
+
+//解题思路：
+//我们可以通过把1存在内存中，然后通过看他的存储形式，我们来判断当前机器到底是大端还是小端
+//1的16形式的格式为00000001
+//如果存储形式是01000000那么就是小端
+//如果存储形式是00000001那么就是大段
+//我们可以通过读取第一个字节的内容，来判断到底是大端还是小端，想要读取一个整形的第一个字节，那么存储在char*类型的变量中就可以了。
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	if (*pa == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+//封装成一个函数
+#include<stdio.h>
+int check_system()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	if (*pa == 1)
+		return 1;
+	else
+		return 0;
+}
+int main()
+{
+	int ret = check_system();
+	if (ret == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+int check_system()
+{
+	union Un
+	{
+		int i;
+		char c;
+	}u;
+	u.i = 1;
+	return u.c;
+}
+int main()
+{
+	int ret = check_system();
+	if (ret == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char a = -128;
+	//10000000000000000000000010000000   原码-- -128
+	//11111111111111111111111101111111   反码
+	//11111111111111111111111110000000   补码
+	//但是只能存8个比特位，结果为10000000
+	//需要进行整形提升
+	//提升的时候需要看原来的符号位，全部补成1
+	//结果为11111111111111111111111110000000  --补码
+	//又因为是无符号数，所以原码，反码，补码全部相同
+	//所以就得到了结果：4294967168
+	printf("%u\n", a);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 9;
+	//9存在内存中，存储的时他的补码
+	//00000000000000000000000000001001
+	float* pFloat = (float*)& n;
+	//要把一个整形，转换层成一个浮点型，那么现在就来转换一下
+	//0 00000000 00000000000000000001001
+	//又因为上述浮点型的存储中E全为0，所以这个数字是一个无限接近于0的数字 ，所以打印的结果为0.000000
+	printf("n的值为:%d\n", n);
+	//以整形的形式存入，再以整形的形式取出，那么结果当然还是整形了，所以说结果为9;
+	printf("*pFloat的值为:%f\n", *pFloat);
+	//要把一个整形，转换层成一个浮点型，那么现在就来转换一下
+	//0 00000000 00000000000000000001001
+	//又因为上述浮点型的存储中E全为0，所以这个数字是一个无限接近于0的数字 ，所以打印的结果为0.000000
+	*pFloat = 9.0;
+	printf("n的值为:%d\n", n);
+	//以浮点数的形式存进去，以浮点数的形式往出拿
+	//先将9写成二进制的形式
+	//1001.0
+	//1.001*2^3;
+	//0 100000010 00100000000000000000000  --补码
+	//整数，原，反，补相同
+	//所以结果为010000001000100000000000000000000  --结果为1091567616
+	printf("*pFloat的值为:%f\n", *pFloat);
+	//以浮点数形式存进去，再以浮点是结果拿出来，结果是不会变化的
+	//结果为9.000000
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char* p1 = "abcdef";
+	char* p2 = "abcdef";
+	if (p1 == p2)
+		printf("p1==p2");
+	else
+		printf("p1!=p2");
+	return 0;
+	//打印结果为p1==p2
+	//虽然把字符串abcdef地址存在了p1和p2当中，但是两个字符串是一样的
+	//都是abcdef，所以首元素a的地址自然也是一样的了
+	//abcdef为常量字符串，所以说他是不能被改变的，其实也只会出现一次abcdef
+	//但是p1的地址和p2的地址是绝对不一样的。
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = "abcdef";
+	//创建了一个数组，数组中存的是abcdef
+	const char* p = "abcdef";
+	//将字符串abcdef中首元素的地址，即a的地址，存在p中
+	arr1[2] = 'w';
+	//修改数组中的内容，是被允许的
+	//*(p + 2) = 'w';        //p[2]
+	//p所指向的是一个常量字符串，常量字符串中的值是不可以被修改的
+	return 0;
+	//这个程序运行起来是会崩溃的，崩溃的主要原因在于，*(p+2)='w'
+	//这个语句是不正确的
+	//字符指针不仅仅用来存放字符的地址，还可能存放一个字符串的起始地址
+}
+
+
+#include<stdio.h>
+void Print1(int arr[3][5], int x, int y)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < x; i++)
+	{
+		for (j = 0; j < y; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+void Print2(int(*p)[5], int x, int y)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < x; i++)
+	{
+		for (j = 0; j < y; j++)
+		{
+			//printf("%d ", (*(p+i))[j]);
+			printf("%d ", *(*(p + i) + j));
+			//*(p+i)相当于找到了第i行首元素的地址
+			//也可以写成p[i][j]
+			//其实p就相当于arr
+		}
+		printf("\n");
+	}
+}
+int main()
+{
+	int arr[3][5] = { 1,2,3,4,5,3,4,5,6,7,4,5,6,7,8 };
+	Print1(arr, 3, 5);
+	Print2(arr, 3, 5);
+	//arr代表首元素的地址，对于二维数组来说，首元素的地址就是第一行的地址
+	//所以列标是很重要的
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[5];          //整形数组
+	int* parr1[10];      //指针数组
+	int(*parr2)[10];     //数组指针
+	int(*parr3[10])[5];  //存放数组指针的数组，可以存放10个这样的指针
+	//数组有10个对象，数组中存储对象的类型是数组指针
+	//5个整形元素的数组的地址 数组中每个元素的类型是int
+}
+
+
+#include<stdio.h>
+void test(int arr[3][5])
+{
+
+}
+//void test(int arr[][])
+//{
+//	//这种传参是不正确的，可以省略行，不可以省略列
+//}
+void test(int arr[][5])
+{
+
+}
+//总结:二维数组传参，函数形式的设计只能省略第一维的[],不能省略第二维的
+//因为对一个二维数组，可以不知道有多少行，但是必须知道一行有多少个元素
+//这样才会方便运算，同样，一样的道理，如果用数组接收参数，那么就原模原样的接收就可以了
+void test(int(*arr)[5])
+{
+
+}
+int main()
+{
+	int arr[3][5];
+	test(arr);
+}
+
+
+#include<stdio.h>
+int Add(int x, int y)
+{
+	return x + y;
+}
+int main()
+{
+	printf("%p\n", &Add);   //证实了函数也是有地址的
+	int (*p)(int x, int y) = &Add;
+	int ret = (*p)(2, 3);   //函数的调用
+	//也可以写车： int ret=p(2,3);
+	//哪个*写不写都行，写几个都行
+	//(*p)是先找到这个函数，然后传参进行调用
+	//p是函数指针
+	return 0;
+}
+
+
+#include<stdio.h>
+int my_strlen(const char* str)
+{
+
+}
+int main()
+{
+	int* arr[10];  //整形指针数组
+	char* arr[5];  //字符指针数组
+	int (*ps)(const char*) = &my_strlen;
+	int (*ps2)(const char*) = &my_strlen;
+	int (*ps3)(const char*) = &my_strlen;
+	int (*pArr[3])(const char*) = &my_strlen;   //函数指针数组
+	//存放函数指针的数组
+	return 0;
+}
+
+
+//利用函数指针数组优化计算器
+#include<stdio.h>
+void menu()
+{
+	printf("**********************\n");
+	printf("*** 欢迎来到计算器 ***\n");
+	printf("***  1.Add   2.Sub ***\n");
+	printf("***  3.Mul   4.Div ***\n");
+	printf("**********************\n");
+}
+int Add(int x, int y)
+{
+	return x + y;
+}
+int Sub(int x, int y)
+{
+	return x - y;
+}
+int Mul(int x, int y)
+{
+	return x * y;
+}
+int Div(int x, int y)
+{
+	return x / y;
+}
+void test()
+{
+	int input = 0;
+	int x = 0;
+	int y = 0;
+	int ret = 0;
+	int (*parray[5])(int, int) = { 0,Add,Sub,Mul,Div };
+	do
+	{
+		menu();
+		printf("请选择:>");
+		(void)scanf("%d", &input);
+		printf("情输入两个操作数:>\n");
+		(void)scanf("%d %d", &x, &y);
+		ret = parray[input](x, y);
+		printf("%d\n", ret);
+	} while (input);
+}
+int main()
+{
+	//test();
+	return 0;
+}
+
+
+//利用快速排序进行排序
+#include<stdio.h>
+#include<stdlib.h>
+int cmp(const void* e1, const void* e2)
+{
+	return *(int*)e1 - *(int*)e2;
+}
+int main()
+{
+	int arr[10] = { 1,3,5,7,9,2,4,6,8,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	qsort(arr, sz, sizeof(arr[0]), cmp);
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct Stu
+{
+	char name[20];
+	int age;
+};
+int cmp_struct_by_name(const void* elem1, const void* elem2)
+{
+	return strcmp(((struct Stu*)elem1)->name, ((struct Stu*)elem2)->name);
+}
+int main()
+{
+	struct Stu arr[3] = { {"zhangsan",20},{"lisi",15},{"wangwu",25} };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	qsort(arr, sz, sizeof(arr[0]), cmp_struct_by_name);
+}
+
+
+//模拟冒泡排序
+#include<stdio.h>
+int cmp_int(const void* e1, const void* e2)
+{
+	return *(int*)e1 - *(int*)e2;
+}
+void Swap(char* buf1, char* buf2,int width)
+{
+	for (int i = 0; i < width; i++)
+	{
+		char temp = *buf1;
+		*buf1 = *buf2;
+		*buf2 = temp;
+		buf1++;
+		buf2++;
+	}
+}
+void my_bubblesort(void *base, int sz, int width, int (*cmp)(const void* e1, const void* e2))
+{
+	for (int i = 0; i < sz - i; i++)
+	{
+		for (int j = 0; j < sz - i - 1; j++)
+		{
+			if (cmp((char*)base + j * width, (char*)base + (j + 1) * width) > 0)
+			{
+				//进行交换
+				Swap((char*)base + j * width, (char*)base + (j + 1) * width, width);
+			}
+		}
+	}
+}
+int main()
+{
+	int arr[10] = { 1,3,5,7,9,2,4,6,8,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int width = sizeof(arr[0]);
+	my_bubblesort(arr, sz, width, cmp_int);
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
