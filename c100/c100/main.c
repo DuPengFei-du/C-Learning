6a7c7f6d360529155a0aca465c885b3eb1a16e08
DuPengFei-du <935820240@qq.com>
2020-04-23 08:26:13 +0800

    test

---
+++ c100/c100/main.c
@@ -0,0 +1,433 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = { 'a','b','c' };
+	char arr2[] = "abc";
+	printf("%s", arr1);
+	printf("%s", arr2);
+	return 0;
+}
+//这两个数组的是不一样的
+//arr1是不可以看成数组的，因为它没有\0，\0是字符串的结束标志
+//如果没有\0是不可以看成字符串的
+//arr2是可以看成字符串的，c后面还有一个隐藏的\0
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int* p = arr;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d ", *(p + i));
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int i = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (i = 0; i < sz; i++)
+	{
+		printf("&arr[%d]=%p\n", i, &arr[i]);
+	}
+	return 0;
+}
+//每个元素地址之间差的值是4
+//至于为什么差4，因为我是一个整形的数组，每个元素占有4个字节。
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int* p = arr;  //得到了首元素的地址
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%p===%p\n", &arr[i], p + i);
+	}
+	return 0;
+}
+//这个代码就说明：p+i为下标为i的元素的地址
+//*(p + i)为下标为i的元素的值
+
+
+#include<stdio.h>
+int main()
+{
+	int arr1[3][4]; //创建了一个三行四列的数组
+	char arr2[4][5];
+	double arr3[3][5];
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[3][4] = { 0 };
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[3][4] = { 0 };
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			printf("&arr[%d][%d]=%p\n", i, j, &arr[i][j]);
+		}
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	printf("%p\n", &arr);
+	printf("%p\n", &arr + 1);
+	return 0;
+}
+//这两行之间的地址值的差值为40，刚好为数组的大小
+
+
+//大小端字节序的问题
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	if (*pa == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	return *pa;
+}
+int main()
+{
+	int ret = CheckSystem();
+	if (ret == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+int Check()
+{
+	union Un
+	{
+		int i;
+		char c;
+	}u;
+	u.i = 1;
+	return u.c;
+}
+int main()
+{
+	int ret = Check();
+	if (ret == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	//数组也是一种自定义类型
+	int arr[10] = { 0 };
+	printf("%d\n", sizeof(arr));                //40
+	printf("%d\n", sizeof(int[10]));          //40
+	int a = 10;
+	sizeof(int);  //可以的
+	sizeof(a);    //可以的
+	//那么问题来了，数组的类型是什么呢？
+	//数组的类型，就是数组去掉数组名剩下的部分
+	//比如上述对数组的声明 int arr[10],那么数组的类型就是int [10]
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0x11223344;
+	//0x11223344放在a里面是刚刚好的
+	//因为11是一个字节，22是一个字节，33是一个字节，44是一个字节
+	//通过监视看：转换到16进制，观察到a的值为0x11223344
+	//通过内存看：输入&a，可以观察到，a显示为44332211
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	//加入说我想算一个1-1的结果是多少
+	//但是遗憾的是，计算机不能算减法
+	//那我只好把减法转换为加法的形式
+	//1 - 1;
+	1 + (-1);
+	//如果按原码来计算的话
+	// 1的原码
+	//00000000000000000000000000000001
+	//-1的原码
+	//10000000000000000000000000000001
+	//两个码相加的结果为
+	//10000000000000000000000000000010
+	//如果按照原码直接相加的话，结果就为-2，这显然是不正确的
+	//所以科学家们推出了补码的概念
+	//用补码来算的时候，可以算出正确的结果
+}
+
+
+//解题思路：
+//我们可以通过把1存在内存中，然后通过看他的存储形式，我们来判断当前机器到底是大端还是小端
+//1的16形式的格式为00000001
+//如果存储形式是01000000那么就是小端
+//如果存储形式是00000001那么就是大段
+//我们可以通过读取第一个字节的内容，来判断到底是大端还是小端，想要读取一个整形的第一个字节，那么存储在char*类型的变量中就可以了。
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	if (*pa == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+//封装成一个函数
+#include<stdio.h>
+int check_system()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	if (*pa == 1)
+		return 1;
+	else
+		return 0;
+}
+int main()
+{
+	int ret = check_system();
+	if (ret == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char* p1 = "abcdef";
+	char* p2 = "abcdef";
+	if (p1 == p2)
+		printf("p1==p2");
+	else
+		printf("p1!=p2");
+	return 0;
+	//打印结果为p1==p2
+	//虽然把字符串abcdef地址存在了p1和p2当中，但是两个字符串是一样的
+	//都是abcdef，所以首元素a的地址自然也是一样的了
+	//abcdef为常量字符串，所以说他是不能被改变的，其实也只会出现一次abcdef
+	//但是p1的地址和p2的地址是绝对不一样的。
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = "abcdef";
+	//创建了一个数组，数组中存的是abcdef
+	const char* p = "abcdef";
+	//将字符串abcdef中首元素的地址，即a的地址，存在p中
+	arr1[2] = 'w';
+	//修改数组中的内容，是被允许的
+	//*(p + 2) = 'w';        //p[2]
+	//p所指向的是一个常量字符串，常量字符串中的值是不可以被修改的
+	return 0;
+	//这个程序运行起来是会崩溃的，崩溃的主要原因在于，*(p+2)='w'
+	//这个语句是不正确的
+	//字符指针不仅仅用来存放字符的地址，还可能存放一个字符串的起始地址
+}
+
+
+#include<stdio.h>
+void Print1(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 1; i <= sz; i++)
+	{
+		printf("%d ", i);
+	}
+	printf("\n");
+}
+void Print2(int(*p)[10], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", (*p)[i]);
+		//首先找到数组，用(*p)找到数组，然后去访问第i个元素
+		//如果不带括号的话，那么p就会先和[]进行结合
+		//其实(*p)就等于是数组名然后[i]，就相当于是arr[i];
+	}
+}
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	Print1(arr, sz);
+	Print2(&arr, sz);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	//代码1
+	(*(void(*)())0)();
+
+	/*
+	首先，void(*)()为函数指针类型，其次void(*)()内放在括号中
+	形成了(void(*)()),我们都知道，一个类型放在括号中，是用来
+	强制类型转换的，(void(*)())又放在0前面，意思就是吧0转换成
+	函数指针类型，0本来的类型是int，我希望把0当成函数指针类型的这样的
+	一个函数的地址，所以我希望0这个地址处放的是一个无参，返回值类型
+	为void的函数，然后，既然是个地址，那么我就解引用一下
+	所以综上，上述是一场函数调用。
+	调用0位置处的函数，被调用的这个函数是无参的，返回值类型为void的类型
+	*/
+
+	//代码2
+	void (*signal(int, void(*)(int)))(int);
+	/*
+	这段代码整体来看是一个函数声明，其中signal为函数名，signal函数的第一个参数
+	为int类型，第二个参数类型为函数指针类型，有了函数名，有了参数
+	剩下的就是函数的返回值类型了，这个函数的返回值类型也是一个函数指针类型
+	*/
+	/*
+	但是又因为这个代码出现了两次函数指针类型，所以我们可以用typedef进行
+	类型重命名，即：
+	typedef void(*pfun_t)(int);
+	所以上述声明也可以变成
+	pfun_t signal(int,pfun_t);
+	*/
+}
+
+
+#include<stdio.h>
+void Swap(char* buf1, char* buf2, int width)
+{
+	int i = 0;
+	for (i = 0; i < width; i++)
+	{
+		char temp = *buf1;
+		*buf1 = *buf2;
+		*buf2 = temp;
+		buf1++;
+		buf2++;
+	}
+}
+void my_BubbleSort(void* base, int sz, int width, int (*cmp)(const void* elem1, const void* elem2))
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < sz - 1; i++)
+	{
+		for (j = 0; j < sz - 1 - i; j++)
+		{
+			//传给cmp的是要交换的两个元素的地址
+			if (cmp((char*)base + j * width, (char*)base + (j + 1) * width) > 0)
+			{
+				//进行交换
+				Swap((char*)base + j * width, (char*)base + (j + 1) * width, width);
+			}
+		}
+	}
+}
+void Print(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+}
+int cmp_int(const void* elem1, const void* elem2)
+{
+	return *(int*)elem1 - *(int*)elem2;
+}
+void test()
+{
+	int arr[10] = { 1,3,5,7,9,2,4,6,8,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	my_BubbleSort(arr, sz, sizeof(arr[0]), cmp_int);
+	Print(arr, sz);
+}
+int main()
+{
+	test();
+	return 0;
+}
