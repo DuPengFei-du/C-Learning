5fab400ba591a3800c11d05d89213ed27eda4bfd
DuPengFei-du <935820240@qq.com>
2020-04-24 08:29:38 +0800

    test

---
+++ Pro71/Pro71/test.c
@@ -0,0 +1,657 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+//不创建临时变量，交换两个数字的值
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+//二进制中1的个数
+#include<stdio.h>
+int main()
+{
+	int count = 0;
+	int n = 0;
+	(void)scanf("%d", &n);
+	while (n)
+	{
+		count++;
+		n = n & (n - 1);
+	}
+	printf("%d", count);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int count = 0;
+	(void)scanf("%d", &n);
+	for (int i = 0; i < 32; i++)
+	{
+		if (((n >> i) & 1) == 1)
+		{
+			count++;
+		}
+	}
+	printf("%d", count);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = a << 1;
+	//<<左移操作符
+	//左边丢弃，右边补0
+	printf("%d\n", b);
+	printf("%d\n", a);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = -1;
+	int b = a >> 1;
+	printf("%d\n", b);
+	return 0;
+	//采用的是算数右移
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = -1;
+	int b = a >> 1;
+	printf("%d\n", b);
+	return 0;
+	//采用的是算数右移
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 3;  //011
+	int b = 5;  //101
+	int c = a ^ b;   //110
+	printf("%d\n", c);
+	return 0;
+}
+//结果为6
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	a = a + 2;
+	a += 2;
+	printf("%d\n", a);
+	return 0;
+}
+//其他的诸如此类都是这样的
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = !a;
+	printf("%d\n", b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	printf("%p\n", &a);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	printf("%p\n", &a);   //003DF718
+	printf("%x\n", &a);   //3df718
+	return 0;
+}
+//%x表示用16进制打印，如果用16进制打印，前面就会少00
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	printf("%p\n", arr);      //首元素的地址
+	printf("%p\n", &arr[0]); //首元素的地址
+	printf("%p\n", &arr);    //数组的地址
+	return 0;
+}
+//三个的值均为00FBFCF0
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	printf("%p\n", arr);
+	printf("%p\n", arr + 1);
+	printf("%p\n", &arr[0]);
+	printf("%p\n", &arr[0] + 1);
+	printf("%p\n", &arr);   //数组的地址
+	printf("%p\n", &arr + 1); //数组的地址加1
+	return 0;
+}
+//数组的地址加1跳过的是整个数组
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	printf("%d\n", sizeof(a));  //4
+	printf("%d\n", sizeof(int));   //4
+	return 0;
+}
+//printf("%d\n",sizeof a);
+//sizeof可以对变量省略括号，不能对类型省略括号
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[10] = "abcdef";
+	if (arr1 == "abcdef")
+	{
+		printf("hehe\n");
+	}
+	return 0;
+}
+//这段代码是不会打印任何结果的
+//arr1其实是首元素的地址，所以是不等于abcdef的
+//不能用==来判断字符串和数组是否相等
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 5;
+	int c = a && b;
+	printf("%d\n", c);
+	return 0;
+}
+//结果为0
+
+
+#include <stdio.h>
+int main()
+{
+	int i = 0, a = 0, b = 2, c = 3, d = 4;
+	i = a++ && ++b && d++;
+	printf("a = %d\n b = %d\n c = %d\nd = %d\n", a, b, c, d);
+	return 0;
+}
+// 打印的值是1 2 3 4
+
+
+#include <stdio.h>
+int main()
+{
+	int i = 0, a = 0, b = 2, c = 3, d = 4;
+	i = a++ || ++b || d++;
+	printf("a = %d\n b = %d\n c = %d\nd = %d\n", a, b, c, d);
+	return 0;
+}
+//打印的值为1 3 3 4
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int* p = arr;
+	printf("%d\n", arr[5]);
+	printf("%d\n", p[5]);
+	printf("%d\n", *(p + 5));
+	printf("%d\n", *(arr + 5));
+	return 0;
+	//p[5]==*(p+5)
+	//arr[5]==*(arr+5)==*(5+arr)==5[arr]
+}
+//这几个打印的结果都一样的
+
+
+#include<stdio.h>
+int main()
+{
+	char a = 0xb6;//11000110
+	short b = 0xb600;
+	int c = 0xb6000000;
+	if (a == 0xb6)
+		printf("a");
+	if (b == 0xb600)
+		printf("b");
+	if (c == 0xb6000000)
+		printf("c");
+	system("pause");
+	return 0;
+}
+//打印结果为c
+//另外两个都发生整形提升了
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[] = "abcdef";
+	char arr2[] = "abc";
+	if (strlen(arr2) - strlen(arr1)>0)
+		printf("arr2>arr1");
+	else
+		printf("arr2<arr1");
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strlen(const char* str)
+{
+	int count = 0;
+	assert(str);
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+int main()
+{
+	char arr[] = "hello world";
+	int len = my_strlen(arr);
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strlen(const char* str)
+{
+	assert(str);
+	if (*str == '\0')
+		return 0;
+	else
+		return 1 + my_strlen(str + 1);
+}
+int main()
+{
+	char arr[] = "hello world";
+	int len = my_strlen(arr);
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strlen(const char* str)
+{
+	assert(str);
+	return !(*str) ? 0 : 1 + my_strlen(str + 1);
+}
+int main()
+{
+	char arr[] = "hello world";
+	int len = my_strlen(arr);
+	printf("%d\n", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strlen(char* str)
+{
+	assert(str);
+	char* x = str;
+	while (*str++ != '\0')
+	{
+		;
+	}
+	return str - x - 1;
+}
+int main()
+{
+	char arr[] = "hello world";
+	int len = my_strlen(arr);
+	printf("%d", len); 
+	return 0;
+}
+
+
+/*
+编写一个程序，可以一直接收键盘字符
+如果是小写字符就输出对应大写字符
+如果是大写字符，就输出对应的小写字符
+如果是数字，那么就不输出
+*/
+#include<stdio.h>
+int main()
+{
+	int ch = 0;
+	while (ch = getchar() != EOF)
+	{
+		if (ch >= 'a' && ch <= 'z')
+		{
+			putchar(ch - 32);
+		}
+		else if (ch >= 'A' && ch <= 'Z')
+		{
+			putchar(ch + 32);
+		}
+		else
+		{
+			putchar(ch);
+		}
+	}
+}
+
+
+//实现一个函数，打印乘法口诀表，口诀表的行数和列数自己指定
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	(void)scanf("%d", &n);
+	for (int a = 1; a <= n; a++)
+	{
+		for (int b = 1; b <= a; b++)
+		{
+			printf("%d*%d=%2d ", a, b, a * b);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+
+//逆序字符串
+#include<stdio.h>
+void my_reverse(char* str, int sz)
+{
+	int left = 0;
+	int right = sz - 1;
+	while (left < right)
+	{
+		int temp = str[left];
+		str[left] = str[right];
+		str[right] = temp;
+		left++;
+		right--;
+	}
+}
+void Print(char arr[], int sz)
+{
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%c", arr[i]);
+	}
+}
+int main()
+{
+	char arr[] = "hello world";
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	my_reverse(arr, sz);
+	Print(arr, sz);
+	return 0;
+}
+
+
+//获取一个数二进制序列中所有的偶数位和奇数位
+//分别输出二进制序列
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int i = 0;
+	(void)scanf("%d", &n);
+	//偶数位
+	for (i = 31; i >= 1; i -= 2)
+	{
+		printf("%d", (n >> i) & 1);
+	}
+	printf("\n");
+	//奇数位
+	for (i = 30; i >= 0; i -= 2)
+	{
+		printf("%d", (n >> i) & 1);
+	}
+	return 0;
+}
+
+
+//编程实现
+//两个int(32位)整数m和n二进制表达式中
+//有多少个bit不同
+//输入例子
+// 1999 2299
+//输出例子 ：7
+#include<stdio.h>
+int main()
+{
+	int m = 0;
+	int n = 0;
+	int a = 0;
+	int count = 0;
+	(void)scanf("%d %d", &m, &n);
+	a = m ^ n;
+	for (int i = 0; i < 32; i++)
+	{
+		if (((a >> i) & 1) == 1)
+			count++;
+	}
+	printf("%d", count);
+	return 0;
+}
+
+
+/*
+5位运动员参加了10m跳水比赛，有人让他们预测比赛结果
+A选手说：B第二，我第三;
+B选手说，我第二，E第四;
+C选手说，我第一，D第二;
+D选手说，C最后，我第三;
+E选手说，我第四，A第一;
+比赛结束后，每位选手都说对了一半，请编程确定比赛的名次
+*/
+
+/*
+通过这个题，我们可以知道A,B,C,D,E都可能是第一名到第五名的名词，那么我们可以
+通过把所有的可能罗列出来，然后看那种满足每人都说对了一半的情况
+*/
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	int e = 0;
+	for (a = 1; a <= 5; a++)
+	{
+		for (b = 1; b <= 5; b++)
+		{
+			for (c = 1; c <= 5; c++)
+			{
+				for (d = 1; d <= 5; d++)
+				{
+					for (e = 1; e <= 5; e++)
+					{
+						if (((b == 2) + (a == 3) == 1)
+							&& ((b == 2) + (e == 4) == 1)
+							&& ((c == 1) + (d == 2) == 1)
+							&& ((c == 5) + (d == 3) == 1)
+							&& ((e == 4) + (a == 1) == 1))
+						{
+							if (a * b * c * d * e == 120)
+								printf("%d %d %d %d %d\n", a, b, c, d, e);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+
+/*
+日本某地发生了一件谋杀案，警察通过排查确定杀人凶手必为
+四个嫌疑犯中的一个。以下为4个嫌犯的供词：
+A说:不是我
+B说:是C
+C说:是D
+D说:C在胡说
+已知三个人说了真话，一个人说的是假话
+现在请根据这些信息，写一个程序来确定到底谁是凶手
+*/
+#include<stdio.h>
+int main()
+{
+	char killer = 0;
+	for (killer = 'a'; killer <= 'd'; killer++)
+	{
+		if ((killer != 'a') + (killer == 'c') + (killer == 'd') + (killer != 'd') == 3)
+		{
+			printf("killer is %c", killer);
+		}
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[20] = { 0 };
+	char arr2[] = { 'a','b','c' };
+	strcpy(arr1, arr2);
+	printf("%s\n", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[3] = { 0 };
+	char arr2[] = { 'a','b','c' };
+	strcpy(arr1, arr2);
+	printf("%s\n", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char *p="hello world"
+	char arr2[] = { 'a','b','c' };
+	strcpy(p, arr2);
+	printf("%s\n", p);
+	return 0;
+}
+
+
+//strcpy函数的模拟实现
+#include<stdio.h>
+#include<assert.h>
+char* my_strcpy(char* dest, const char* source)
+{
+	char* ret = dest;
+	assert(dest);
+	assert(source);
+	while (*dest++ = *source++)
+	{
+		;
+	}
+	return ret;
+}
+int main()
+{
+	char arr1[20] = { 0 };
+	char arr2[] = "Hello world";
+	my_strcpy(arr1, arr2);
+	printf("%s\n", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[20] = "Hello";
+	char arr2[] = " world";
+	strcat(arr1, arr2);
+	printf("%s\n", arr1);
+	return 0;
+}
+
+
+模拟实现strcat
+#include<stdio.h>
+#include<assert.h>
+char* my_strcat(char* dest, const char* source)
+{
+	char* ret = dest;
+	assert(dest);
+	assert(source);
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*dest++ = *source++)
+	{
+		;
+	}
+	return ret;
+
+}
+int main()
+{
+	char arr1[20] = "Hello";
+	char arr2[] = " world";
+	my_strcat(arr1, arr2);
+	printf("%s\n", arr1);
+	return 0;
+}
