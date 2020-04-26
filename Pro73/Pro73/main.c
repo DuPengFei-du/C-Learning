1ebd62ac373ed347c5aaffb5e8ecd5b6eb9f9687
DuPengFei-du <935820240@qq.com>
2020-04-26 08:35:23 +0800

    test

---
+++ Pro73/Pro73/main.c
@@ -0,0 +1,630 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	(void)scanf("%d %d", &a, &b);
+	c = a + b;
+	printf("%d", c);
+	return 0;
+}
+
+
+#include<stdio.h>
+int Add(int x, int y)
+{
+	return x + y;
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	(void)scanf("%d %d", &a, &b);
+	c = Add(a,b);
+	printf("%d", c);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	printf("%c\n",'\'');
+	printf("%s\n","\"");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	printf("%d\n", sizeof(int));  //4
+	printf("%d\n", sizeof(char)); //1
+	printf("%d\n", sizeof(short)); //2
+	printf("%d\n", sizeof(long));  //4/8
+	printf("%d\n", sizeof(long long));  //8
+	printf("%d\n", sizeof(float));  //4
+	printf("%d\n", sizeof(double));  //8
+	return 0;
+}
+//类型的大小单位都是字节
+//计算机中的单位 
+//bit(位) byte(字节) kbyte(kb) mb gb tb pb  
+//前两个单位之间的换算单位是8，后面的单位之间的是1024
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	{
+//		int num = 10;
+//	}
+//	printf("%d", num);
+//	return 0;
+//	此时，这个程序会出错，num作用域中不包括有printf语句。
+//}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = "abc";
+	char arr2[] = { 'a','b','c',0 };
+	printf("%s\n", "abc");
+	printf("%s\n", arr1);
+	printf("%s\n", arr2);
+	return 0;
+	//三种打印的结果都是abc
+	//如果arr2后面不加0的话，那么打印结果就会是再abc的后面还会出现烫烫烫的字符
+	//因为如果不加0的话，字符串就会并没有结束，只有加了0或者\0才会停止打印
+	//烫烫烫背后的真实东西其实是0xcccccc
+	//由此可以看来\0对字符串来说是非常重要的
+	//字符串是\0需要的结束标志的符号；
+}
+
+
+#include<stdio.h>
+int main()
+{
+	printf("c:\test\test.c\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	printf("%d\n", 10);
+	printf("%d\n", strlen("abc"));
+	printf("%d\n", strlen("c:\test\47\test.c"));
+	return 0;
+	//其中，第三个printf语句的输出结果为13，主要的陷阱就在于\47
+	//这其中的本质还是在考察对于转义字符的理解
+	//\ddd也为转义字符，其中ddd表示1-3个八进制的数字，这三个东西统一的看成一个字符，所以说长度为1.
+	//\xddd  ddd表示3个16进制数字
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	while (i < 10)
+	{
+		printf("%d ", arr[i]);
+		i++;
+	}
+	printf("\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a1 = 5 / 2;
+	float a2 = 5 / 2;
+	float a3 = 5.0 / 2;
+	printf("%d\n", a1);
+	printf("%f\n", a2);
+	printf("%f\n", a3);
+	return 0;
+}
+//针对于除法操作，与他存储的是什么类型是没有关系的
+//主要是看两个除法的操作数到底是什么类型的，如果有一个是浮点型的，得到的结果就会是浮点型的结果
+
+
+#include<stdio.h>
+int main()
+{
+	//int a = 5/2;
+	//int a = 5/2.0;
+	//printf("%d\n", a);
+	//int a = 5%2;
+	//printf("%d\n", a);
+	int a = 15;
+	int b = a << 1;
+	//a = a<<1;
+	printf("%d\n", a);
+	printf("%d\n", b);
+	//<< 移位操作符-移动的是二进制位
+	//原码-反码-补码
+	//00000000000000000000000000001111-原码-反码-补码相同
+	//00000000000000000000000000011110
+	//
+	//-1
+	//10000000000000000000000000000001-原码
+	//11111111111111111111111111111110-反码
+	//11111111111111111111111111111111-补码
+	system("pause");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = a++;
+	printf("%d\n", a);  //11
+	printf("%d\n", b);  //10
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = ++a;
+	printf("%d\n", a);   //11
+	printf("%d\n", b);   //11
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 3;
+	int b = 5;
+	int c = a && b;
+	printf("%d", c);
+	return 0;
+}
+//结果为1
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int max = 0;
+	max = (a > b) ? a : b;
+	printf("%d", max);
+	return 0;
+}
+
+
+#include<stdio.h>
+void test()
+{
+	int n = 1;
+	n++;
+	printf("%d ", n);
+}
+int main()
+{
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		test();
+	}
+	return 0;
+	//屏幕上会出现10个2
+}
+
+
+#include<stdio.h>
+void test()
+{
+	static int n = 1;   //n为局部变量
+	n++;
+	printf("%d ", n);
+}
+int main()
+{
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		test();
+	}
+	return 0;
+}
+//打印的结果是2-11，用static修饰变量，创建了就不销毁了。
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int arr1[10] = { 0 };
+	int arr2[3] = { 1,2,3 };
+	memcpy(arr1, arr2, 12);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+void* my_memcpy(void* dest, const void* source, size_t num)
+{
+	//每次拷贝一个字节最为合适
+	void* ret = dest;
+	assert(dest != NULL);
+	assert(source != NULL);
+	while (num--)
+	{
+		*(char*)dest = *(char*)source;
+		((char*)dest)++;
+		((char*)source)++;
+	}
+	return ret;
+}
+int main()
+{
+	int arr1[10] = { 0 };
+	int arr2[] = { 1,2,3 };
+	my_memcpy(arr1, arr2, 12);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+void* my_memcpy(void* dest, const void* source, size_t num)
+{
+	//每次拷贝一个字节最为合适
+	void* ret = dest;
+	assert(dest != NULL);
+	assert(source != NULL);
+	while (num--)
+	{
+		*(char*)dest = *(char*)source;
+		((char*)dest)++;
+		((char*)source)++;
+	}
+	return ret;
+}
+int main()
+{
+	int arr1[10] = { 0 };
+	int arr2[] = { 1,2,3 };
+	my_memcpy(arr1, arr2, 12);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	(void)scanf("%d", &n);
+	if (n % 2 == 1)
+		printf("奇数");
+	else
+		printf("偶数");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int count = 0;
+	for (int i = 100; i < 200; i++)
+	{
+		if (i % 2 == 1)
+		{
+			count++;
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	printf("%d", count);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	for (int i = 1; i <= 10; i++)
+	{
+		printf("%d ", i);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		printf("%d ", i);
+		i++;
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int left = 0;
+	int right = sz - 1;
+	int key = 9;
+	while (left <= right)
+	{
+		int mid = left + (right - left) / 2;
+		if (arr[mid] < key)
+			left = mid + 1;
+		else if (arr[mid] > key)
+			right = mid - 1;
+		else
+		{
+			printf("找到了，下标为:%d", mid);
+			break;
+		}
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int i = 1;
+	int sum = 1;
+	(void)scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		sum *= i;
+	}
+	printf("%d", sum);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int i = 1;
+	int sum = 0;
+	int ret = 1;
+	(void)scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		ret *= i;
+		sum += ret;
+	}
+	printf("%d", sum);
+	return 0;
+}
+
+
+//100-200之间的素数
+#include<stdio.h>
+int main()
+{
+	int count = 0;
+	int i = 0;
+	int j = 0;
+	for (i = 100; i <= 200; i++)
+	{
+		for (j = 2; j < i; j++)
+		{
+			if (i % j == 0)
+				break;
+		}
+		if (j == i)
+		{
+			count++;
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	printf("%d", count);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int count = 0;
+	int i = 0;
+	int j = 0;
+	for (i = 100; i <= 200; i++)
+	{
+		for (j = 2; j <= sqrt(i) ; j++)
+		{
+			if (i % j == 0)
+				break;
+		}
+		if (j >sqrt(i))
+		{
+			count++;
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	printf("%d", count);
+	return 0;
+}
+
+
+//最大公约数
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 1;
+	(void)scanf("%d %d", &a, &b);
+	while (c = a % b)
+	{
+		a = b;
+		b = c;
+	}
+	printf("%d", b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int age = 0;
+	(void)scanf("%d", &age);
+	if (age < 18)
+		printf("未成年\n");
+	else
+		printf("成年\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int age = 10;
+	if (age == 5)
+		printf("age==5\n");
+	return 0;
+}
+//屏幕上什么都不会打印，因为age！=5；
+
+
+#include<stdio.h>
+int main()
+{
+	int age = 10;
+	if (age = 5)
+		printf("age==5\n");
+	return 0;
+}
+//频幕上会打印age==5，因为把5赋值给了age，所以if后面的括号里面的值是真的，所以回打印要求的信息
+
+
+#include<stdio.h>
+int main()
+{
+	int day = 0;
+	scanf("%d", &day);
+	switch (day)
+	{
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+		printf("weekday\n");
+		break;
+	case 6:
+	case 7:
+		printf("weekend\n");
+		break;
+	default:
+		printf("选择错误\n");
+		break;
+	}
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		i++;
+		if (i == 5)
+			continue;
+		printf("%d ", i);
+	}
+	return 0;
+}
+//打印结果为2，3，4，5，7，8，9，10，11
+//continue作用：continue用来终止本次的循环，当次continue之后的语句不再执行。
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	for (a = 1; a <= 9; a++)
+	{
+		for (b = 1; b <= a; b++)
+		{
+			printf("%d*%d=%2d ", a, b, a * b);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int i = 0;
+	char password[20] = { 0 };
+	for (i = 0; i < 3; i++)
+	{
+		printf("请输入密码:>");
+		scanf("%s", password);
+		if (0 == strcmp(password, "123456"))
+		{
+			printf("登陆成功\n");
+			break;
+		}
+		else
+		{
+			printf("密码错误，请重新输入！\n");
+		}
+	}
+	if (i == 3)
+		printf("三次密码均错误，退出程序！\n");
+	return 0;
+}
