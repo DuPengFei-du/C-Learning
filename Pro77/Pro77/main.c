3e5a020bd2a3f9d956c1a9b8a34d6d5654f6c7b5
DuPengFei-du <935820240@qq.com>
2020-05-09 08:52:23 +0800

    test

---
+++ Pro77/Pro77/main.c
@@ -0,0 +1,441 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include<stdio.h>
+int main()
+{
+	int arr1[10];
+	char arr2[5];
+	return 0;
+}
+//[]中必须是常量或者常量表达式，不然就会报错
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5 };
+	return 0;
+}
+//没有被赋值的元素则默认为为0
+//通过监视窗口来看
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = { 'a','b','c' };
+	char arr2[] = "abc";
+	printf("%s", arr1);
+	printf("%s", arr2);
+	return 0;
+}
+//这两个数组的是不一样的
+//arr1是不可以看成数组的，因为它没有\0，\0是字符串的结束标志
+//如果没有\0是不可以看成字符串的
+//arr2是可以看成字符串的，c后面还有一个隐藏的\0
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int i = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (i = 0; i < sz; i++)
+	{
+		printf("&arr[%d]=%p\n", i, &arr[i]);
+	}
+	return 0;
+}
+//每个元素地址之间差的值是4
+//至于为什么差4，因为我是一个整形的数组，每个元素占有4个字节。
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int* p = arr;  //得到了首元素的地址
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%p===%p\n", &arr[i], p + i);
+	}
+	return 0;
+}
+//这个代码就说明：p+i为下标为i的元素的地址
+//*(p + i)为下标为i的元素的值
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int* p = arr;
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", *(p + i));
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[3][4] = { 0 };
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[3][4] = { 0 };
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			printf("&arr[%d][%d]=%p\n", i, j, &arr[i][j]);
+		}
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+void BubbleSort(int arr[], int sz)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < sz - 1; i++)
+	{
+		for (j = 0; j < sz - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
+int main()
+{
+	int i = 0;
+	int arr[10] = { 1,3,5,7,9,2,4,6,8,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	BubbleSort(arr, sz);
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	printf("%p\n", &arr);
+	printf("%p\n", &arr + 1);
+	return 0;
+}
+//这两行之间的地址值的差值为40，刚好为数组的大小
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	printf("%d", sizeof(arr));
+	return 0;
+}
+//这个代码的结果是40
+
+
+#include<stdio.h>
+int main()
+{
+	//数组也是一种自定义类型
+	int arr[10] = { 0 };
+	printf("%d\n", sizeof(arr));                //40
+	printf("%d\n", sizeof(int[10]));          //40
+	int a = 10;
+	sizeof(int);  //可以的
+	sizeof(a);    //可以的
+	//那么问题来了，数组的类型是什么呢？
+	//数组的类型，就是数组去掉数组名剩下的部分
+	//比如上述对数组的声明 int arr[10],那么数组的类型就是int [10]
+}
+
+
+#include<stdio.h>
+void test()
+{
+	printf("hehe");
+}
+int main()
+{
+	test(20);
+	return 0;
+}
+//屏幕上打印的结果是hehe，虽然test函数实际上是没有参数的，但是在主函数调用test函数时候，还是传了参数，但是是没有任何影响的。
+//你传是你的事情，我接不接受是我的事情。
+//但是如果没有参数的话，最好还是在test函数的括号中加一个void，声明这个函数是没有参数的。
+
+
+#include<stdio.h>
+int main()
+{
+	//加入说我想算一个1-1的结果是多少
+	//但是遗憾的是，计算机不能算减法
+	//那我只好把减法转换为加法的形式
+	//1 - 1;
+	1 + (-1);
+	//如果按原码来计算的话
+	// 1的原码
+	//00000000000000000000000000000001
+	//-1的原码
+	//10000000000000000000000000000001
+	//两个码相加的结果为
+	//10000000000000000000000000000010
+	//如果按照原码直接相加的话，结果就为-2，这显然是不正确的
+	//所以科学家们推出了补码的概念
+	//用补码来算的时候，可以算出正确的结果
+}
+
+
+//判断机器的大小端问题
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	if (*pa == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	if (*pa == 1)
+		return 1;
+	else
+		return 0;
+}
+int main()
+{
+	int ret = 0;
+	ret = CheckSystem();
+	if (ret == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	union Un
+	{
+		int i;
+		char c;
+	}u;
+	u.i = 1;
+	return u.c;
+}
+int main()
+{
+	int ret = 0;
+	ret = CheckSystem();
+	if (ret == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+struct Stu
+{
+	char name[20];
+	int age;
+};
+union Un
+{
+	int i;
+	char c;
+};    //联合体类型
+int main()
+{
+	union Un u = { 0 };   //u为联合体对象
+	printf("%d\n", sizeof(u));   //结果为4
+	printf("%p\n", &u);
+	printf("%p\n", &(u.i));
+	printf("%p\n", &(u.c));
+	//三个的地址是一模一样的，说明i和c占用同一块空间
+	//所以联合体又叫共用体，共用体的意思就是我的成员要共用同一块空间
+	//那么我给i里面放东西c就会发生变化
+	//我给c里面放东西，i也会发生变化
+	//c是i占的四个字节中的第一个字节，这也解决了大小端算法的另一种解法问题
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char a = -1;
+	signed char b = -1;
+	//char和signed char其实是一样得，所以前两个得结果一定是一样的
+	//-1的补码为：11111111111111111111111111111111
+	//保存到char中为11111111
+	//反码为：11111110
+	//原码为：10000001  --所以说前两个的结果为-1 和 -1
+	//有符号的类型，高位所需要补的是符号位
+	unsigned char c = -1;
+	//c放在内存中 11111111---补码
+	//又因为c是无符号数，所以给c的高位补0
+	//00000000000000000000000011111111  -- 补码
+	//又因为最高位是0，所以说为正数，正数的原，反，补相同
+	//所以c的结果为255  8个1的结果为255
+	printf("a=%d,b=%d,c=%d", a, b, c);
+	//-1 -1 255
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char a = -128;
+	//10000000000000000000000010000000   原码-- -128
+	//11111111111111111111111101111111   反码
+	//11111111111111111111111110000000   补码
+	//但是只能存8个比特位，结果为10000000
+	//需要进行整形提升
+	//提升的时候需要看原来的符号位，全部补成1
+	//结果为11111111111111111111111110000000  --补码
+	//又因为是无符号数，所以原码，反码，补码全部相同
+	//所以就得到了结果：4294967168
+	printf("%u\n", a);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char a = 128;
+	printf("%u\n", a);
+	return 0;
+}
+//char的范围是-128~+127,所以char是无法表示+128的
+
+
+#include<stdio.h>
+int main()
+{
+	int i = -20;
+	unsigned  int  j = 10;
+	printf("%d\n", i + j);
+	//10000000000000000000000000010100
+	//11111111111111111111111111101011
+	//11111111111111111111111111101100
+	//00000000000000000000000000001010
+	//11111111111111111111111111110110
+	//11111111111111111111111111110101
+	//10000000000000000000000000001010
+	//-10
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char a[1000];
+	int i;
+	for (i = 0; i < 1000; i++)
+	{
+		a[i] = -1 - i;
+	}
+	printf("%d", strlen(a));
+	//什么时候遇到\0，长度就是多少，参考char的范围的哪个圆圈
+	//所以结果为255
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 9;
+	//9存在内存中，存储的时他的补码
+	//00000000000000000000000000001001
+	float* pFloat = (float*)& n;
+	//要把一个整形，转换层成一个浮点型，那么现在就来转换一下
+	//0 00000000 00000000000000000001001
+	//又因为上述浮点型的存储中E全为0，所以这个数字是一个无限接近于0的数字 ，所以打印的结果为0.000000
+	printf("n的值为:%d\n", n);
+	//以整形的形式存入，再以整形的形式取出，那么结果当然还是整形了，所以说结果为9;
+	printf("*pFloat的值为:%f\n", *pFloat);
+	//要把一个整形，转换层成一个浮点型，那么现在就来转换一下
+	//0 00000000 00000000000000000001001
+	//又因为上述浮点型的存储中E全为0，所以这个数字是一个无限接近于0的数字 ，所以打印的结果为0.000000
+	*pFloat = 9.0;
+	printf("n的值为:%d\n", n);
+	//以浮点数的形式存进去，以浮点数的形式往出拿
+	//先将9写成二进制的形式
+	//1001.0
+	//1.001*2^3;
+	//0 100000010 00100000000000000000000  --补码
+	//整数，原，反，补相同
+	//所以结果为010000001000100000000000000000000  --结果为1091567616
+	printf("*pFloat的值为:%f\n", *pFloat);
+	//以浮点数形式存进去，再以浮点是结果拿出来，结果是不会变化的
+	//结果为9.000000
+	return 0;
+}
