112f2530b32170ac957e855b10051c93ee38de60
DuPengFei-du <935820240@qq.com>
2020-05-18 07:48:49 +0800

    test

---
+++ Pro80/Pro80/main.c
@@ -0,0 +1,344 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include<stdio.h>
+struct Stu
+{
+	//成员变量
+	//成员列表
+	char name[20];
+	short age;
+	char sex[5];
+	float score;
+}s1, s2, s3;  //学生s1,s2,s3   //创建了三个学生出来
+int main()
+{
+	struct Stu s;
+	return 0;
+}
+
+
+#include<stdio.h>
+struct Stu
+{
+	//成员变量
+	//成员列表
+	char name[20];
+	short age;
+	char sex[5];
+	float score;
+};
+int main()
+{
+	struct Stu s;
+	return 0;
+}
+
+
+#include<stdio.h>
+typedef struct Stu
+{
+	char name[20];
+	short age;
+	char sex[5];
+	float score;
+}Stu;   //Stu==struct Stu  此处得Stu为类型名，不是变量名
+int main()
+{
+	return 0;
+}
+
+
+#include<stdio.h>
+struct Point
+{
+	int x;
+	int y;
+	//注意结构体中不能包含自己这一结构体，但是可以包含别的结构体
+	//包含自己这个结构体，在算结构体大小的时候就不好算了
+};
+struct Test
+{
+	int a;
+	char arr[10];
+	int* p;
+	struct Point;
+};
+int main()
+{
+	return 0;
+}
+
+
+#include<stdio.h>
+struct Point
+{
+	int x;
+	int y;
+}p1;  //声明类型的同时定义p1
+int main()
+{
+	struct Point p2;     //定义结构体变量p2
+	return 0;
+}
+//通常采用p2的方式
+
+
+#include<stdio.h>
+struct Point
+{
+	int x;
+	int y;
+};
+struct T
+{
+	char arr[10];
+	int age;
+	struct Point p;
+	int arr2[5];
+};
+int main()
+{
+	struct Point p = { 1,2 };
+	struct T t = { "hehe",20,{2,4},{1,2,3,4} };
+	printf("%s %d %d %d %d %d\n", t.arr, t.age, t.p.x, t.p.y, t.arr2[0], t.arr2[1]);
+	return 0;
+}
+
+
+struct Stu
+{
+	char name[20];
+	int age;
+};
+//如果只有这些代码的话，他是没有向内存中申请空间的。
+struct Stu s = { "zhangsan",20 };
+//有了上面的创建s对象的那句语句，才算向内存中申请了一定的空间。
+//想找到name的话，就用s.name，想找到age的话，就用s.age
+
+
+#include<stdio.h>
+struct Stu
+{
+	char name[20];
+	int age;
+};
+void Print(struct Stu* s)  //结构体指针
+{
+	printf("%s %d", s->name, s->age);
+}
+int main()
+{
+	struct Stu s = { "张三",20 };
+	Print(&s);
+	return 0;
+}
+
+
+#include<stdio.h>
+struct Stu
+{
+	char name[20];
+	int age;
+};
+void Print(struct Stu s)  //结构体指针
+{
+	printf("%s %d", s.name, s.age);
+}
+int main()
+{
+	struct Stu s = { "张三",20 };
+	Print(s);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	//数组也是一种自定义类型
+	int arr[10] = { 0 };
+	printf("%d\n", sizeof(arr));                //40
+	printf("%d\n", sizeof(int[10]));          //40
+	int a = 10;
+	sizeof(int);  //可以的
+	sizeof(a);    //可以的
+	//那么问题来了，数组的类型是什么呢？
+	//数组的类型，就是数组去掉数组名剩下的部分
+	//比如上述对数组的声明 int arr[10],那么数组的类型就是int [10]
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = -10;
+	//10000000000000000000000000001010  --原码
+	//11111111111111111111111111110101  --反码
+	//11111111111111111111111111110110  --补码
+	//fffffff6               ---补码转换成16进制的样子
+	//但是在内存中&a显示的结果是f6ffffff
+}
+
+
+#include<stdio.h>
+int main()
+{
+	//加入说我想算一个1-1的结果是多少
+	//但是遗憾的是，计算机不能算减法
+	//那我只好把减法转换为加法的形式
+	//1 - 1;
+	1 + (-1);
+	//如果按原码来计算的话
+	// 1的原码
+	//00000000000000000000000000000001
+	//-1的原码
+	//10000000000000000000000000000001
+	//两个码相加的结果为
+	//10000000000000000000000000000010
+	//如果按照原码直接相加的话，结果就为-2，这显然是不正确的
+	//所以科学家们推出了补码的概念
+	//用补码来算的时候，可以算出正确的结果
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0x11223344;
+	//0x11223344放在a里面是刚刚好的
+	//因为11是一个字节，22是一个字节，33是一个字节，44是一个字节
+	//通过监视看：转换到16进制，观察到a的值为0x1223344
+	//通过内存看：输入&a，可以观察到，a显示为44332211
+	return 0;
+}
+
+
+//解题思路：
+//我们可以通过把1存在内存中，然后通过看他的存储形式，我们来判断当前机器到底是大端还是小端
+//1的16形式的格式为00000001
+//如果存储形式是01000000那么就是小端
+//如果存储形式是00000001那么就是大段
+//我们可以通过读取第一个字节的内容，来判断到底是大端还是小端，想要读取一个整形的第一个字节，那么存储在char*类型的变量中就可以了。
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	if (*pa == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+//封装成一个函数
+#include<stdio.h>
+int check_system()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	if (*pa == 1)
+		return 1;
+	else
+		return 0;
+}
+int main()
+{
+	int ret = check_system();
+	if (ret == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+struct Stu
+{
+	char name[20];
+	int age;
+};
+union Un
+{
+	int i;
+	char c;
+};    //联合体类型
+int main()
+{
+	union Un u = { 0 };   //u为联合体对象
+	printf("%d\n", sizeof(u));   //结果为4
+	printf("%p\n", &u);
+	printf("%p\n", &(u.i));
+	printf("%p\n", &(u.c));
+	//三个的地址是一模一样的，说明i和c占用同一块空间
+	//所以联合体又叫共用体，共用体的意思就是我的成员要共用同一块空间
+	//那么我给i里面放东西c就会发生变化
+	//我给c里面放东西，i也会发生变化
+	//c是i占的四个字节中的第一个字节，这也解决了大小端算法的另一种解法问题
+	return 0;
+}
+
+
+#include<stdio.h>
+int check_system()
+{
+	union Un
+	{
+		int i;
+		char c;
+	}u;
+	u.i = 1;
+	return u.c;
+}
+int main()
+{
+	int ret = check_system();
+	if (ret == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char a = -1;
+	signed char b = -1;
+	//char和signed char其实是一样得，所以前两个得结果一定是一样的
+	//-1的补码为：11111111111111111111111111111111
+	//保存到char中为11111111
+	//反码为：11111110
+	//原码为：10000001  --所以说前两个的结果为-1 和 -1
+	//有符号的类型，高位所需要补的是符号位
+	unsigned char c = -1;
+	//c放在内存中 11111111---补码
+	//又因为c是无符号数，所以给c的高位补0
+	//00000000000000000000000011111111  -- 补码
+	//又因为最高位是0，所以说为正数，正数的原，反，补相同
+	//所以c的结果为255  8个1的结果为255
+	printf("a=%d,b=%d,c=%d", a, b, c);
+	//-1 -1 255
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int i = -20;
+	unsigned  int  j = 10;
+	printf("%d\n", i + j);
+	//10000000000000000000000000010100
+	//11111111111111111111111111101011
+	//11111111111111111111111111101100
+	//00000000000000000000000000001010
+	//11111111111111111111111111110110
+	//11111111111111111111111111110101
+	//10000000000000000000000000001010
+	//-10
+	return 0;
+}
