2b2a94fbbb23f6c6e0c206aacb05a6f7ab3acd3a
DuPengFei-du <935820240@qq.com>
2020-06-02 07:55:45 +0800

    test

---
+++ Pro89/Pro89/main.c
@@ -0,0 +1,540 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	printf("%d", sizeof(arr));
+	return 0;
+}
+//这个代码的结果是40
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	printf("%p\n", &arr);
+	printf("%p\n", &arr + 1);
+	return 0;
+}
+//这两行之间的地址值的差值为40，刚好为数组的大小
+
+
+#include<stdio.h>
+int main()
+{
+	int a[5] = { 1, 2, 3, 4, 5 };
+	int* ptr = (int*)(&a + 1);  //加1跳过了整个数组，强制转换成了int* 
+	printf("%d,%d", *(a + 1), *(ptr - 1));  //-1向前挪动一个，指向5的地址，解引用之后
+	//得到的就是5的值
+	return 0;
+	//结果为2 5
+}
+
+
+struct Test
+{
+	int Num;
+	char* pcName;
+	short sDate;
+	char cha[2];
+	short sBa[4];
+}*p;
+//假设p 的值为0x100000。 如下表表达式的值分别为多少？
+//结构体大小为20个字节
+int main()
+{
+	p = (struct Test*)0x100000;
+	//这道题主要考察的点在于指针+1的权限到底有多大
+	//指针的权限是和指针的类型密切相关的
+	printf("%p\n", p + 0x1);
+	//p作为一个指针变量，p的大小是20个字节，那么对于此处来说，p+1，它所能够访问的权限就是20个字节
+	//所以p+1，加的就是20个字节，0x是16进制，20在16进制下就是14，又因为p为0x100000,所以它+14的结果是
+	//0x100014
+	printf("%p\n", (unsigned long)p + 0x1);
+	//此处p被强制类型转换成了整形，所以p+1在此处就是p+1进行整数运算，所以结果为
+	//0x100001
+	printf("%p\n", (unsigned int*)p + 0x1);
+	//此处p被强制类型转换成了指针类型，整形指针+1所拥有的权限是4个字节，那么，p+1的结果是
+	//0x100014
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a[4] = { 1, 2, 3, 4 };
+	int* ptr1 = (int*)(&a + 1);
+	int* ptr2 = (int*)((int)a + 1);
+	printf("%x,%x", ptr1[-1], *ptr2);
+	return 0;
+}
+
+
+#include <stdio.h>
+int main()
+{
+	int a[3][2] = { (0, 1), (2, 3), (4, 5) };
+	int* p;
+	p = a[0];
+	printf("%d", p[0]);
+	return 0;
+	//打印的结果是1，考察的点是逗号表达式，利用逗号表达式对数组进行初始化
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a[5][5];
+	int(*p)[4];
+	p = a;
+	printf("%p,%d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int aa[2][5] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int* ptr1 = (int*)(&aa + 1);
+	//&数组名，代表的是整个数组，整个数组加1，位置来到了10的后面
+	//然后再-1，位置来到了10，然后再解引用就得到10位置处的值
+	//所以结果就为10
+	int* ptr2 = (int*)(*(aa + 1));
+	//首元素的地址加1，位置来到了第二行，然后进行解引用，相当于拿到了第二行
+	//也就是拿到了6的地址，然后-1，到了5的地址处，然后解引用
+	//得到了5的值，所以结果为5
+	printf("%d,%d", *(ptr1 - 1), *(ptr2 - 1));
+	return 0;
+	//结果为 10 和 5 
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char* a[] = { "work","at","alibaba" };
+	char** pa = a;
+	pa++;
+	printf("%s\n", *pa);
+	return 0;
+	//结果为at
+	//a为指针数组
+	//pa+1,跳过一个char*的大小
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char* c[] = { "ENTER","NEW","POINT","FIRST" };
+	char** cp[] = { c + 3,c + 2,c + 1,c };
+	char*** cpp = cp;
+	printf("%s\n", **++cpp);
+	printf("%s\n", *-- * ++cpp + 3);
+	printf("%s\n", *cpp[-2] + 3);
+	printf("%s\n", cpp[-1][-1] + 1);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int temp = 0;
+	(void)scanf("%d %d", &a, &b);
+	temp = a;
+	a = b;
+	b = temp;
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int count = 0;
+	(void)scanf("%d", &n);
+	while (n)
+	{
+		count++;
+		n = n & (n - 1);
+	}
+	printf("%d", count);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int  count = 0;
+	(void)scanf("%d", &n);
+	for (int i = 0; i < 32; i++)
+	{
+		if (((n >> i) & 1) == 1)
+		{
+			count++;
+		}
+	}
+	printf("%d", count);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = a << 1;
+	//<<左移操作符
+	//左边丢弃，右边补0
+	printf("%d\n", b);
+	printf("%d\n", a);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = -1;
+	int b = a << 1;
+	//整数在内存中存储的是二进制补码
+	//-1的在内存中存储，就是存储32个1，32个1为-1的补码。
+	//正数的原，反，补码相同
+	//负数：写先原码，最高位为符号位，负数的最高位为1，负数的符号位为1，正数为0
+	//反码：在原码的基础上，符号位不动，其他位按位取反，得到反码
+	//反码+1得到补码
+	printf("%d\n", b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = -1;
+	int b = a >> 1;
+	printf("%d\n", b);
+	return 0;
+	//采用的是算数右移
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 16;
+	int b = a >> 1;
+	printf("%d", b);
+	return 0;
+}
+//结果是8
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 3;   //011
+	int b = 5;   //101
+	int c = a & b;    //001
+	printf("%d\n", c);
+	return 0;
+	//结果是1
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 3;  //011
+	int b = 5;  //101
+	int c = a ^ b;   //110
+	printf("%d\n", c);
+	return 0;
+}
+//结果为6
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	a = a + 2;
+	a += 2;
+	printf("%d\n", a);
+	return 0;
+}
+//其他的诸如此类都是这样的
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = !a;
+	printf("%d\n", b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	printf("%p\n", &a);
+	return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	printf("%p\n", &a);   //003DF718
+	printf("%x\n", &a);   //3df718
+	return 0;
+}
+//%x表示用16进制打印，如果用16进制打印，前面就会少00
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int* p = &a;
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	printf("%p\n", arr);      //首元素的地址
+	printf("%p\n", &arr[0]); //首元素的地址
+	printf("%p\n", &arr);    //数组的地址
+	return 0;
+}
+//三个的值均为00FBFCF0
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	printf("%d\n", sizeof(a));  //4
+	printf("%d\n", sizeof(int));   //4
+	return 0;
+}
+printf("%d\n",sizeof a);
+sizeof可以对变量省略括号，不能对类型省略括号
+
+
+#include<stdio.h>
+void test(int arr[])
+{
+	printf("%d\n", sizeof(arr));   //数组传参传过来的是首元素的地址，所以结果是4
+	//要用指针接收
+}
+int main()
+{
+	int arr[10] = { 0 };
+	test(arr);
+}
+
+
+#include<stdio.h>
+int main()
+{
+	short a = 10;
+	int b = 3;
+	printf("%d\n", sizeof(a = b + 5));  //2
+	printf("%d\n", a);         //10
+	return 0;
+}
+//值得需要注意的是：
+//sizeof是运算符，并不是函数。
+//所以sizeof括号中的内容只能算的上是传参，括号中的表达式是不会被计算的。
+//所以a的值是并没有发生改变的。
+//把int类型的东西非要放在short类型中，那当然是short说了算的，所以sizeof算出来的字节数的结果是2
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = ~a;
+	printf("%d\n", b);
+	return 0;
+	//结果是-11
+	//00000000000000000000000000001010  --补码
+	//11111111111111111111111111110101  --按位取反后的补码
+	//11111111111111111111111111110100  --反码
+	//10000000000000000000000000001011  --原码
+}
+
+
+#include <stdio.h>
+void test1(int arr[])
+{
+	printf("%d\n", sizeof(arr));//(2)   //用指针接收的，大小为4
+}
+void test2(char ch[])
+{
+	printf("%d\n", sizeof(ch));//(4)  //这里是一个易错的点，他是用指针接收的
+	//所以大小还是4，与char是没有任何关系的
+}
+int main()
+{
+	int arr[10] = { 0 };
+	char ch[10] = { 0 };
+	printf("%d\n", sizeof(arr));//(1)
+	printf("%d\n", sizeof(ch));//(3)
+	test1(arr);
+	test2(ch);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 3;
+	int b = 5;
+	int c = a && b;
+	printf("%d\n", c);
+	return 0;
+}
+//结果为1，逻辑与只看数字的真假
+
+
+#include <stdio.h>
+int main()
+{
+	int i = 0, a = 0, b = 2, c = 3, d = 4;
+	i = a++ || ++b || d++;
+	printf("a = %d\n b = %d\n c = %d\nd = %d\n", a, b, c, d);
+	return 0;
+}
+//打印的值为1 3 3 4
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int* p = arr;
+	printf("%d\n", arr[5]);
+	printf("%d\n", p[5]);
+	printf("%d\n", *(p + 5));
+	printf("%d\n", *(arr + 5));
+	return 0;
+	//p[5]==*(p+5)
+	//arr[5]==*(arr+5)==*(5+arr)==5[arr]
+}
+//这几个打印的结果都一样的
+
+
+#include<stdio.h>
+int main()
+{
+	char a = 127;
+	char b = 3;
+	char c = a + b;
+	//00000000000000000000000001111111  --a的32位形式下的存储
+	//00000000000000000000000001000011  --b的32位形式下的存储
+	//但是a和b都是char类型的，char之由一个字节，所以a，b为：
+	//01111111  --a
+	//00000011  --b
+	//字符在算的时候都达不到整形的大小，所以要先把他们提升成整形的大小，然后再进行运算
+	//a，b均为有符号的char类型，所以最高位是符号位
+	//所以需要把a首先补成这个样子，00000000000000000000000001111111
+	//b是同理，b变成了，00000000000000000000000001000011
+	//将a和b进行相加：
+	//00000000000000000000000001111111
+	//00000000000000000000000001000011
+	//00000000000000000000000010000010   ---a和b相加的结果
+	//a+b的结果要存在c里面，只能存在有8个比特位，结果为10000010
+	//对c进行整形提升，要补的是符号位，补完的结果如下所示:
+	//11111111111111111111111110000010 ---再内存中放的是补码，打印的是原码
+	//补码-1是反码
+	//反码为：11111111111111111111111110000001
+	//原码为：10000000000000000000000001111110  --结果为-126
+	printf("%d", c);   //---126
+	return 0;
+}
+//无符号数进行提升的时候，只能补0
+
+
+#include<stdio.h>
+int main()
+{
+	char a = 0xb6;//11000110
+	short b = 0xb600;
+	int c = 0xb6000000;
+	if (a == 0xb6)
+		printf("a");
+	if (b == 0xb600)
+		printf("b");
+	if (c == 0xb6000000)
+		printf("c");
+	system("pause");
+	return 0;
+}
+//打印结果为c
+//另外两个都发生整形提升了
+
+
+#include<stdio.h>
+int fun()
+{
+	static int count = 1;
+	return ++count;
+}
+int main()
+{
+	int answer;
+	answer = fun() - fun() * fun();
+	printf("%d\n", answer);
+	return 0;
+}
+//在vs上的输出结果为-10  2-3*4=-10
