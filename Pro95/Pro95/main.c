c2592f9d2ce4eaa80e42ee203256507be6466903
DuPengFei-du <935820240@qq.com>
2020-06-17 07:53:41 +0800

    test

---
+++ Pro95/Pro95/main.c
@@ -0,0 +1,632 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include<stdio.h>
+int main()
+{
+	//数组也是一种自定义类型
+	int arr[10] = { 0 };
+	printf("%d\n", sizeof(arr));                //40
+	printf("%d\n", sizeof(int[10]));          //40
+	int a = 10;
+	sizeof(int);  //可以的
+	sizeof(a);    //可以的
+	//那么问题来了，数组的类型是什么呢？
+	//数组的类型，就是数组去掉数组名剩下的部分
+	//比如上述对数组的声明 int arr[10],那么数组的类型就是int [10]
+}
+
+
+#include<stdio.h>
+void test()
+{
+	printf("hehe");
+}
+int main()
+{
+	test(20);
+	return 0;
+}
+//屏幕上打印的结果是hehe，虽然test函数实际上是没有参数的，但是在主函数调用test函数时候，还是传了参数，但是是没有任何影响的。
+//你传是你的事情，我接不接受是我的事情。
+//但是如果没有参数的话，最好还是在test函数的括号中加一个void，声明这个函数是没有参数的。
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0x11223344;
+	//0x11223344放在a里面是刚刚好的
+	//因为11是一个字节，22是一个字节，33是一个字节，44是一个字节
+	//通过监视看：转换到16进制，观察到a的值为0x11223344
+	//通过内存看：输入&a，可以观察到，a显示为44332211
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = -10;
+	//10000000000000000000000000001010  --原码
+	//11111111111111111111111111110101  --反码
+	//11111111111111111111111111110110  --补码
+	//fffffff6               ---补码转换成16进制的样子
+	//但是在内存中&a显示的结果是f6ffffff
+}
+
+
+//判断到底是大端字节序还是小端字节序
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	if (*pa == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	int a = 1;
+	char *pa = (char*)& a;
+	return *pa;
+}
+int main()
+{
+	int ret = CheckSystem();
+	if (ret == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	union Un
+	{
+		int i;
+		char c;
+	}u;
+	u.i = 1;
+	return u.c;
+}
+int main()
+{
+	int ret = CheckSystem();
+	if (ret == 1)
+		printf("小端");
+	else
+		printf("大端");
+	return 0;
+}
+
+
+#include<stdio.h>
+struct Stu
+{
+	char name[20];
+	int age;
+};
+union Un
+{
+	int i;
+	char c;
+};    //联合体类型
+int main()
+{
+	union Un u = { 0 };   //u为联合体对象
+	printf("%d\n", sizeof(u));   //结果为4
+	printf("%p\n", &u);
+	printf("%p\n", &(u.i));
+	printf("%p\n", &(u.c));
+	//三个的地址是一模一样的，说明i和c占用同一块空间
+	//所以联合体又叫共用体，共用体的意思就是我的成员要共用同一块空间
+	//那么我给i里面放东西c就会发生变化
+	//我给c里面放东西，i也会发生变化
+	//c是i占的四个字节中的第一个字节，这也解决了大小端算法的另一种解法问题
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char a = -1;
+	signed char b = -1;
+	//char和signed char其实是一样得，所以前两个得结果一定是一样的
+	//-1的补码为：11111111111111111111111111111111
+	//保存到char中为11111111
+	//反码为：11111110
+	//原码为：10000001  --所以说前两个的结果为-1 和 -1
+	//有符号的类型，高位所需要补的是符号位
+	unsigned char c = -1;
+	//c放在内存中 11111111---补码
+	//又因为c是无符号数，所以给c的高位补0
+	//00000000000000000000000011111111  -- 补码
+	//又因为最高位是0，所以说为正数，正数的原，反，补相同
+	//所以c的结果为255  8个1的结果为255
+	printf("a=%d,b=%d,c=%d", a, b, c);
+	//-1 -1 255
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char a = -128;
+	//10000000000000000000000010000000   原码-- -128
+	//11111111111111111111111101111111   反码
+	//11111111111111111111111110000000   补码
+	//但是只能存8个比特位，结果为10000000
+	//需要进行整形提升
+	//提升的时候需要看原来的符号位，全部补成1
+	//结果为11111111111111111111111110000000  --补码
+	//又因为是无符号数，所以原码，反码，补码全部相同
+	//所以就得到了结果：4294967168
+	printf("%u\n", a);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int i = -20;
+	unsigned  int  j = 10;
+	printf("%d\n", i + j);
+	//10000000000000000000000000010100
+	//11111111111111111111111111101011
+	//11111111111111111111111111101100
+	//00000000000000000000000000001010
+	//11111111111111111111111111110110
+	//11111111111111111111111111110101
+	//10000000000000000000000000001010
+	//-10
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	unsigned int i;
+	//无符号的数，是没有负数的
+	//判断条件为i>=0，但是无符号的，i>=0是永恒成立的
+	//所以结果为死循环
+	for (i = 9; i >= 0; i--)
+	{
+		printf("%u\n", i);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char a[1000];
+	int i;
+	for (i = 0; i < 1000; i++)
+	{
+		a[i] = -1 - i;
+	}
+	printf("%d", strlen(a));
+	//什么时候遇到\0，长度就是多少，参考char的范围的哪个圆圈
+	//所以结果为255
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 9;
+	//9存在内存中，存储的时他的补码
+	//00000000000000000000000000001001
+	float* pFloat = (float*)& n;
+	//要把一个整形，转换层成一个浮点型，那么现在就来转换一下
+	//0 00000000 00000000000000000001001
+	//又因为上述浮点型的存储中E全为0，所以这个数字是一个无限接近于0的数字 ，所以打印的结果为0.000000
+	printf("n的值为:%d\n", n);
+	//以整形的形式存入，再以整形的形式取出，那么结果当然还是整形了，所以说结果为9;
+	printf("*pFloat的值为:%f\n", *pFloat);
+	//要把一个整形，转换层成一个浮点型，那么现在就来转换一下
+	//0 00000000 00000000000000000001001
+	//又因为上述浮点型的存储中E全为0，所以这个数字是一个无限接近于0的数字 ，所以打印的结果为0.000000
+	*pFloat = 9.0;
+	printf("n的值为:%d\n", n);
+	//以浮点数的形式存进去，以浮点数的形式往出拿
+	//先将9写成二进制的形式
+	//1001.0
+	//1.001*2^3;
+	//0 100000010 00100000000000000000000  --补码
+	//整数，原，反，补相同
+	//所以结果为010000001000100000000000000000000  --结果为1091567616
+	printf("*pFloat的值为:%f\n", *pFloat);
+	//以浮点数形式存进去，再以浮点是结果拿出来，结果是不会变化的
+	//结果为9.000000
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	(void)scanf("%d", &n);
+	if (n % 2 == 0)
+		printf("偶数\n");
+	else
+		printf("奇数\n");
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int count = 0;
+	for (int i = 100; i <= 200; i++)
+	{
+		if (i % 2 == 1)
+		{
+			count++;
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	printf("%d", count);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int age = 0;
+	(void)scanf("%d", &age);
+	if (age < 18)
+		printf("未成年\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int age = 0;
+	(void)scanf("%d", &age);
+	if (age < 18)
+		printf("未成年\n");
+	else
+		printf("成年\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 2;
+	if (a == 1)
+		if (b == 2)
+			printf("hehe\n");
+		else
+			printf("haha\n");
+	return 0;
+}
+//这段代码什么都不会打印 第二个else会在编译器中主动和第二个if靠近
+//所以代码什么都不会打印，没有符号的条件。
+
+
+#include<stdio.h>
+int main()
+{
+	int age = 10;
+	if (age == 5)
+		printf("age==5\n");
+	return 0;
+}
+//屏幕上什么都不会打印，因为age！=5；
+
+
+#include<stdio.h>
+int main()
+{
+	int age = 10;
+	if (age = 5)
+		printf("age==5\n");
+	return 0;
+}
+//频幕上会打印age==5，因为把5赋值给了age，所以if后面的括号里面的值是真的，所以回打印要求的信息
+
+
+#include<stdio.h>
+int main()
+{
+	int day = 0;
+	scanf("%d", &day);
+	switch (day)
+	{
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+		printf("weekday\n");
+		break;
+	case 6:
+	case 7:
+		printf("weekend\n");
+		break;
+	default:
+		printf("选择错误\n");
+		break;
+	}
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 1;
+	int m = 2;
+	switch (n)
+	{
+	case 1:m++;
+	case 2:n++;
+	case 3:
+		switch (n)
+		{//switch允许嵌套使用
+		case 1:	n++;
+		case 2:m++; n++;	break;
+		}
+	case 4:m++; break;
+	default:
+		break;
+	}
+	printf("m = %d, n = %d\n", m, n);
+	system("pause");
+	return 0;
+}
+//结果是m=5，n=3；
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		printf("%d ", i);
+		i++;
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		if (i == 5)
+			break;
+		printf("%d ", i);
+		i++;
+	}
+	return 0;
+}
+//打印结果为1，2，3，4
+//break永久的跳出循环，停止后期的所有循环，永久的终止循环
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		if (i == 5)
+			continue;
+		printf("%d ", i);
+		i++;
+	}
+	return 0;
+}
+//打印结果是1，2，3，4，4，4，4，4...死循环
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		i++;
+		if (i == 5)
+			continue;
+		printf("%d ", i);
+	}
+	return 0;
+}
+//打印结果为2，3，4，5，7，8，9，10，11
+//continue作用：continue用来终止本次的循环，当次continue之后的语句不再执行。
+
+
+//EOF为end of file
+#include<stdio.h>
+int main()
+{
+	int ch = 0;
+	while ((ch = getchar()) != EOF)
+		putchar(ch);
+	return 0;
+}
+//用ctrl+z来停下来。
+
+
+//求素数
+#include<stdio.h>
+int main()
+{
+	int j = 0;
+	int count = 0;
+	for(int i=100;i<=200;i++)
+	{
+		for (j = 2; j < i; j++)
+		{
+			if (i % j == 0)
+				break;
+		}
+		if (j == i)
+		{
+			count++;
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	printf("%d", count);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int j = 0;
+	int count = 0;
+	for (int i = 100; i <= 200; i++)
+	{
+		for (j = 2; j <= sqrt(i); j++)
+		{
+			if (i % j == 0)
+				break;
+		}
+		if (j > sqrt(i))
+		{
+			count++;
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	printf("%d", count);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int count = 0;
+	for (i = 1000; i <= 2000; i++)
+	{
+		if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0)
+		{
+			printf("%d ", i);
+			count++;
+		}
+	}
+	printf("\n");
+	printf("%d", count);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	for (a = 1; a <= 9; a++)
+	{
+		for (b = 1; b <= a; b++)
+		{
+			printf("%d*%d=%2d ", a, b, a * b);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int temp = 0;
+	(void)scanf("%d %d", &a, &b);
+	temp = a;
+	a = b;
+	b = temp;
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+	printf("%d %d", a, b);
+	return 0;
+}
+//按二进制位，异或：相同为0，相异为1；
+//异或不能解决除了整形以外类型的运算
+//但是这种算法不存在有溢出的问题
+//这段代码的可读性也比较差，一开始是看不懂的
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	printf("%d %d", a, b);
+	return 0;
+}
+//但是这种方法存在有溢出的问题，如果a和b特别大的话，把a+b的值放在a里面就会溢出了
+//这样就得不到想要的结果了
+
+
+//求十个整数中的最大值
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 12,34,675,32,75,24,86,23,88,16 };
+	int max = arr[0];
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int i = 0;
+	for (i = 1; i < sz; i++)
+	{
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
+	}
+	printf("%d", max);
+	return 0;
+}
