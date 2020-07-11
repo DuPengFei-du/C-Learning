a4380c71e5df4b5a8596a65a943dc682979919e8
DuPengFei-du <935820240@qq.com>
2020-07-12 07:50:58 +0800

    test

---
+++ test(2)/test(2)/main.c
@@ -0,0 +1,759 @@
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int i = 1;
+	int sum = 1;
+	(void)scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
+	{
+		sum *= i;
+	}
+	printf("%d", sum);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int i = 1;
+	int ret = 1;
+	int sum = 0;
+	(void)scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
+	{
+		ret *= i;
+		sum += ret;
+	}
+	printf("%d", sum);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int i = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int left = 0;
+	int right = sz - 1;
+	int key = 6;
+	while (left <= right)
+	{
+		int mid = left + (right - left) / 2;
+		if (arr[mid] < key)
+			left = mid + 1;
+		else if (arr[mid] > key)
+			right = mid - 1;
+		else if (arr[mid] == key)
+		{
+			printf("找到了，下标为:%d", mid);
+			break;
+		}
+	}
+	return -1;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = "###########";
+	char arr2[] = "hello world";
+	int left = 0;
+	int sz = sizeof(arr1) / sizeof(arr1[0]);
+	int right = sz - 1;
+	while (left <= right)
+	{
+		arr1[left] = arr2[left];
+		arr1[right] = arr2[right];
+		left++;
+		right--;
+	}
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	char password[20] = { 0 };
+	for (i = 0; i < 3; i++)
+	{
+		printf("请输入密码:>\n");
+		(void)scanf("%s", password);
+		if (0 == strcmp(password, "123456"))
+		{
+			printf("登陆成功!\n");
+			break;
+		}
+		else
+		{
+			printf("密码错误，请重新输入!\n");
+		}
+	}
+	if (i == 3)
+	{
+		printf("三次全错，无法登录！");
+	}
+	return 0;
+}
+
+
+//猜数字游戏的实现
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+void menu()
+{
+	printf("***** 欢迎来到猜数字游戏！*****\n");
+	printf("*****        1. play      *****\n");
+	printf("*****        0. exit      *****\n");
+	printf("***** 欢迎来到猜数字游戏！*****\n");
+}
+void game()
+{
+	int guess = 0;
+	int ret = 0;
+	ret = rand() % 100 + 1;
+	while (1)
+	{
+		printf("请输入要猜测的数字:>\n");
+		(void)scanf("%d", &guess);
+		if (guess > ret)
+			printf("猜大了\n");
+		else if (guess > ret)
+			printf("猜小了\n");
+		else if (guess = ret)
+		{
+			printf("猜对了\n");
+			break;
+		}
+	}
+}
+void test()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do
+	{
+		menu();
+		printf("请选择:>\n");
+		(void)scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出猜数字游戏!\n");
+			break;
+		default:
+			printf("输入错误，请重新输入!\n");
+			break;
+		}
+	} while (input);
+}
+int main()
+{
+	test();
+	return 0;
+}
+
+
+#include<stdio.h>
+void Print1(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 1; i <= sz; i++)
+	{
+		printf("%d ", i);
+	}
+	printf("\n");
+}
+void Print2(int(*p)[10], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", (*p)[i]);
+		//首先找到数组，用(*p)找到数组，然后去访问第i个元素
+		//如果不带括号的话，那么p就会先和[]进行结合
+		//其实(*p)就等于是数组名然后[i]，就相当于是arr[i];
+	}
+}
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	Print1(arr, sz);
+	Print2(&arr, sz);
+	return 0;
+}
+
+
+#include<stdio.h>
+void test(int arr[3][5])
+{
+
+}
+//void test(int arr[][])
+//{
+//	//这种传参是不正确的，可以省略行，不可以省略列
+//}
+void test(int arr[][5])
+{
+
+}
+//总结:二维数组传参，函数形式的设计只能省略第一维的[],不能省略第二维的
+//因为对一个二维数组，可以不知道有多少行，但是必须知道一行有多少个元素
+//这样才会方便运算，同样，一样的道理，如果用数组接收参数，那么就原模原样的接收就可以了
+void test(int(*arr)[5])
+{
+
+}
+int main()
+{
+	int arr[3][5];
+	test(arr);
+}
+
+
+#include<stdio.h>
+void Print(int* p, int sz)
+{
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d ", *(p + i));
+	}
+}
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int* p = arr;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	Print(p, sz);
+	return 0;
+}
+
+
+#include<stdio.h>
+int Add(int x, int y)
+{
+	return x + y;
+}
+int main()
+{
+	printf("%p\n", &Add);   //证实了函数也是有地址的
+	int (*p)(int x, int y) = &Add;
+	int ret = (*p)(2, 3);   //函数的调用
+	//也可以写车： int ret=p(2,3);
+	//哪个*写不写都行，写几个都行
+	//(*p)是先找到这个函数，然后传参进行调用
+	printf("%d", ret);
+	//p是函数指针
+	return 0;
+}
+
+
+//计算器的实现
+#include<stdio.h>
+void menu()
+{
+	printf("**********************\n");
+	printf("*** 欢迎来到计算器 ***\n");
+	printf("***  1.Add   2.Sub ***\n");
+	printf("***  3.Mul   4.Div ***\n");
+	printf("**********************\n");
+}
+int Add(int x, int y)
+{
+	return x + y;
+}
+int Sub(int x, int y)
+{
+	return x - y;
+}
+int Mul(int x, int y)
+{
+	return x * y;
+}
+int Div(int x, int y)
+{
+	return x / y;
+}
+void test()
+{
+	int x = 0;
+	int y = 0;
+	int ret = 0;
+	int input = 0;
+	do
+	{
+		menu();
+		printf("请选择:>\n");
+		(void)scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			printf("请输入两个操作数:>\n");
+			(void)scanf("%d %d", &x, &y);
+			ret = Add(x, y);
+			printf("%d\n", ret);
+			break;
+		case 2:
+			printf("请输入两个操作数:>\n");
+			(void)scanf("%d %d", &x, &y);
+			ret = Sub(x, y);
+			printf("%d\n", ret);
+			break;
+		case 3:
+			printf("请输入两个操作数:>\n");
+			(void)scanf("%d %d", &x, &y);
+			ret = Mul(x, y);
+			printf("%d\n", ret);
+			break;
+		case 4:
+			printf("请输入两个操作数:>\n");
+			(void)scanf("%d %d", &x, &y);
+			ret = Div(x, y);
+			printf("%d\n", ret);
+			break;
+		case 0:
+			printf("退出计算器!\n");
+			break;
+		default:
+			printf("选择错误，请重新选择:>\n");
+			break;
+		}
+	} while (input);
+}
+int main()
+{
+	test();
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	//代码1
+	(*(void(*)())0)();
+
+	/*
+	首先，void(*)()为函数指针类型，其次void(*)()内放在括号中
+	形成了(void(*)()),我们都知道，一个类型放在括号中，是用来
+	强制类型转换的，(void(*)())又放在0前面，意思就是吧0转换成
+	函数指针类型，0本来的类型是int，我希望把0当成函数指针类型的这样的
+	一个函数的地址，所以我希望0这个地址处放的是一个无参，返回值类型
+	为void的函数，然后，既然是个地址，那么我就解引用一下
+	所以综上，上述是一场函数调用。
+	调用0位置处的函数，被调用的这个函数是无参的，返回值类型为void的类型
+	*/
+
+	//代码2
+	void (*signal(int, void(*)(int)))(int);
+	/*
+	这段代码整体来看是一个函数声明，其中signal为函数名，signal函数的第一个参数
+	为int类型，第二个参数类型为函数指针类型，有了函数名，有了参数
+	剩下的就是函数的返回值类型了，这个函数的返回值类型也是一个函数指针类型
+	*/
+	/*
+	但是又因为这个代码出现了两次函数指针类型，所以我们可以用typedef进行
+	类型重命名，即：
+	typedef void(*pfun_t)(int);
+	所以上述声明也可以变成
+	pfun_t signal(int,pfun_t);
+	*/
+}
+
+
+#include<stdio.h>
+void menu()
+{
+	printf("**********************\n");
+	printf("*** 欢迎来到计算器 ***\n");
+	printf("***  1.Add   2.Sub ***\n");
+	printf("***  3.Mul   4.Div ***\n");
+	printf("**********************\n");
+}
+int Add(int x, int y)
+{
+	return x + y;
+}
+int Sub(int x, int y)
+{
+	return x - y;
+}
+int Mul(int x, int y)
+{
+	return x * y;
+}
+int Div(int x, int y)
+{
+	return x / y;
+}
+void test()
+{
+	int x = 0;
+	int y = 0;
+	int ret = 0;
+	int input = 0;
+	int (*p[5])(int, int) = { 0,Add,Sub,Mul,Div };
+	do
+	{
+		menu();
+		printf("请选择:>\n");
+		(void)scanf("%d", &input);
+		printf("请输入两个操作数:>\n");
+		(void)scanf("%d %d", &x, &y);
+		ret = p[input](x, y);
+		printf("%d\n", ret);
+	} while (input);
+}
+int main()
+{
+	test();
+	return 0;
+}
+
+
+//计算器的实现
+#include<stdio.h>
+void menu()
+{
+	printf("**********************\n");
+	printf("*** 欢迎来到计算器 ***\n");
+	printf("***  1.Add   2.Sub ***\n");
+	printf("***  3.Mul   4.Div ***\n");
+	printf("**********************\n");
+}
+int Add(int x, int y)
+{
+	return x + y;
+}
+int Sub(int x, int y)
+{
+	return x - y;
+}
+int Mul(int x, int y)
+{
+	return x * y;
+}
+int Div(int x, int y)
+{
+	return x / y;
+}
+void Cal(int(*p)(int, int))
+{
+	int x = 0;
+	int y = 0;
+	int ret = 0;
+	printf("请输入两个操作数:>\n");
+	(void)scanf("%d %d", &x, &y);
+	ret = p(x, y);
+	printf("%d\n", ret);
+}
+void test()
+{
+	int input = 0;
+	//函数指针
+	do
+	{
+		menu();
+		printf("请选择:>\n");
+		(void)scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			Cal(Add);
+			break;
+		case 2:
+			Cal(Sub);
+			break;
+		case 3:
+			Cal(Mul);
+			break;
+		case 4:
+			Cal(Div);
+			break;
+		case 0:
+			printf("退出计算器!\n");
+			break;
+		default:
+			printf("选择错误，请重新选择:>\n");
+			break;
+		}
+	} while (input);
+}
+int main()
+{
+	test();
+	return 0;
+}
+
+
+int Add(int x, int y)
+{
+	return x + y;
+}
+int Sub(int x, int y)
+{
+	return x - y;
+}
+int (*p)(int, int);   //函数指针
+int (*parray[2])(int, int);  //函数指针数组
+int(*(*ptr)[2])(int, int) = &parray;//指向函数指针数组的指针
+//ptr是指针
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[20] = { 0 };
+	char arr2[] = "hello world";
+	strcpy(arr1, arr2);
+	printf("%s\n", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int n = 0;
+	int k = 0;
+	double ret = 0;
+	(void)scanf("%d %d", &n, &k);
+	ret = pow(n, k);
+	printf("%lf", ret);
+	return 0;
+}
+
+
+#include<stdio.h>
+int MAX(int x, int y)
+{
+	return x > y ? x : y;
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	(void)scanf("%d %d", &a, &b);
+	c= MAX(a, b);
+	printf("%d",c);
+	return 0;
+}
+
+
+#include<stdio.h>
+void Swap(int* x, int* y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	Swap(&a, &b);
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int is_prime(int n)
+{
+	for (int i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+			return 0;
+	}
+	return 1;
+}
+int main()
+{
+	int ret = 0;
+	int n = 0;
+	(void)scanf("%d", &n);
+	ret = is_prime(n);
+	if (ret == 1)
+		printf("素数");
+	else
+		printf("不是素数");
+	return 0;
+}
+
+
+//打印每一位数字
+#include<stdio.h>
+void Print(int n)
+{
+	if (n > 9)
+		Print(n / 10);
+	printf("%d ", n % 10);
+}
+int main()
+{
+	int n = 0;
+	(void)scanf("%d", &n);
+	Print(n);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = "hello world";
+	memset(arr1, 'x', 5);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+int is_leapyear(int i)
+{
+	if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0)
+		return 1;
+	else
+		return 0;
+}
+int main()
+{
+	int n = 0;
+	(void)scanf("%d", &n);
+	if (is_leapyear(n) == 1)
+	{
+		printf("是闰年\n");
+	}
+	else
+	{
+		printf("不是闰年\n");
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	printf("%d", printf("%d", printf("%d", 43)));
+	return 0;
+}
+//打印的结果为4 3 2 1
+//printf函数的返回值是字符的个数
+
+
+#include<stdio.h>
+int my_strlen(const char* str)
+{
+	int count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+int main()
+{
+	int len = my_strlen("abc");
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+int my_strlen(const char* str)
+{
+	if (*str == '\0')
+		return 0;
+	else
+		return 1 + my_strlen(str + 1);
+}
+int main()
+{
+	int len = my_strlen("abcdef");
+	printf("%d\n", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+void Print1(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 1; i <= sz; i++)
+	{
+		printf("%d ", i);
+	}
+	printf("\n");
+}
+void Print2(int(*p)[10], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", (*p)[i]);
+		//首先找到数组，用(*p)找到数组，然后去访问第i个元素
+		//如果不带括号的话，那么p就会先和[]进行结合
+		//其实(*p)就等于是数组名然后[i]，就相当于是arr[i];
+	}
+}
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	Print1(arr, sz);
+	Print2(&arr, sz);
+	return 0;
+}
+
+
+#include<stdio.h>
+//1号程序员写的代码
+int Add(int x, int y)
+{
+	return x + y;
+}
+//2号程序员写的代码
+int test(int (*pf)(int, int))
+{
+	//如果2号程序员想在他的内部调用Add函数的话，我可以在主函数中将Add函数的地址传递给2号程序员写的代码
+	//那么2号程序员写的代码，就要对Add函数的地址进行接收，利用函数指针(指向函数的指针)
+	//pf为指针变量
+	pf(2, 4);  //调用这个指针所指向的函数，此时被调用的函数就称为回调函数
+}
+int main()
+{
+	//1号程序员进行调用
+	//自己写的代码，自己直接调用，这种不被称为回调函数
+	int ret = Add(2, 3);
+	int res = test(Add);
+	printf("%d", res);
+	return 0;
+}
+
+
+void qsort(void* base,    //目标数组的起始位置
+	size_t num,    //数组元素的个数
+	size_t width,  //一个元素几个字节大小 
+	int(*compare)(const void* elem1, const void* elem2));
+//compare---函数比较  写函数的人想要实现的某种功能，程序员要写出如果比较才能的到他自己想要的结果
+//其实也就是再写不同类型的比较方法的
+//其中elem1和elem2为我所要比较的两个元素的地址
