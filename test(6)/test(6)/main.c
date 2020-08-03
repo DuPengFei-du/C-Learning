762b3c711a83070dd75f0bba28a97656836913f1
DuPengFei-du <935820240@qq.com>
2020-08-03 21:42:53 +0800

    test

---
+++ test(6)/test(6)/main.c
@@ -0,0 +1,884 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strlen(const char* str)
+{
+	assert(str);
+	if (*str == '\0')
+		return 0;
+	else
+		return 1 + my_strlen(str+1);
+}
+int main()
+{
+	char arr[] = "ancdef";
+	int len = my_strlen(arr);
+	printf("%d", len);
+	return 0;
+}
+
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strlen(const char* str)
+{
+	assert(str);
+	int count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+int main()
+{
+	char arr[] = "ancdef";
+	int len = my_strlen(arr);
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strlen(char* str)
+{
+	assert(str);
+	char* ret = str;
+	while (*str != '\0')
+	{
+		str++;
+	}
+	return str - ret;
+}
+int main()
+{
+	char arr[] = "ancdef";
+	int len = my_strlen(arr);
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+char* my_strcpy(char* dest, const char* source)
+{
+	assert(dest);
+	assert(source);
+	char* ret = dest;
+	while (*dest++ = *source++)
+	{
+		;
+	}
+	return ret;
+}
+int main()
+{
+	char arr1[20] = "hello";
+	char arr2[] = "world";
+	my_strcpy(arr1, arr2);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+char* my_strcat(char* dest, const char* source)
+{
+	assert(dest);
+	assert(source);
+	char* ret = dest;
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*dest++ = *source++)
+	{
+		;
+	}
+	return ret;
+}
+int main()
+{
+	char arr1[20] = "hello";
+	char arr2[] = " world";
+	my_strcat(arr1, arr2);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr[20] = "abcdef";
+	int len = strlen(arr);
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[20] = { 0 };
+	char arr2[] = "abcdef";
+	strcpy(arr1, arr2);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+//字符串比较函数
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[] = "abcdef";
+	char arr2[] = "bcd";
+	int ret= strcmp(arr1, arr2);
+	printf("%d", ret);
+	return 0;
+}
+
+
+//模拟实现
+#include<stdio.h>
+#include<assert.h>
+int my_strcmp(const char* str1, const char* str2)
+{
+	assert(str1);
+	assert(str2);
+	while (*str1 == *str2)
+	{
+		if (*str1 == '\0')
+			return 0;
+		str1++;
+		str2++;
+	}
+	return *str1 - *str2;
+}
+int main()
+{
+	char arr1[] = "abcdef";
+	char arr2[] = "bcd";
+	int ret= my_strcmp(arr1, arr2);
+	printf("%d", ret);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strcmp(const char* str1, const char* str2)
+{
+	assert(str1 != NULL);
+	assert(str2 != NULL);
+	while (*str1 == *str2)
+	{
+		if (*str1 == '\0')
+		{
+			return 0;
+		}
+		str1++;
+		str2++;
+	}
+	if (*str1 > * str2)
+	{
+		return 1;
+	}
+	else
+	{
+		return -1;
+	}
+}
+int main()
+{
+	char arr1[] = "bedfwseg";
+	char arr2[] = "abc";
+	int ret = my_strcmp(arr1, arr2);
+	printf("%d", ret);
+	return 0;
+}
+
+
+//strncpy
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[20] = { 0 };
+	char arr2[] = "abcdef";
+	strncpy(arr1, arr2, 3);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+#include<assert.h>
+char* my_strncpy(char* dest, const char* source, int count)
+{
+	assert(dest);
+	assert(source);
+	int sz = 0;
+	char* ret = dest;
+	if (strlen(source) < count)
+	{
+		sz = count - strlen(source);
+		count = strlen(source);
+	}
+	while (count--)
+	{
+		*dest++ = *source++;
+	}
+	while (sz--)
+	{
+		*dest = '\0';
+	}
+	return ret;
+}
+int main()
+{
+	char arr1[20] = { 0 };
+	char arr2[] = "abcdef";
+	my_strncpy(arr1, arr2, 3);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+//strncat
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[20] = "hello";
+	char arr2[] = " world";
+	strncat(arr1, arr2, 3);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+//模拟实现strncat
+#include<stdio.h>
+#include<assert.h>
+char* my_strncat(char* dest, const char* source, int count)
+{
+	assert(dest);
+	assert(source);
+	char* ret = dest;
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (count--)
+	{
+		*dest++ = *source++;
+	}
+	*dest = '\0';
+	return ret;
+}
+int main()
+{
+	char arr1[20] = "hello";
+	char arr2[] = " world";
+	my_strncat(arr1, arr2, 4);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	{
+		int num = 10;
+	}
+	//printf("%d", num);
+	return 0;
+	//此时，这个程序会出错，num作用域中不包括有printf语句。
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	(void)scanf("%d %d", &a, &b);
+	c = a + b;
+	printf("%d", c);
+	return 0;
+}
+
+
+#include<stdio.h>
+int Add(int x, int y)
+{
+	return x + y;
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	(void)scanf("%d %d", &a, &b);
+	c = Add(a, b);
+	printf("%d", c);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = "abc";
+	char arr2[] = { 'a','b','c',0 };
+	printf("%s\n", "abc");
+	printf("%s\n", arr1);
+	printf("%s\n", arr2);
+	return 0;
+	//三种打印的结果都是abc
+	//如果arr2后面不加0的话，那么打印结果就会是再abc的后面还会出现烫烫烫的字符
+	//因为如果不加0的话，字符串就会并没有结束，只有加了0或者\0才会停止打印
+	//烫烫烫背后的真实东西其实是0xcccccc
+	//由此可以看来\0对字符串来说是非常重要的
+	//字符串是\0需要的结束标志的符号；
+}
+
+
+#include<stdio.h>
+int main()
+{
+	printf("%c\n", '\'');
+	printf("%s\n", "\"");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,3,5,7,9,2,4,6,8,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	while (i < 10)
+	{
+		printf("%d ", arr[i]);
+		i++;
+	}
+	printf("\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a1 = 5 / 2;
+	float a2 = 5 / 2;
+	float a3 = 5.0 / 2;
+	printf("%d\n", a1);
+	printf("%f\n", a2);
+	printf("%f\n", a3);
+	return 0;
+}
+//针对于除法操作，与他存储的是什么类型是没有关系的
+//主要是看两个除法的操作数到底是什么类型的，如果有一个是浮点型的，得到的结果就会是浮点型的结果
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = a++;
+	printf("%d\n", a);  //11
+	printf("%d\n", b);  //10
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = ++a;
+	printf("%d\n", a);   //11
+	printf("%d\n", b);   //11
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 3;
+	int b = 5;
+	int c = a && b;
+	printf("%d", c);
+	return 0;
+}
+//结果为1
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 5;
+	int c = a && b;
+	printf("%d", c);
+	return 0;
+}
+//结果为0
+
+
+#include<stdio.h>
+int main()
+{
+	int a = (1, 2, 3, 4, 5);
+	printf("%d\n", a);
+	return 0;
+}
+
+
+#include<stdio.h>
+void test()
+{
+	static int n = 1;   //n为局部变量
+	n++;
+	printf("%d ", n);
+}
+int main()
+{
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		test();
+	}
+	return 0;
+}
+//打印的结果是2-11，用static修饰变量，创建了就不销毁了。
+
+
+#include<stdio.h>
+//自定义类型
+struct Stu
+{
+	char name[20];   //一个汉字是两个字符
+	int age;
+	float score;
+};
+int main()
+{
+	struct Stu s = { "张三",20,60.0f };
+	printf("name :%s\n", s.name);
+	printf("age:%d\n", s.age);
+	printf("score :%f\n", s.score);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strlen(const char* str)
+{
+	assert(str);
+	if (*str == '\0')
+		return 0;
+	else
+		return 1 + my_strlen(str + 1);
+}
+int main()
+{
+	char arr1[] = "abcdef";
+	int len = my_strlen(arr1);
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strlen(const char* str)
+{
+	assert(str);
+	int count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+int main()
+{
+	char arr1[] = "abcdef";
+	int len = my_strlen(arr1);
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strlen(char* str)
+{
+	assert(str);
+	char* ret = str;
+	while (*str != '\0')
+	{
+		str++;
+	}
+	return str - ret;
+}
+int main()
+{
+	char arr1[] = "abcdef";
+	int len = my_strlen(arr1);
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+char* my_strcpy(char* dest, const char* source)
+{
+	assert(dest);
+	assert(source);
+	char* ret = dest;
+	while(*dest++=*source++)
+	{
+		;
+	}
+	return ret;
+}
+int main()
+{
+	char arr1[20] = { 0 };
+	char arr2[] = "abcdef";
+	my_strcpy(arr1, arr2);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+char* my_strcat(char* dest, const char* source)
+{
+	assert(dest);
+	assert(source);
+	char* ret = dest;
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while(*dest++=*source++)
+	{
+		;
+	}
+	return ret;
+}
+int main()
+{
+	char arr1[20] = "hello";
+	char arr2[] = " world";
+	my_strcat(arr1, arr2);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+#include<string.h>
+char* my_strncpy(char* dest, const char* source,size_t count)
+{
+	assert(dest);
+	assert(source);
+	char* ret = dest;
+	int sz = 0;
+	if (strlen(source) < count)
+	{
+		sz = count - strlen(source);
+		count = strlen(source);
+	}
+	while (count--)
+	{
+		*dest++ = *source++;
+	}
+	while(sz--)
+	{
+		*dest = '\0';
+	}
+	return ret;
+}
+int main()
+{
+	char arr1[20] = { 0 };
+	char arr2[] = "abcdef";
+	my_strncpy(arr1, arr2,3);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strcmp(const char* str1, const char* str2)
+{
+	assert(str1);
+	assert(str2);
+	while (*str1 == *str2)
+	{
+		if (*str1 == '\0')
+		{
+			return 0;
+		}
+		str1++;
+		str2++;
+	}
+	return *str1 - *str2;
+}
+int main()
+{
+	char arr1[] = "abcdef";
+	char arr2[] = "cbde";
+	int ret = 0;
+	ret = my_strcmp(arr1, arr2);
+	printf("%d", ret);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[30] = "hello world";
+	char arr2[] = "change world";
+	int ret = strncmp(arr1, arr2, 3);
+	printf("%d", ret);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	(void)scanf("%d", &n);
+	if (n % 2 == 1)
+	{
+		printf("奇数\n");
+	}
+	else
+	{
+		printf("偶数\n");
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int count = 0;
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i % 2 == 1)
+		{
+			count++;
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	printf("%d", count);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int day = 0;
+	(void)scanf("%d", &day);
+	switch (day)
+	{
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+		printf("weekday\n");
+		break;
+	case 6:
+	case 7:
+		printf("weekend\n");
+		break;
+	default:
+		printf("输入错误，请重新输入!\n");
+		break;
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int count = 0;
+	int j = 0;
+	for (int i = 100; i <= 200; i++)
+	{
+		for (j = 2; j < i; j++)
+		{
+			if (i % j == 0)
+				break;
+		}
+		if (j == i)
+		{
+			count++;
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	printf("%d", count);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int count = 0;
+	int j = 0;
+	for (int i = 100; i <= 200; i++)
+	{
+		for (j = 2; j <= sqrt(i); j++)
+		{
+			if (i % j == 0)
+				break;
+		}
+		if (j > sqrt(i))
+		{
+			count++;
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	printf("%d", count);
+	return 0;
+}
+
+
+//最大公约数
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 1;
+	(void)scanf("%d %d", &a, &b);
+	while (c = a % b)
+	{
+		a = b;
+		b = c;
+	}
+	printf("%d", b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int age = 0;
+	(void)scanf("%d", &age);
+	if (age < 18)
+		printf("未成年\n");
+	else
+		printf("成年\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int age = 10;
+	if (age == 5)
+		printf("age==5\n");
+	return 0;
+}
+//屏幕上什么都不会打印，因为age！=5；
+
+
+#include<stdio.h>
+int main()
+{
+	int age = 10;
+	if (age = 5)
+		printf("age==5\n");
+	return 0;
+}
+//频幕上会打印age==5，因为把5赋值给了age，所以if后面的括号里面的值是真的，所以回打印要求的信息
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		if (i == 5)
+			break;
+		printf("%d ", i);
+		i++;
+	}
+	return 0;
+}
+//打印结果为1，2，3，4
+//break永久的跳出循环，停止后期的所有循环，永久的终止循环
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		if (i == 5)
+			continue;
+		printf("%d ", i);
+		i++;
+	}
+	return 0;
+}
+//打印结果是1，2，3，4，4，4，4，4...死循环
