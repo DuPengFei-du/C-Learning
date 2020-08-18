3f8d70e849c058d139679b29d38190f563eab6e4
DuPengFei-du <935820240@qq.com>
2020-08-18 08:25:20 +0800

    test

---
+++ test(10)/test(10)/main.c
@@ -0,0 +1,735 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+//库函数下得strstr
+#include<stdio.h>
+#include<assert.h>
+#include<string.h>
+int main()
+{
+	char arr1[] = "abcdef";
+	char arr2[] = "defet";
+	char* ret = strstr(arr1, arr2);
+	if (ret == NULL)
+		printf("不存在\n");
+	else
+		printf("%s\n", ret);
+	return 0;
+}
+
+
+//库函数下得strstr
+#include<stdio.h>
+#include<assert.h>
+#include<string.h>
+int main()
+{
+	char arr1[] = "abcdef";
+	char arr2[] = "bcde";
+	char* ret = strstr(arr1, arr2);
+	if (ret == NULL)
+		printf("不存在\n");
+	else
+		printf("%s\n", ret);
+	return 0;
+}
+
+
+//模拟实现strstr
+#include<stdio.h>
+#include<assert.h>
+char* my_strstr(const char* str1, const char* str2)
+{
+	const char* s1 = str1;
+	const char* s2 = str2;
+	const char* start = str1;
+	assert(str1);
+	assert(str2);
+	while (*str2 == '\0')
+		return (char*)str1;
+	while (*start != '\0')
+	{
+		s2 = str2;
+		s1 = start;
+		while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+		if (*s2 == '\0')
+			return (char*)start;
+		start++;
+	}
+	return NULL;
+}
+int main()
+{
+	char arr1[] = "abcdef";
+	char arr2[] = "bcde";
+	char* ret = my_strstr(arr1, arr2);
+	if (ret == NULL)
+		printf("不存在\n");
+	else
+		printf("%s\n", ret);
+	return 0;
+}
+
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	const char str[] = "http://www.runoob.com";
+	const char ch = '.';
+	char* ret;
+	ret = strchr(str, ch);
+	printf("|%c| 之后的字符串是 - |%s|\n", ch, ret);
+	return(0);
+}
+
+
+//模拟实现strchr
+#include<stdio.h>
+#include<assert.h>
+char* my_strchr(const char* str, const char ch)
+{
+	assert(str);
+	const char* dest = str;
+	while (dest != '\0' && *dest != ch)
+	{
+		dest++;
+	}
+	if (*dest == ch)
+		return (char*)dest;
+	return NULL;
+}
+int main()
+{
+	char* ret = my_strchr("hello", 'l');
+	if (ret == NULL)
+		printf("不存在");
+	else
+		printf("%s\n", ret);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int i = 1;
+	int sum = 1;
+	(void)scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		sum *= i;
+	}
+	printf("%d", sum);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int i = 1;
+	int ret = 1;
+	int sum = 0;
+	(void)scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		ret *= i;
+		sum += ret;
+	}
+	printf("%d", sum);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int left = 0;
+	int right = sz - 1;
+	int key = 5;
+	while (left <= right)
+	{
+		int mid = left + (right - left) / 2;
+		if (arr[mid] > key)
+			right = mid - 1;
+		else if (arr[mid] < key)
+			left = mid + 1;
+		else if (arr[mid] == key)
+		{
+			printf("找到了，下标为:%d", mid);
+			break;
+		}
+	}
+	return -1;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = "**********";
+	char arr2[] = "hello wold";
+	int left = 0;
+	int sz = sizeof(arr1) / sizeof(arr1[0]);
+	int right = sz - 1;
+	while (left <= right)
+	{
+		arr1[left] = arr2[left];
+		arr1[right] = arr2[right];
+		left++;
+		right--;
+	}
+	printf("%s", arr1);
+	return 0;
+}
+
+
+//模拟登陆层序
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int i = 0;
+	char password[20] = { 0 };
+	for (i = 0; i < 3; i++)
+	{
+		printf("请输入密码:\n");
+		(void)scanf("%s", password);
+		if (0 == strcmp(password, "nishizhu"))
+		{
+			printf("登录成功\n");
+			break;
+		}
+		else
+		{
+			printf("密码错误，请重新输入!\n");
+		}
+	}
+	if (i == 3)
+		printf("无法登录\n");
+	return 0;
+}
+
+
+//猜数字游戏
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+void menu()
+{
+	printf("***** 欢迎来到猜数字游戏！*****\n");
+	printf("*****        1. play      *****\n");
+	printf("*****        0. exit      *****\n");
+	printf("***** 欢迎来到猜数字游戏！*****\n");
+}
+void game()
+{
+	int guess = 0;
+	int ret = 0;
+	ret = rand() % 100 + 1;
+	printf("请输入所猜测的数字:>\n");
+	while (1)
+	{
+		(void)scanf("%d", &guess);
+		if (guess > ret)
+			printf("猜大了!\n");
+		else if (guess < ret)
+			printf("猜小了!\n");
+		else
+		{
+			printf("猜对了!\n");
+			break;
+		}
+	}
+}
+void test()
+{
+	srand((unsigned int)time(NULL));
+	int input = 0;
+	do
+	{
+		menu();
+		printf("请选择:>\n");
+		(void)scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出猜数字游戏！\n");
+			break;
+		default:
+			printf("选择错误，请重新选择!\n");
+			break;
+		}
+	} while (input);
+}
+int main()
+{
+	test();
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 1;
+	int m = 2;
+	switch (n)
+	{
+	case 1:m++;
+	case 2:n++;
+	case 3:
+		switch (n)
+		{//switch允许嵌套使用
+		case 1:	n++;
+		case 2:m++; n++;	break;
+		}
+	case 4:m++; break;
+	default:
+		break;
+	}
+	printf("m = %d, n = %d\n", m, n);
+	system("pause");
+	return 0;
+}
+//结果是m=5，n=3；
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		if (i == 5)
+			break;
+		printf("%d ", i);
+		i++;
+	}
+	return 0;
+}
+//打印结果为1，2，3，4
+//break永久的跳出循环，停止后期的所有循环，永久的终止循环
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int j = 0;
+	int count = 0;
+	for (i = 100; i <= 200; i++)
+	{
+		for (j = 2; j <= i - 1; j++)
+		{
+			if (i % j == 0)
+				break;
+		}
+		if (j == i)
+		{
+			printf("%d ", i);
+			count++;
+		}
+	}
+	printf("\n");
+	printf("%d", count);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	for (i = 1; i <= 10; i++)
+	{
+		if (i == 5)
+			break;
+		printf("%d ", i);
+	}
+	return 0;
+}
+//打印结果为1，2，3，4
+
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	for (i = 0; i <= 12; i++)
+	{
+		printf("hehe\n");
+		arr[i] = 0;
+	}
+	return 0;
+}
+//代码的打印结果是hehe的死循环
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int k = 0;
+	for (i = 0, k = 0; k = 0; i++, k++)
+		k++;
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<windows.h>
+int main()
+{
+	char arr1[] = "###########";
+	char arr2[] = "Hello World";
+	int sz = sizeof(arr1) / sizeof(arr1[0]);
+	int left = 0;
+	int right = sz - 2;
+	while (left <= right)
+	{
+		arr1[left] = arr2[left];
+		arr1[right] = arr2[right];
+		printf("%s\n", arr1);
+		Sleep(1000);
+		left++;
+		right--;
+	}
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main()
+{
+	char input[] = { 0 };
+	system("shutdown -s -t 60");
+again:
+	printf("请注意，你的电脑在1分钟内关机，如果输入：我是猪，就取消关机\n");
+	scanf("%s", input);
+	if (0 == strcmp(input, "我是猪"))
+	{
+		system("shutdown -a");
+	}
+	else
+	{
+		printf("输入错误，请重新输入!\n");
+		goto again;
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int arr1[10] = { 0 };
+	int arr2[3] = { 1,2,3 };
+	memcpy(arr1, arr2, 12);
+	//最后一个参数为要拷贝的字节数
+	return 0;
+}
+
+
+
+#include<stdio.h>
+#include<assert.h>
+void* my_memcpy(void* destination, const void* source, size_t num)
+{
+	size_t i = 0;
+	assert(destination != NULL);
+	assert(source != NULL);
+	char* dest = (char*)destination;
+	char* sour = (char*)source;
+	for (i = 0; i < num; i++)
+	{
+		dest[i] = sour[i];
+	}
+	return destination;
+}
+int main()
+{
+	int arr1[10] = { 0 };
+	int arr2[] = { 1,2,3 };
+	my_memcpy(arr1, arr2, 12);
+	return 0;
+}
+
+//第二种方法
+#include<stdio.h>
+#include<assert.h>
+void* my_memcpy(void* dest, const void* source, size_t num)
+{
+	//每次拷贝一个字节最为合适
+	assert(dest != NULL);
+	assert(source != NULL);
+	size_t i = 0;
+	char* ret = (char*)dest;
+	char* s1 = (char*)dest;
+	const char* s2 = (char*)source;
+	for (i = 0; i < num; i++)
+	{
+		*(char*)dest = *(char*)source;
+		s1++;
+		s2++;
+	}
+	return ret;
+}
+int main()
+{
+	int arr1[10] = { 0 };
+	int arr2[] = { 1,2,3 };
+	my_memcpy(arr1, arr2, 12);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[20] = { 0 };
+	char arr2[] = "hello world";
+	strcpy(arr1, arr2);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int n = 0;
+	int k = 0;
+	double ret = 0;
+	(void)scanf("%d %d", &n, &k);
+	ret = pow(n, k);
+	printf("%lf", ret);
+	return 0;
+}
+
+
+#include<stdio.h>
+int MAX(int x, int y)
+{
+	return x > y ? x : y;
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	(void)scanf("%d %d", &a, &b);
+	c = MAX(a, b);
+	printf("%d", c);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int is_prime(int n)
+{
+	for (int i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+			return 0;
+	}
+	return 1;
+}
+int main()
+{
+	int n = 0;
+	(void)scanf("%d", &n);
+	int ret = is_prime(n);
+	if (ret == 1)
+		printf("素数\n");
+	else
+		printf("不是素数\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+void Print(int n)
+{
+	if (n > 9)
+		Print(n / 10);
+	printf("%d ", n % 10);
+}
+int main()
+{
+	int n = 0;
+	(void)scanf("%d", &n);
+	Print(n);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strlen(const char* str)
+{
+	assert(str);
+	if (*str == '\0')
+		return 0;
+	else
+		return 1 + my_strlen(str + 1);
+}
+int main()
+{
+	char arr[] = "abcdef";
+	int len = my_strlen(arr);
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = "hello world";
+	memset(arr1, 'x', 5);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+void Swap(int* x, int* y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	Swap(&a, &b);
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int is_leapyear(int i)
+{
+	if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0)
+		return 1;
+	else
+		return 0;
+}
+int main()
+{
+	int n = 0;
+	(void)scanf("%d", &n);
+	if (is_leapyear(n) == 1)
+	{
+		printf("是闰年\n");
+	}
+	else
+	{
+		printf("不是闰年\n");
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+void BinarySearch(int arr[], int sz)
+{
+	int key = 7;
+	int left = 0;
+	int right = sz - 1;
+	while (left <= right)
+	{
+		int mid = left + (right - left) / 2;
+		if (arr[mid] < key)
+			left = mid + 1;
+		else if (arr[mid] > key)
+			right = mid - 1;
+		else
+		{
+			printf("找到了,下标是:%d", mid);
+			break;
+		}
+	}
+	if (left > right)
+		printf("找不到\n");
+}
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int left = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	BinarySearch(arr, sz);
+	return 0;
+}
+
+
+#include<stdio.h>
+void new_line()
+{
+	printf("hehe\n");
+}
+void three_line()
+{
+	for (int i = 0; i < 3; i++)
+	{
+		new_line();
+	}
+}
+int main()
+{
+	three_line();
+	return 0;
+}
