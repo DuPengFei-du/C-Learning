31fdc7f119dece44bdbbc1f48e67bc48687b42a3
DuPengFei-du <935820240@qq.com>
2021-02-12 22:52:14 +0800

    test

---
+++ test(42)/test(42)/main.c
@@ -0,0 +1,735 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	(void)scanf("%d", &n);
+	if (n % 2 == 1)
+		printf("奇数\n");
+	else
+		printf("偶数\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int count = 0;
+	for (int i = 0; i < 100; i++)
+	{
+		if (i % 2 == 1)
+		{
+			count++;
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	printf("%d", count);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int day = 0;
+	(void)scanf("%d", &day);
+	switch (day)
+	{
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+			printf("weekday\n");
+			break;
+		case 6:
+		case 7:
+			printf("weekend\n");
+			break;
+		default:
+			printf("输入错误，请重新输入!\n");
+			break;
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int i = 1;
+	int sum = 1;
+	(void)scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		sum *= i;
+	}
+	printf("%d", sum);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int i = 1;
+	int sum = 0;
+	int ret = 1;
+	(void)scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		ret *= i;
+		sum += ret;
+	}
+	printf("%d", sum);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int key = 8;
+	int left = 0;
+	int right = sz - 1;
+	while (left <= right)
+	{
+		int mid = left + (right - left) / 2;
+		if (arr[mid] < key)
+		{
+			left = mid + 1;
+		}
+		else if (arr[mid] > key)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			printf("找到了，下标为:%d", mid);
+			break;
+		}
+	}
+	return -1;
+}
+
+
+//登录程序
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int i = 0;
+	char password[20] = { 0 };
+	for (i = 0; i < 3; i++)
+	{
+		printf("请输入密码\n");
+		(void)scanf("%s", password);
+		if (0 == strcmp(password, "123456"))
+		{
+			printf("登陆成功\n");
+			break;
+		}
+		else
+		{
+			printf("密码错误，请重新输入!\n");
+		}
+	}
+	if (i == 3)
+		printf("无法登录!\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[20] = { 0 };
+	char arr2[] = "hello world";
+	int sz = sizeof(arr2) / sizeof(arr2[0]);
+	int left = 0;
+	int right = sz - 1;
+	while (left <= right)
+	{
+		arr1[left] = arr2[left];
+		arr1[right] = arr2[right];
+		left++;
+		right--;
+	}
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int count = 0;
+	int j = 0;
+	for (int i = 100; i <= 200; i++)
+	{
+		for (j = 2; j < i; j++)
+		{
+			if (i % j == 0)
+				break;
+		}
+		if (j == i)
+		{
+			count++;
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	printf("%d", count);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int count = 0;
+	int j = 0;
+	for (int i = 100; i <= 200; i++)
+	{
+		for (j = 2; j <= sqrt(i); j++)
+		{
+			if (i % j == 0)
+				break;
+		}
+		if (j > sqrt(i))
+		{
+			count++;
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	printf("%d", count);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 1;
+	(void)scanf("%d %d", &a, &b);
+	while (c = a % b)
+	{
+		a = b;
+		b = c;
+	}
+	printf("%d", b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int age = 0;
+	(void)scanf("%d", &age);
+	if (age < 18)
+		printf("未成年\n");
+	else
+		printf("成年\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 2;
+	if (a == 1)
+		if (b == 2)
+			printf("hehe\n");
+		else
+			printf("haha\n");
+	return 0;
+}
+//这段代码什么都不会打印 第二个else会在编译器中主动和第二个if靠近
+//所以代码什么都不会打印，没有符号的条件。
+
+
+#include<stdio.h>
+int main()
+{
+	int age = 10;
+	if (age = 5)
+		printf("age==5\n");
+	return 0;
+}
+//频幕上会打印age==5，因为把5赋值给了age，所以if后面的括号里面的值是真的，所以回打印要求的信息
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 1;
+	int m = 2;
+	switch (n)
+	{
+	case 1:m++;
+	case 2:n++;
+	case 3:
+		switch (n)
+		{//switch允许嵌套使用
+		case 1:	n++;
+		case 2:m++; n++;	break;
+		}
+	case 4:m++; break;
+	default:
+		break;
+	}
+	printf("m = %d, n = %d\n", m, n);
+	system("pause");
+	return 0;
+}
+//结果是m=5，n=3；
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		printf("%d ", i);
+		i++;
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		if (i == 5)
+			break;
+		printf("%d ", i);
+		i++;
+	}
+	return 0;
+}
+//打印结果为1，2，3，4
+//break永久的跳出循环，停止后期的所有循环，永久的终止循环
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		if (i == 5)
+			continue;
+		printf("%d ", i);
+		i++;
+	}
+	return 0;
+}
+//打印结果是1，2，3，4，4，4，4，4...死循环
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		i++;
+		if (i == 5)
+			continue;
+		printf("%d ", i);
+	}
+	return 0;
+}
+//打印结果为2，3，4，5，7，8，9，10，11
+//continue作用：continue用来终止本次的循环，当次continue之后的语句不再执行。
+
+
+//EOF为end of file
+#include<stdio.h>
+int main()
+{
+	int ch = 0;
+	while ((ch = getchar()) != EOF)
+		putchar(ch);
+	return 0;
+}
+//用ctrl+z来停下来。
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	for (a = 1; a <= 9; a++)
+	{
+		for (b = 1; b <= a; b++)
+		{
+			printf("%d*%d=%2d ", a, b, a * b);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int count = 0;
+	for (i = 1000; i <= 2000; i++)
+	{
+		if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0)
+		{
+			printf("%d ", i);
+			count++;
+		}
+	}
+	printf("\n");
+	printf("%d", count);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+	printf("%d %d", a, b);
+	return 0;
+}
+//按二进制位，异或：相同为0，相异为1；
+//异或不能解决除了整形以外类型的运算
+//但是这种算法不存在有溢出的问题
+//这段代码的可读性也比较差，一开始是看不懂的
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	printf("%d %d",a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+		i = 5;
+	}
+	return 0;
+}
+//第一个打印的数字为0，之后为6的死循环
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+		i += 5;
+	}
+	return 0;
+}
+//循环两次就结束了，打印的结果为0，6
+
+
+#include<stdio.h>
+int main()
+{
+	int x, y;
+	for (x = 0, y = 0; x < 2 && y < 5; ++x, ++y)
+	{
+		printf("hehe\n");
+	}
+	return 0;
+}
+//打印结果为2个hehe
+//&&操作符只要一边为假，另一边就不用看了。
+
+
+#include<stdio.h>
+int main()
+{
+	int x = 0;
+	int y = 0;
+	for (x = 0, y = 0; x < 2, y < 5; ++x, ++y)
+	{
+		printf("hehe\n");
+	}
+	return 0;
+}
+//打印结果为5个hehe，利用了逗号表达式，x=2的情况被忽略了，只看y<5,所以打印5个hehe
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	if (*pa == 1)
+		printf("小端\n");
+	else
+		printf("大端\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	return *pa;
+}
+int main()
+{
+	int ret = CheckSystem();
+	if (ret == 1)
+		printf("小端\n");
+	else
+		printf("大端\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	union Un
+	{
+		int i;
+		char c;
+	}u;
+	u.i = 1;
+	return u.c;
+}
+int main()
+{
+	int ret = CheckSystem();
+	if (ret == 1)
+		printf("小端\n");
+	else
+		printf("大端\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char a = 'w';
+	char* pc = &a;   //这个时候，pc称为字符指针，pc的类型为char*
+	char arr[10] = "abc";
+	char* pa = arr;   //arr作为数组名，他代表的是数组首元素的地址，pa的类型为char*
+	char* p = "abcdef";  //此时p只具有4个字节，但是abcdef不算\0有6个字节，显然放不下
+	//那么此时，p中存的就是"abcdef"中首个字母a的地址，地址用指针接收，刚好为4个字节
+	//上述常量表达式的结果就是首字符a的地址。
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = "abcdef";
+	char arr2[] = "abcdef";
+	if (arr1 == arr2)
+		printf("arr1==arr2\n");
+	else
+		printf("arr1!=arr2\n");
+	return 0;
+	//打印结果为arr1!=arr2
+	//原因:因为arr1和arr2会开辟两块空间的大小，虽然都表示的是首字母a的地址
+	//但是两块空间的地址是不一样的，所以两块空间中a的地址自然也就是不一样的了。
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char* p1 = "abcdef";
+	char* p2 = "abcdef";
+	if (p1 == p2)
+		printf("p1==p2");
+	else
+		printf("p1!=p2");
+	return 0;
+	//打印结果为p1==p2
+	//虽然把字符串abcdef地址存在了p1和p2当中，但是两个字符串是一样的
+	//都是abcdef，所以首元素a的地址自然也是一样的了
+	//abcdef为常量字符串，所以说他是不能被改变的，其实也只会出现一次abcdef
+	//但是p1的地址和p2的地址是绝对不一样的。
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = "abcdef";
+	//创建了一个数组，数组中存的是abcdef
+	const char* p = "abcdef";
+	//将字符串abcdef中首元素的地址，即a的地址，存在p中
+	arr1[2] = 'w';
+	//修改数组中的内容，是被允许的
+	//*(p + 2) = 'w';        //p[2]
+	//p所指向的是一个常量字符串，常量字符串中的值是不可以被修改的
+	return 0;
+	//这个程序运行起来是会崩溃的，崩溃的主要原因在于，*(p+2)='w'
+	//这个语句是不正确的
+	//字符指针不仅仅用来存放字符的地址，还可能存放一个字符串的起始地址
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10];  //整形数组
+	int* arr1[5];  //指针数组，存放指针的数组，也就是其内部元素的类型是指针类型
+	char** arr3[5]; //二级字符指针的数组
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	char c = 'w';
+	int* p1 = &a;
+	char* p2 = &c;
+	int arr[10] = { 0 };
+	char ch[5] = { 0 };
+	char* arr2[6] = { 0 };
+	//数组指针就是指向数组的指针
+	//arr  表示数组首元素的地址
+	//&arr[0]表示数组首元素的地址
+	//&arr 表示数组的地址
+	int(*p)[10] = &arr;  //数组的地址 
+	char(*pc)[5] = &ch;
+	char* (*pa)[6] = &arr2;
+	return 0;
+}
+
+
+#include<stdio.h>
+void Print1(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 1; i <= sz; i++)
+	{
+		printf("%d ", i);
+	}
+	printf("\n");
+}
+void Print2(int(*p)[10], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", (*p)[i]);
+		//首先找到数组，用(*p)找到数组，然后去访问第i个元素
+		//如果不带括号的话，那么p就会先和[]进行结合
+		//其实(*p)就等于是数组名然后[i]，就相当于是arr[i];
+	}
+}
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	Print1(arr, sz);
+	Print2(&arr, sz);
+	return 0;
+}
+
+
+#include<stdio.h>
+void Print1(int arr[3][5], int x, int y)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < x; i++)
+	{
+		for (j = 0; j < y; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+void Print2(int(*p)[5], int x, int y)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < x; i++)
+	{
+		for (j = 0; j < y; j++)
+		{
+			//printf("%d ", (*(p+i))[j]);
+			printf("%d ", *(*(p + i) + j));
+			//*(p+i)相当于找到了第i行首元素的地址
+			//也可以写成p[i][j]
+			//其实p就相当于arr
+		}
+		printf("\n");
+	}
+}
+int main()
+{
+	int arr[3][5] = { 1,2,3,4,5,3,4,5,6,7,4,5,6,7,8 };
+	Print1(arr, 3, 5);
+	Print2(arr, 3, 5);
+	//arr代表首元素的地址，对于二维数组来说，首元素的地址就是第一行的地址
+	//所以列标是很重要的
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[5];          //整形数组
+	int* parr1[10];      //指针数组
+	int(*parr2)[10];     //数组指针
+	int(*parr3[10])[5];  //存放数组指针的数组，可以存放10个这样的指针
+	//数组有10个对象，数组中存储对象的类型是数组指针
+	//5个整形元素的数组的地址 数组中每个元素的类型是int
+}
