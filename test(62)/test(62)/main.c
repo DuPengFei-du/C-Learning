74f0d4ae520407767c874bb9e0fad0deab62cad7
DuPengFei-du <935820240@qq.com>
2021-07-06 23:03:46 +0800

    test

---
+++ test(62)/test(62)/main.c
@@ -0,0 +1,850 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+/*
+当*start！='\0'的时候，就把start赋值给s1，让他去查找,把str2赋值给s2
+让s2也从起始位置开始，然后循环的判断条件 *s1 != '\0' && *s2 != '\0' && *s1 == *s2
+然后s1和s2进行加加，加加完了之后，再上去判断，当有一次，s1或者s2
+等于'\0’的时候，或者他们不相等的时候，就跳出来，如果*str2=='\0'的时候，就是找到了，然后跳出来
+如果找不到的话，就返回空指针。
+如果要找一个空字符串的话（特殊情况）：
+在库里面，对于这种特殊情况的处理，就是直接返回str1
+*/
+#include<stdio.h>
+#include<assert.h>
+char* my_strstr(const char* str1, const char* str2)
+{
+	const char* s1 = str1;
+	const char* s2 = str2;
+	const char* start = str1;
+	assert(str1 != NULL);
+	assert(str2 != NULL);
+	if (*str2 == '\0')
+	{
+		return (char*)str1;   //找空字符串，直接返回str1
+	}
+	while (*start != '\0')//当start遇到'\0'的时候就没有比要再继续查找了，那一定是查找不到的了
+	{
+		s2 = str2;
+		s1 = start;
+		while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+		if (*s2 == '\0')
+		{
+			return (char*)start;
+		}
+		start++;
+	}
+	return NULL;
+}
+int main()
+{
+	char arr1[] = "abcdefabcdef";
+	char arr2[] = "bcd";
+	char* ret = my_strstr(arr1, arr2);
+	if (*ret == NULL)
+	{
+		printf("不存在\n");
+	}
+	else
+	{
+		printf("%s\n", ret);
+	}
+	return 0;
+}
+
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	const char str[] = "http://www.runoob.com";
+	const char ch = '.';
+	const char* ret;
+	ret = strchr(str, ch);
+	printf("|%c| 之后的字符串是 - |%s|\n", ch, ret);
+	return 0;
+}
+//如果找不到还是返回空指针
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	if (*pa == 1)
+		printf("小端\n");
+	else
+		printf("大端\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	return *pa;
+}
+int main()
+{
+	int ret = CheckSystem();
+	if (ret == 1)
+		printf("小端\n");
+	else
+		printf("大端\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	union Un
+	{
+		int i;
+		char c;
+	}u;
+	u.i = 1;
+	return u.c;
+}
+int main()
+{
+	int ret = CheckSystem();
+	if (ret == 1)
+		printf("小端\n");
+	else
+		printf("大端\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int cmp_int(const void* e1, const void* e2)
+{
+	return *(int*)e1 - *(int*)e2;
+}
+void Swap(char* buf1, char* buf2, int width)
+{
+	for (int i = 0; i < width; i++)
+	{
+		char temp = *buf1;
+		*buf1 = *buf2;
+		*buf2 = temp;
+		buf1++;
+		buf2++;
+	}
+}
+void My_BubbleSort(int arr[], int sz, int width, int (*cmp_int)(const void* e1, const void* e2))
+{
+	for (int i = 0; i < sz - 1; i++)
+	{
+		for (int j = 0; j < sz - i - 1; j++)
+		{
+			if (cmp_int((char*)arr + j * width, (char*)arr + (j + 1) * width) > 0)
+			{
+				Swap((char*)arr + j * width, (char*)arr + (j + 1) * width, width);
+			}
+		}
+	}
+}
+int main()
+{
+	int arr[10] = { 1,3,5,7,9,2,4,6,8,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	My_BubbleSort(arr, sz, sizeof(arr[0]), cmp_int);
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr[20] = "abcdef";
+	int len = strlen(arr);
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[] = "abcdef";
+	char arr2[] = "abc";
+	if (strlen(arr2) - strlen(arr1) > 0)
+		printf("arr2>arr1");
+	else
+		printf("arr2<arr1");
+	return 0;
+}
+
+
+//模拟实现strcpy
+//字符串的拷贝
+#include<stdio.h>
+void my_strcpy(char* dest, char* source)//用指针接收
+{
+	while (*source != '\0')
+	{
+		*dest = *source;
+		dest++;
+		source++;
+	}
+	*dest = *source;
+}
+int main()
+{
+	char arr1[20] = { 0 };
+	char arr2[] = "hello word";
+	my_strcpy(arr1, arr2);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+char* my_strcat(char* dest, const char* source)
+{
+	//字符串的连接首先要找到目标字符串的\0,然后在去追加，一共分为两步
+	char* ret = dest;
+	assert(dest != NULL);
+	assert(source != NULL);
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*dest++ = *source++)
+	{
+		;
+	}
+	return ret;
+}
+int main()
+{
+	char arr1[20] = "hello ";
+	my_strcat(arr1, "world");
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[30] = "hello world";
+	char arr2[] = "change world";
+	int ret = strncmp(arr1, arr2, 3);
+	printf("%d", ret);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = "abcdefabcdef";
+	char arr2[] = "bcd";
+	char* ret = strstr(arr1, arr2);
+	if (ret == NULL)
+	{
+		printf("找不到\n");   //不能直接返回，需要先进行判断一下
+	}
+	else
+	{
+		printf("%s\n", ret);
+
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+char* my_strstr(const char* str1, const char* str2)
+{
+	const char* s1 = str1;
+	const char* s2 = str2;
+	const char* start = str1;
+	assert(str1 != NULL);
+	assert(str2 != NULL);
+	if (*str2 == '\0')
+	{
+		return (char*)str1;   //找空字符串，直接返回str1
+	}
+	while (*start != '\0')//当start遇到'\0'的时候就没有比要再继续查找了，那一定是查找不到的了
+	{
+		s2 = str2;
+		s1 = start;
+		while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+		if (*s2 == '\0')
+		{
+			return (char*)start;
+		}
+		start++;
+	}
+	return NULL;
+}
+int main()
+{
+	char arr1[] = "abcdefabcdef";
+	char arr2[] = "bcd";
+	char* ret = my_strstr(arr1, arr2);
+	if (*ret == NULL)
+	{
+		printf("不存在\n");
+	}
+	else
+	{
+		printf("%s\n", ret);
+	}
+	return 0;
+}
+
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	const char str[] = "http://www.runoob.com";
+	const char ch = '.';
+	const char* ret;
+	ret = strchr(str, ch);
+	printf("|%c| 之后的字符串是 - |%s|\n", ch, ret);
+	return 0;
+}
+//如果找不到还是返回空指针
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	const char* p = "@.";
+	char* ret = NULL;
+	char arr1[] = "xiaodu@learn.ing";
+	char temp[50] = { 0 };
+	strcpy(temp, arr1);
+	for (ret = strtok(temp, p); ret != NULL; ret = strtok(NULL, p))
+	{
+		printf("%s", ret);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	if (*pa == 1)
+		printf("小端\n");
+	else
+		printf("大端\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	return *pa;
+}
+int main()
+{
+	int ret = CheckSystem();
+	if (ret == 1)
+		printf("小端\n");
+	else
+		printf("大端\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	union Un
+	{
+		int i;
+		char c;
+	}u;
+	u.i = 1;
+	return u.c;
+}
+int main()
+{
+	int ret = CheckSystem();
+	if (ret == 1)
+		printf("小端\n");
+	else
+		printf("大端\n");
+	return 0;
+}
+
+#include<stdlib.h>
+#include<stdio.h>
+int cmp_int(const void* e1, const void* e2)
+{
+	return *(int*)e1 - *(int*)e2;
+}
+int main()
+{
+	int arr[10] = { 1,3,5,7,9,2,4,6,8,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	qsort(arr, sz, sizeof(arr[0]), cmp_int);
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+char* my_strcpy(char* dest, const char* source)
+{
+	assert(dest);
+	assert(source);
+	char* ret = dest;
+	while(*dest++=*source++)
+	{
+		;
+	}
+	return ret;
+}
+int main()
+{
+	char arr[20] = { 0 };
+	char arr1[] = "abcdef";
+	my_strcpy(arr, arr1);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+#include<string.h>
+char* my_strncpy(char* dest, const char* source,size_t count)
+{
+	assert(dest);
+	assert(source);
+	char* ret = dest;
+	int sz = 0;
+	if (strlen(source) < count)
+	{
+		sz = count - strlen(source);
+		count = strlen(source);
+	}
+	while (count--)
+	{
+		*dest++ = *source++;
+	}
+	while (sz--)
+	{
+		*dest = '\0';
+	}
+	return ret;
+}
+int main()
+{
+	char arr[20] = { 0 };
+	char arr1[] = "abcdef";
+	my_strncpy(arr, arr1,3);
+	printf("%s", arr);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr[20] = "abcdef";
+	int len = strlen(arr);
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[] = "abcdef";
+	char arr2[] = "abc";
+	if (strlen(arr2) - strlen(arr1) > 0)
+		printf("arr2>arr1");
+	else
+		printf("arr2<arr1");
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[20] = { 0 };
+	char arr2[] = "abcdef";
+	strcpy(arr1, arr2);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+//模拟实现strcpy
+//字符串的拷贝
+#include<stdio.h>
+void my_strcpy(char* dest, char* source)//用指针接收
+{
+	while (*source != '\0')
+	{
+		*dest = *source;
+		dest++;
+		source++;
+	}
+	*dest = *source;
+}
+int main()
+{
+	char arr1[20] = { 0 };
+	char arr2[] = "hello word";
+	my_strcpy(arr1, arr2);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[20] = "Hello";
+	char arr2[] = " world";
+	strcat(arr1, arr2);
+	printf("%s\n", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[20] = "hello world";
+	char arr2[] = "hi";
+	strncpy(arr1, arr2, 2);
+	printf("%s\n", arr1);
+	return 0;
+}
+
+
+char* strstr(const char* string, const char* strCharSet);
+//查找子字符串，返回的是地址，第一次出现的位置
+//如果没有找到的话，返回空指针，但是不能直接返回，因为如果是没有找到的话，返回空指针就是非法的内存访问了，就会出错了。
+
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	const char str[] = "http://www.runoob.com";
+	const char ch = '.';
+	const char* ret;
+	ret = strchr(str, ch);
+	printf("|%c| 之后的字符串是 - |%s|\n", ch, ret);
+	return 0;
+}
+//如果找不到还是返回空指针
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	if (*pa == 1)
+		printf("小端\n");
+	else
+		printf("大端\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	return *pa;
+}
+int main()
+{
+	int ret = CheckSystem();
+	if (ret == 1)
+		printf("小端\n");
+	else
+		printf("大端\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	union Un
+	{
+		int i;
+		char c;
+	}u;
+	u.i = 1;
+	return u.c;
+}
+int main()
+{
+	int ret = CheckSystem();
+	if (ret == 1)
+		printf("小端\n");
+	else
+		printf("大端\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int cmp_int(const void* e1, const void* e2)
+{
+	return *(int*)e1 - *(int*)e2;
+}
+void Swap(char* buf1, char* buf2,int width)
+{
+	for (int i = 0; i < width; i++)
+	{
+		char temp = *buf1;
+		*buf1 = *buf2;
+		*buf2 = temp;
+		buf1++;
+		buf2++;
+	}
+}
+void My_BubbleSort(int arr[], int sz, int width, int (*cmp_int)(const void* e1, const void* e2))
+{
+	for (int i = 0; i < sz -1 ; i++)
+	{
+		for (int j = 0; j < sz - i -1 ; j++)
+		{
+			if (cmp_int((char*)arr+j*width,(char*)arr+(j+1)*width)>0)
+			{
+				Swap((char*)arr + j * width, (char*)arr + (j + 1) * width, width);
+			}
+		}
+	}
+}
+int main()
+{
+	int arr[10] = { 1,3,5,7,9,2,4,6,8,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	My_BubbleSort(arr, sz, sizeof(arr[0]), cmp_int);
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strlen(const char* str)
+{
+	assert(str);
+	if (*str == '\0')
+		return 0;
+	else
+		return 1 + my_strlen(str + 1);
+}
+int main()
+{
+	char arr[] = "abcdef";
+	int len = my_strlen(arr);
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strlen(const char* str)
+{
+	assert(str);
+	if (*str == '\0')
+		return 0;
+	else
+		return 1 + my_strlen(str + 1);
+}
+int main()
+{
+	char arr[] = "abcdef";
+	int len = my_strlen(arr);
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strlen(const char* str)
+{
+	assert(str);
+	int count = 0;
+	while (*str)
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+int main()
+{
+	char arr[] = "abcdef";
+	int len = my_strlen(arr);
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strlen(char* str)
+{
+	assert(str);
+	char* ret = str;
+	while (*str)
+	{
+		str++;
+	}
+	return str - ret;
+}
+int main()
+{
+	char arr[] = "abcdef";
+	int len = my_strlen(arr);
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr[20] = "abcdef";
+	int len = strlen(arr);
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[] = "abcdef";
+	char arr2[] = "abc";
+	if (strlen(arr2) - strlen(arr1) > 0)
+		printf("arr2>arr1");
+	else
+		printf("arr2<arr1");
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[20] = { 0 };
+	char arr2[] = "abcdef";
+	strcpy(arr1, arr2);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+//模拟实现strcpy
+//字符串的拷贝
+#include<stdio.h>
+void my_strcpy(char* dest, char* source)//用指针接收
+{
+	while (*source != '\0')
+	{
+		*dest = *source;
+		dest++;
+		source++;
+	}
+	*dest = *source;
+}
+int main()
+{
+	char arr1[20] = { 0 };
+	char arr2[] = "hello word";
+	my_strcpy(arr1, arr2);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+char* my_strcpy(char* dest, const char* source)
+{
+	assert(dest);
+	assert(source);
+	char* ret = dest;
+	while (*dest++ = *source++)
+	{
+		;
+	}
+	return ret;
+}
+int main()
+{
+	char arr1[20] = { 0 };
+	char arr2[] = "hello word";
+	my_strcpy(arr1, arr2);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[20] = "Hello";
+	char arr2[] = " world";
+	strcat(arr1, arr2);
+	printf("%s\n", arr1);
+	return 0;
+}
