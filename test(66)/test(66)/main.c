22266f25ac8bad8a811ae26ddcac5d395a921dba
DuPengFei-du <935820240@qq.com>
2021-07-20 22:17:40 +0800

    test

---
+++ test(66)/test(66)/main.c
@@ -0,0 +1,600 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	if (*pa == 1)
+		printf("小端\n");
+	else
+		printf("大端\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	return *pa;
+}
+int main()
+{
+	int ret = CheckSystem();
+	if (ret == 1)
+		printf("小端\n");
+	else
+		printf("大端\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	union Un
+	{
+		int i;
+		char c;
+	}u;
+	u.i = 1;
+	return u.c;
+}
+int main()
+{
+	int ret = CheckSystem();
+	if (ret == 1)
+		printf("小端\n");
+	else
+		printf("大端\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<stdlib.h>
+int cmp_int(const void* e1, const void* e2)
+{
+	return *(int*)e1 - *(int*)e2;
+}
+int main()
+{
+	int arr[10] = { 1,3,5,7,9,2,4,6,8,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	qsort(arr, sz, sizeof(arr[0]), cmp_int);
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int cmp_int(const void* e1, const void* e2)
+{
+	return *(int*)e1 - *(int*)e2;
+}
+void Swap(char* buf1, char* buf2, int width)
+{
+	for (int i = 0; i < width; i++)
+	{
+		char temp = *buf1;
+		*buf1 = *buf2;
+		*buf2 = temp;
+		buf1++;
+		buf2++;
+	}
+}
+void My_BubbleSort(int arr[], int sz, int width, int (*cmp_int)(const void* e1, const void* e2))
+{
+	for (int i = 0; i < sz - 1; i++)
+	{
+		for (int j = 0; j < sz - i - 1; j++)
+		{
+			if (cmp_int((char*)arr + j * width, (char*)arr + (j + 1) * width) > 0)
+			{
+				Swap((char*)arr + j * width, (char*)arr + (j + 1) * width, width);
+			}
+		}
+	}
+}
+int main()
+{
+	int arr[10] = { 1,3,5,7,9,2,4,6,8,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	My_BubbleSort(arr, sz, sizeof(arr[0]), cmp_int);
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strlen(const char* str)
+{
+	assert(str);
+	if (*str == '\0')
+		return 0;
+	else
+		return 1 + my_strlen(str + 1);
+}
+int main()
+{
+	char arr[] = "abcdef";
+	int len = my_strlen(arr);
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strlen(const char* str)
+{
+	assert(str);
+	int count = 0;
+	while (*str)
+	{
+		str++;
+		count++;
+	}
+	return count;
+}
+int main()
+{
+	char arr[] = "abcdef";
+	int len = my_strlen(arr);
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strlen(const char* str)
+{
+	assert(str);
+	char* ret = str;
+	while (*str)
+	{
+		str++;
+	}
+	return str-ret;
+}
+int main()
+{
+	char arr[] = "abcdef";
+	int len = my_strlen(arr);
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+char *my_strcpy(char* dest,const char *source)
+{
+	assert(dest);
+	assert(source);
+	char* ret = dest;
+	while (*dest++ = *source++)
+	{
+		;
+	}
+	return ret;
+}
+int main()
+{
+	char arr[] = "abcdef";
+	char arr1[20] = { 0 };
+	my_strcpy(arr1, arr);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+char* my_strcat(char* dest, const char* source)
+{
+	assert(dest);
+	assert(source);
+	char* ret = dest;
+	while (*dest)
+	{
+		dest++;
+	}
+	while (*dest++ = *source++)
+	{
+		;
+	}
+	return ret;
+}
+int main()
+{
+	char arr[] = "abcdef";
+	char arr1[20] = "AB";
+	my_strcat(arr1, arr);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+char* my_strncat(char* dest, const char* source,int n)
+{
+	assert(dest);
+	assert(source);
+	char* ret = dest;
+	while (*dest)
+	{
+		dest++;
+	}
+	while (n--)
+	{
+		*dest++ = *source++;
+	}
+	*dest = '\0';
+	return ret;
+}
+int main()
+{
+	char arr[] = "abcdef";
+	char arr1[20] = "AB";
+	my_strncat(arr1, arr,3);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+#include<string.h>
+char* my_strncpy(char* dest, const char* source, size_t count)
+{
+	assert(dest);
+	assert(source);
+	char* ret = dest;
+	int sz = 0;
+	if (strlen(source) < count)
+	{
+		sz = count - strlen(source);
+		count = strlen(source);
+	}
+	while (count--)
+	{
+		*dest++ = *source++;
+	}
+	while (sz--)
+	{
+		*dest = '\0';
+	}
+	return ret;
+}
+int main()
+{
+	char arr[] = "abcdef";
+	char arr1[20] = "AB";
+	my_strncpy(arr1, arr, 3);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	if (*pa == 1)
+		printf("小端\n");
+	else
+		printf("大端\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	return *pa;
+}
+int main()
+{
+	int ret = CheckSystem();
+	if (ret == 1)
+		printf("小端\n");
+	else
+		printf("大端\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	union Un
+	{
+		int i;
+		char c;
+	}u;
+	u.i = 1;
+	return u.c;
+}
+int main()
+{
+	int ret = CheckSystem();
+	if (ret == 1)
+		printf("小端\n");
+	else
+		printf("大端\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strlen(const char* str)
+{
+	assert(str);
+	if (*str == '\0')
+		return 0;
+	else
+		return 1 + my_strlen(str + 1);
+}
+int main()
+{
+	char arr[] = "abcdef";
+	int len = my_strlen(arr);
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strlen(const char* str)
+{
+	assert(str);
+	int count = 0;
+	while (*str)
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+int main()
+{
+	char arr[] = "abcdef";
+	int len = my_strlen(arr);
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strlen(char* str)
+{
+	assert(str);
+	char* ret = str;
+	while (*str)
+	{
+		str++;
+	}
+	return str - ret;
+}
+int main()
+{
+	char arr[] = "abcdef";
+	int len = my_strlen(arr);
+	printf("%d", len);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+char * my_strcpy(char* dest,const char *source)
+{
+	assert(dest);
+	assert(source);
+	char* ret = dest;
+	while (*dest++ = *source++)
+	{
+		;
+	}
+	return ret;
+}
+int main()
+{
+	char arr[] = "abcdef";
+	char arr1[20] = { 0 };
+	my_strcpy(arr1, arr);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+#include<string.h>
+char* my_strncpy(char* dest, const char* source,size_t count)
+{
+	assert(dest);
+	assert(source);
+	char* ret = dest;
+	int sz = 0;
+	if (strlen(source) < count)
+	{
+		sz = count - strlen(source);
+		count = strlen(source);
+	}
+	while (count--)
+	{
+		*dest++ = *source++;
+	}
+	while (sz--)
+	{
+		*dest = '\0';
+	}
+	return ret;
+}
+int main()
+{
+	char arr[] = "abcdef";
+	char arr1[20] = { 0 };
+	my_strncpy(arr1, arr,3);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+char * my_strcat(char* dest,const char *source)
+{
+	assert(dest);
+	assert(source);
+	char* ret = dest;
+	while (*dest)
+	{
+		dest++;
+	}
+	while (*dest++ = *source++)
+	{
+		;
+	}
+	return ret;
+}
+int main()
+{
+	char arr[] = "abcdef";
+	char arr1[20] = "AB";
+	my_strcat(arr1, arr);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+char * my_strncat(char* dest,const char *source,int n)
+{
+	assert(dest);
+	assert(source);
+	char* ret = dest;
+	while (*dest)
+	{
+		dest++;
+	}
+	while (n--)
+	{
+		*dest++ = *source++;
+	}
+	*dest = '\0';
+	return ret;
+}
+int main()
+{
+	char arr[] = "abcdef";
+	char arr1[20] = "AB";
+	my_strncat(arr1, arr,4);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[] = "abcdef";
+	char arr2[] = "abc";
+	if (strlen(arr2) - strlen(arr1) > 0)
+		printf("arr2>arr1");
+	else
+		printf("arr2<arr1");
+	return 0;
+}
+
+
+//模拟实现strcpy
+//字符串的拷贝
+#include<stdio.h>
+void my_strcpy(char* dest, char* source)//用指针接收
+{
+	while (*source != '\0')
+	{
+		*dest = *source;
+		dest++;
+		source++;
+	}
+	*dest = *source;
+}
+int main()
+{
+	char arr1[20] = { 0 };
+	char arr2[] = "hello word";
+	my_strcpy(arr1, arr2);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[] = "bdefetfsf";
+	char arr2[] = "abc";
+	int ret = strcmp(arr1, arr2);
+	printf("%d", ret);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+int my_strcmp(const char* str1, const char* str2)
+{
+	assert(str1 != NULL);
+	assert(str2 != NULL);
+	while (*str1 == *str2)
+	{
+		if (*str1 == '\0')
+		{
+			return 0;
+		}
+		str1++;
+		str2++;
+	}
+	if (*str1 > * str2)
+	{
+		return 1;
+	}
+	else
+	{
+		return -1;
+	}
+}
+int main()
+{
+	char arr1[] = "bedfwseg";
+	char arr2[] = "abc";
+	int ret = my_strcmp(arr1, arr2);
+	printf("%d", ret);
+	return 0;
+}
