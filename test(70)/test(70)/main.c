c17efd4e98d16b1385bf926cd9da9bf344f830b3
DuPengFei-du <935820240@qq.com>
2021-08-09 21:16:23 +0800

    test

---
+++ test(70)/test(70)/main.c
@@ -0,0 +1,813 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int left = 0;
+	int right = sz - 1;
+	int key = 7;
+	while (left <= right)
+	{
+		int mid = left + (right - left) / 2;
+		if (arr[mid] > key)
+			right = mid - 1;
+		else if (arr[mid] < key)
+			left = mid + 1;
+		else
+		{
+			printf("找到了，下标为:%d", mid);
+			break;
+		}
+	}
+	return -1;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int age = 10;
+	if (age = 5)
+		printf("age==5\n");
+	return 0;
+}
+//频幕上会打印age==5，因为把5赋值给了age，所以if后面的括号里面的值是真的，所以回打印要求的信息
+
+
+#include<stdio.h>
+int main()
+{
+	int age = 10;
+	if (age == 5)
+		printf("age==5\n");
+	return 0;
+}
+//屏幕上什么都不会打印，因为age！=5；
+
+
+//猜数字游戏的实现
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+void menu()
+{
+	printf("***** 欢迎来到猜数字游戏！*****\n");
+	printf("*****        1. play      *****\n");
+	printf("*****        0. exit      *****\n");
+	printf("***** 欢迎来到猜数字游戏！*****\n");
+}
+void game()
+{
+	int ret = rand() % 100 + 1;
+	int guess = 0;
+	while (1)
+	{
+		printf("请输入所猜测的数字!\n");
+		(void)scanf("%d", &guess);
+		if (guess > ret)
+			printf("猜大了!\n");
+		else if (guess < ret)
+			printf("猜小了!\n");
+		else
+		{
+			printf("猜对了!\n");
+			break;
+		}
+	}
+}
+void test()
+{
+	srand((unsigned int)time(NULL));
+	int input = 0;
+	do
+	{
+		menu();
+		printf("请选择:>\n");
+		(void)scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏!\n");
+			break;
+		default:
+			printf("输入错误，请重新输入!\n");
+			break;
+		}
+	} while (input);
+}
+int main()
+{
+	test();
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int n = 1;
+	int m = 2;
+	switch (n)
+	{
+	case 1:m++;
+	case 2:n++;
+	case 3:
+		switch (n)
+		{//switch允许嵌套使用
+		case 1:	n++;
+		case 2:m++; n++;	break;
+		}
+	case 4:m++; break;
+	default:
+		break;
+	}
+	printf("m = %d, n = %d\n", m, n);
+	system("pause");
+	return 0;
+}
+//结果是m=5，n=3；
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		if (i == 5)
+			continue;
+		printf("%d ", i);
+		i++;
+	}
+	return 0;
+}
+//打印结果是1，2，3，4，4，4，4，4...死循环
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	for (a = 1; a <= 9; a++)
+	{
+		for (b = 1; b <= a; b++)
+		{
+			printf("%d*%d=%2d ", a, b, a * b);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int temp = 0;
+	(void)scanf("%d %d", &a, &b);
+	temp = a;
+	a = b;
+	b = temp;
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int j = 0;
+	for (; i < 10; i++)
+	{
+		for (; j < 10; j++)
+		{
+			printf("hehe\n");
+		}
+	}
+	return 0;
+}
+//这段代码得打印结果只有10个hehe
+//并且10个hehe是从i=0-- i=9一口气打印出来的
+//当i=0时，j从一直到9，打印了10个hehe之后，j的条件就不再满足了
+//所以一共只会打印10个hehe
+
+
+#include<stdio.h>
+int main()
+{
+	int x = 0;
+	int y = 0;
+	for (x = 0, y = 0; x < 2, y < 5; ++x, ++y)
+	{
+		printf("hehe\n");
+	}
+	return 0;
+}
+//打印结果为5个hehe，利用了逗号表达式，x=2的情况被忽略了，只看y<5,所以打印5个hehe
+
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main()
+{
+	char input[] = { 0 };
+	system("shutdown -s -t 60");
+again:
+	printf("请注意，你的电脑在1分钟内关机，如果输入：我是猪，就取消关机\n");
+	scanf("%s", input);
+	if (0 == strcmp(input, "我是猪"))
+	{
+		system("shutdown -a");
+	}
+	else
+	{
+		printf("输入错误，请重新输入!\n");
+		goto again;
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main()
+{
+	char input[] = { 0 };
+	system("shutdown -s -t 60");
+	//这是Windows系统下的一个关机命令
+	while (1)
+	{
+		printf("你的电脑将在一分钟后关机，如果输入我是猪，就会取消关机！\n请输入：");
+		scanf("%s", input);
+		if (0 == strcmp(input, "我是猪"))
+		{
+			system("shutdown -a");//取消关机命令
+			break;
+		}
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr[20] = { 0 };
+	char arr1[] = "hello world";
+	strcpy(arr, arr1);
+	printf("%s", arr);
+	return 0;
+}
+
+
+#include<stdio.h>
+#include<assert.h>
+char* my_strcpy(char* dest, const char* source)
+{
+	assert(dest);
+	assert(source);
+	char* ret = dest;
+	while (*dest++ = *source++)
+	{
+		;
+	}
+	return ret;
+}
+int main()
+{
+	char arr[20] = { 0 };
+	char arr1[] = "hello world";
+	my_strcpy(arr, arr1);
+	printf("%s", arr);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	if (b > a)
+		printf("%d", b);
+	else
+		printf("%d", a);
+	return 0;
+}
+
+
+#include<stdio.h>
+int MAX(int x, int y)
+{
+	return x > y ? x : y;
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	(void)scanf("%d %d", &a, &b);
+	c = MAX(a, b);
+	printf("%d", c);
+	return 0;
+}
+
+
+//判断一个数字是不是素数
+#include<stdio.h>
+int is_prime(int n)
+{
+	for (int i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+			return 0;
+	}
+	return 1;
+}
+int main()
+{
+	int n = 0;
+	(void)scanf("%d", &n);
+	if (is_prime(n) == 1)
+		printf("素数\n");
+	else
+		printf("不是素数\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+void Print(int n)
+{
+	if (n > 10)
+	{
+		Print(n / 10);
+	}
+	printf("%d ", n % 10);
+}
+int main()
+{
+	int n = 0;
+	(void)scanf("%d", &n);
+	Print(n);
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = "hello world";
+	memset(arr1, 'x', 5);
+	printf("%s", arr1);
+	return 0;
+}
+
+
+#include<stdio.h>
+void Swap(int* x, int* y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	(void)scanf("%d %d", &a, &b);
+	Swap(&a, &b);
+	printf("%d %d", a, b);
+	return 0;
+}
+
+
+#include<stdio.h>
+int is_leapyear(int i)
+{
+	if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0)
+		return 1;
+	else
+		return 0;
+}
+int main()
+{
+	int n = 0;
+	(void)scanf("%d", &n);
+	if (is_leapyear(n) == 1)
+	{
+		printf("是闰年\n");
+	}
+	else
+	{
+		printf("不是闰年\n");
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+void new_line()
+{
+	printf("hehe\n");
+}
+void three_line()
+{
+	for (int i = 0; i < 3; i++)
+	{
+		new_line();
+	}
+}
+int main()
+{
+	three_line();
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	printf("%d", printf("%d", printf("%d", 43)));
+	return 0;
+}
+//打印的结果为4 3 2 1
+//printf函数的返回值是字符的个数
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int Add(int x, int y);  //称为函数的声明
+	int c = Add(a, b);
+	printf("%d", c);
+	return 0;
+}
+//函数定义
+int Add(int x, int y)
+{
+	return x + y;
+}
+
+
+#include<stdio.h>
+int my_strlen(const char* str)
+{
+	int count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+int main()
+{
+	int len = my_strlen("abc");
+	printf("%d", len);
+	return 0;
+}
+
+
+int fac(int n)
+{
+	if (n <= 1)
+		return 1;
+	else
+		return n * fac(n - 1);
+}
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int c = 0;
+	(void)scanf("%d", &n);
+	c = fac(n);
+	printf("%d\n", c);
+	return 0;
+}
+
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	if (*pa == 1)
+		printf("小端\n");
+	else
+		printf("大端\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	int a = 1;
+	char* pa = (char*)& a;
+	return *pa;
+}
+int main()
+{
+	int ret=CheckSystem();
+	if (ret == 1)
+		printf("小端\n");
+	else
+		printf("大端\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int CheckSystem()
+{
+	union Un
+	{
+		int i;
+		char c;
+	}u;
+	u.i = 1;
+	return u.c;
+}
+int main()
+{
+	int ret = CheckSystem();
+	if (ret == 1)
+		printf("小端\n");
+	else
+		printf("大端\n");
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,3,5,7,9,2,4,6,8,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (int i = 0; i < sz - 1; i++)
+	{
+		for (int j = 0; j < sz - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,3,5,7,9,2,4,6,8,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (int i = 0; i < sz - 1; i++)
+	{
+		int flag = 1;
+		for (int j = 0; j < sz - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+				flag = 0;
+			}
+		}
+		if (flag == 1)
+			break;
+	}
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr1[10];
+	char arr2[5];
+	return 0;
+}
+//[]中必须是常量或者常量表达式，不然就会报错
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5 };
+	return 0;
+}
+//没有被赋值的元素则默认为为0
+//通过监视窗口来看
+
+
+#include<stdio.h>
+int main()
+{
+	char arr1[] = { 'a','b','c' };
+	char arr2[] = "abc";
+	printf("%s", arr1);
+	printf("%s", arr2);
+	return 0;
+}
+//这两个数组的是不一样的
+//arr1是不可以看成数组的，因为它没有\0，\0是字符串的结束标志
+//如果没有\0是不可以看成字符串的
+//arr2是可以看成字符串的，c后面还有一个隐藏的\0
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (int i = 0; i < sz; i++)
+	{
+		printf("&arr[%d]=%p\n", i, &arr[i]);
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int* p = arr;  //得到了首元素的地址
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%p===%p\n", &arr[i], p + i);
+	}
+	return 0;
+}
+//这个代码就说明：p+i为下标为i的元素的地址
+//*(p + i)为下标为i的元素的值
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int* p = arr;
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", *(p + i));
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr1[3][4]; //创建了一个三行四列的数组
+	char arr2[4][5];
+	double arr3[3][5];
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[3][4] = { 1,2,3,4,5,6 };//后面没有初始化的元素全部当0对待
+	int arr2[3][4] = { {1,2},{3,4},{5,6} };//第一行为1 2 0 0 第二行为3 4 0 0
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[3][4] = { 0 };
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[2][2] = { 1,2,3,4 };
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[3][4] = { 0 };
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			printf("&arr[%d][%d]=%p\n", i, j, &arr[i][j]);
+		}
+	}
+	return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	printf("%d", sizeof(arr));
+	return 0;
+}
+//这个代码的结果是40
